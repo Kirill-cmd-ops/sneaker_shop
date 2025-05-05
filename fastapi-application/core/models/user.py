d3c0c2b324436d82edd1b9daadaa08b28245f784
Kirill <bondarenkokirill150208@gmail.com>
2025-05-05 22:41:12 +0300

    create users table and migrations

---
+++ fastapi-application/core/models/user.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase
+)
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.types.user_id import UserIdType
+from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
+
+
+
