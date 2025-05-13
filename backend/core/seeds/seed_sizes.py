93f7955684f44677ffd083f4d480b704b7ddd8d9
Kirill <bondarenkokirill150208@gmail.com>
2025-05-14 01:12:29 +0300

    Update seeds for test

--- backend/core/seeds/seed_sizes.py
+++ backend/core/seeds/seed_sizes.py
@@ -2,7 +2,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from backend.core.models.size import Size
 
 async def seed_sizes(db: AsyncSession):
-    sizes = [Size(eu_size=size) for size in [38.0, 40.5, 42.0, 44.5, 46.0]]
+    sizes = [Size(eu_size=size) for size in range(15, 51)]
     db.add_all(sizes)
     await db.commit()
-
--- backend/core/seeds/seed_sneaker_sizes.py
+++ backend/core/seeds/seed_sneaker_sizes.py
@@ -1,19 +1,19 @@
+import random
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.core.models.sneaker_size import SneakerSizeAssociation
 
 async def seed_sneaker_sizes(db: AsyncSession):
     associations = []
 
-    size_ids = [1, 2, 3, 4, 5]
-
     sneaker_ids = list(range(1, 33))
 
     for sneaker_id in sneaker_ids:
+        size_ids = sorted(random.sample(range(1, 37), k=random.randint(2, 5)))
+
         for size_id in size_ids:
-            quantity = (sneaker_id + size_id) % 10 + 1
+            quantity = random.randint(1, 10)
             associations.append(SneakerSizeAssociation(sneaker_id=sneaker_id, size_id=size_id, quantity=quantity))
 
     db.add_all(associations)
     await db.flush()
     await db.commit()
-
