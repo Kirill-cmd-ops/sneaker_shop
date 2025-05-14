9d917212221a0656fedee055fcd15914e49d4fc6
Kirill <bondarenkokirill150208@gmail.com>
2025-05-14 18:25:28 +0300

    Create ClientLayout

---
+++ frontend/app/components/ClientLayout.js
@@ -0,0 +1,16 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function ClientLayout({ children }) {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      {pathname !== "/about" && <Footer />}
+    </div>
+  );
+}
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import ClientLayout from "./components/ClientLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen`}>
-        <Navbar />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
