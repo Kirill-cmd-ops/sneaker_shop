3caf32b407ea6d30e33088c2553a90f8646d38d0
Kirill <bondarenkokirill150208@gmail.com>
2025-06-01 19:32:07 +0300

    Setting up alembic - adding your own Base, config.py

---
+++ backend/favorite_service/favorite_service/favorite/models/base.py
@@ -0,0 +1,19 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
+
+from backend.favorite_service.favorite_service.favorite.config import settings
+from backend.favorite_service.favorite_service.utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        name = camel_case_to_snake_case(cls.__name__)
+        return "countries" if cls.__name__ == "Country" else f"{name}s"
