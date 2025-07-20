b1e181e357b2e035f86851d63a417b200d0fced4
Kirill <bondarenkokirill150208@gmail.com>
2025-07-21 01:02:53 +0300

    handlers for consumers are written

---
+++ backend/favorite_service/favorite_service/favorite/kafka_handler/favorite_handler.py
@@ -0,0 +1,8 @@
+from favorite_service.favorite.models import db_helper
+from favorite_service.favorite.services.favorite import create_favorite
+
+
+async def handle_favorite(key, value):
+    user_id = key or value.get("id")
+    async with db_helper.session_getter() as session:
+        await create_favorite(session, user_id=int(user_id))
