a74599c53f59719b99183c7867d8d7a399d243f2
Kirill <bondarenkokirill150208@gmail.com>
2025-08-03 18:21:42 +0300

    Moved the routing logic to another __init__

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from sneaker_details_service.sneaker_details.routers.sneaker import router as sneaker_details_router
-
-router = APIRouter()
-
-router.include_router(sneaker_details_router)
