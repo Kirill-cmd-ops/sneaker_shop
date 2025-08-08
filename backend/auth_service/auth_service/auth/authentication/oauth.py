d928af61086d780c9ca534d818fc7a9a6f70ec9b
Kirill <bondarenkokirill150208@gmail.com>
2025-08-08 22:36:10 +0300

    GoogleOAuth2 object created

---
+++ backend/auth_service/auth_service/auth/authentication/oauth.py
@@ -0,0 +1,5 @@
+from httpx_oauth.clients.google import GoogleOAuth2
+from ..config import settings
+
+google_oauth_client = GoogleOAuth2(settings.auth_config.client_id, settings.auth_config.client_secret)
+
