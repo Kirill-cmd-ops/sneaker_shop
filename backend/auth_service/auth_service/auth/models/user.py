e3f944391d6d3eb74c7fbe358ff0e6ea59125331
Kirill <bondarenkokirill150208@gmail.com>
2025-08-08 22:43:25 +0300

    Updated properties of 2 fields in the User model + created migration | auth_service

--- backend/auth_service/auth_service/auth/models/user.py
+++ backend/auth_service/auth_service/auth/models/user.py
@@ -3,9 +3,10 @@ from typing import TYPE_CHECKING
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import String
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from auth_service.auth.types.user_id import UserIdType
+from .oauth_account import OAuthAccount
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
@@ -13,12 +14,12 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-
-
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
-    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=True)
+
+    oauth_accounts = relationship("OAuthAccount", lazy="joined")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, cls)
+        return SQLAlchemyUserDatabase(session, cls, OAuthAccount)
