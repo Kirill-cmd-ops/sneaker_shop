57eaf1c86dbef1f6775786b8218a16fdd296d809
Kirill <bondarenkokirill150208@gmail.com>
2025-08-19 20:22:20 +0300

    Based on the previous commit, the names of variables in CookieTransport have been changed | auth_service

--- backend/auth_service/auth_service/auth/authentication/transport.py
+++ backend/auth_service/auth_service/auth/authentication/transport.py
@@ -2,8 +2,8 @@ from fastapi_users.authentication import CookieTransport
 from auth_service.auth.config import settings
 
 cookie_transport = CookieTransport(
-    cookie_name=settings.cookie.cookie_name,
-    cookie_max_age=settings.cookie.cookie_max_age,
+    cookie_name=settings.cookie.jwt_cookie_name,
+    cookie_max_age=settings.cookie.jwt_cookie_max_age,
     cookie_path=settings.cookie.cookie_path,
     cookie_secure=settings.cookie.cookie_secure,    # при продакшене заменить на True
     cookie_httponly=settings.cookie.cookie_httponly,
