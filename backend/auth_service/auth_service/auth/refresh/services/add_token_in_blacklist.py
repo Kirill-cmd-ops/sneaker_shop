2cfe00b2f5855b33ac3be7c176ac5a9c922cd5ab
Kirill <bondarenkokirill150208@gmail.com>
2025-08-19 22:33:43 +0300

    Created a function to add a refresh token to the blacklist | auth_service

---
+++ backend/auth_service/auth_service/auth/refresh/services/add_token_in_blacklist.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from auth_service.auth.models import Blacklist
+
+
+async def add_to_blacklist(
+    session: AsyncSession,
+    refresh_token_id,
+):
+    blacklist = Blacklist(
+        refresh_token_id=refresh_token_id,
+        revoked_at=datetime.utcnow(),
+    )
+    session.add(blacklist)
+    await session.commit()
