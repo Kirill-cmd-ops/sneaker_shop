54caa87184baa3b9e50121d36f435098cb6139ac
Kirill <bondarenkokirill150208@gmail.com>
2025-08-21 22:17:06 +0300

    Updated dependency for getting user ID | auth_service

--- backend/auth_service/auth_service/auth/dependencies/get_current_user.py
+++ backend/auth_service/auth_service/auth/dependencies/get_current_user.py
@@ -5,4 +5,4 @@ async def get_user_by_header(user_id: str = Header(..., alias="X-User-Id")) -> i
     try:
         return int(user_id)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid x-credential-identifier header")
+        raise HTTPException(status_code=400, detail="Invalid X-User-Id Header")
