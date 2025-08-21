37c95889344fbdb5d79602c5eac2b18463533f89
Kirill <bondarenkokirill150208@gmail.com>
2025-08-21 22:16:06 +0300

    Updated dependency for getting user ID | favorite_service

--- backend/favorite_service/favorite_service/favorite/dependencies/get_current_user.py
+++ backend/favorite_service/favorite_service/favorite/dependencies/get_current_user.py
@@ -1,8 +1,8 @@
 from fastapi import Header, HTTPException
 
 
-async def get_user_by_header(user_id: str = Header(..., alias="x-credential-identifier")) -> int:
+async def get_user_by_header(user_id: str = Header(..., alias="X-User-Id")) -> int:
     try:
         return int(user_id)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid x-credential-identifier header")
+        raise HTTPException(status_code=400, detail="Invalid X-User-Id Header")
