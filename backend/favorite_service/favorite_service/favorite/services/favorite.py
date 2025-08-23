a01732658c496d219efc2d453e218cd54bba8070
Kirill <bondarenkokirill150208@gmail.com>
2025-08-23 15:17:01 +0300

    Added message with status_code=404 if user doesn't have favorite | favorite_service

--- backend/favorite_service/favorite_service/favorite/services/favorite.py
+++ backend/favorite_service/favorite_service/favorite/services/favorite.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -15,6 +16,8 @@ async def read_favorite(session: AsyncSession, user_id: int):
     result = await session.execute(stmt)
     favorite = result.scalar_one_or_none()
     if favorite is None:
-        return []
+        raise HTTPException(
+            status_code=404, detail="У данного пользователя нету избранного"
+        )
 
     return favorite.sneaker_associations
