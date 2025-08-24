fc8e9085a2986ed013df356c46053bb3aab6453e
Kirill <bondarenkokirill150208@gmail.com>
2025-08-24 23:07:04 +0300

    Added to handle check for quentity of sneaker sizes > 0 | sneaker_details_service

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/services/sneaker.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/services/sneaker.py
@@ -1,8 +1,10 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.orm import joinedload, selectinload, contains_eager
 
 from sneaker_details_service.sneaker_details.models.sneaker import Sneaker
+
+from sneaker_details_service.sneaker_details.models.sneaker_size import SneakerSizeAssociation
 
 
 async def get_sneaker_details(
@@ -11,15 +13,18 @@ async def get_sneaker_details(
 ):
     stmt = (
         select(Sneaker)
+        .join(Sneaker.size_associations)
+        .join(SneakerSizeAssociation.size)
+        .where(Sneaker.id == sneaker_id)
+        .where(SneakerSizeAssociation.quantity > 0)
         .options(
+            contains_eager(Sneaker.sizes),
             joinedload(Sneaker.brand),
-            selectinload(Sneaker.sizes),
             joinedload(Sneaker.country),
             selectinload(Sneaker.colors),
             selectinload(Sneaker.materials),
         )
-        .where(Sneaker.id == sneaker_id)
     )
     result = await session.execute(stmt)
-    sneaker = result.scalar_one_or_none()
+    sneaker = result.unique().scalar_one_or_none()
     return sneaker
