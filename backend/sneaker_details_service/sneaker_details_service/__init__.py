1e7c5ffaf6a3c111c5f3c57069c2d6c0e0d1dbd1
Kirill <bondarenkokirill150208@gmail.com>
2025-08-28 13:56:44 +0300

    Added connection of the router sneaker_sizes_router | sneaker_details_service

--- backend/sneaker_details_service/sneaker_details_service/__init__.py
+++ backend/sneaker_details_service/sneaker_details_service/__init__.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
-from sneaker_details_service.sneaker_details.routers.sneaker import router as sneaker_details_router
+from sneaker_details_service.sneaker_details.routers.sneaker import router as sneaker_router
+
+from sneaker_details_service.sneaker_details.routers.sneaker_sizes import router as sneaker_sizes_router
+
 
 router = APIRouter()
 
-router.include_router(sneaker_details_router)
+router.include_router(sneaker_router)
+router.include_router(sneaker_sizes_router)
