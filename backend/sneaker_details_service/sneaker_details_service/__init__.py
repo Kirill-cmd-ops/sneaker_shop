792c009ed03d9246ff5ab46087b2582bdc3e4502
Kirill <bondarenkokirill150208@gmail.com>
2025-08-29 14:49:55 +0300

    Color and material handle routers are connected | sneaker_details_service

--- backend/sneaker_details_service/sneaker_details_service/__init__.py
+++ backend/sneaker_details_service/sneaker_details_service/__init__.py
@@ -3,9 +3,13 @@ from fastapi import APIRouter
 from sneaker_details_service.sneaker_details.routers.sneaker import router as sneaker_router
 
 from sneaker_details_service.sneaker_details.routers.sneaker_sizes import router as sneaker_sizes_router
+from sneaker_details_service.sneaker_details.routers.sneaker_colors import router as sneaker_colors_router
+from sneaker_details_service.sneaker_details.routers.sneaker_materials import router as sneaker_materials_router
 
 
 router = APIRouter()
 
 router.include_router(sneaker_router)
 router.include_router(sneaker_sizes_router)
+router.include_router(sneaker_colors_router)
+router.include_router(sneaker_materials_router)
