f788e333ff0956f51be5a2440f7f7684f0f6620a
Kirill <bondarenkokirill150208@gmail.com>
2025-08-29 14:54:06 +0300

    Reworked the handle for deleting the connection between sneaker and size | sneaker_details_service

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/routers/sneaker_sizes.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/routers/sneaker_sizes.py
@@ -4,19 +4,26 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from sneaker_details_service.sneaker_details.schemas.sneaker_sizes import (
     SneakerSizesCreate,
-    SneakerSizesDelete,
     SneakerSizesRead,
     SneakerSizeUpdate,
 )
 
 from sneaker_details_service.sneaker_details.services.sneaker_sizes import (
     create_sneaker_sizes,
-    delete_sneaker_sizes,
     read_sneaker_sizes,
     update_sneaker_sizes,
 )
 
 from sneaker_details_service.sneaker_details.models.db_helper import db_helper
+
+from sneaker_details_service.sneaker_details.models import SneakerSizeAssociation
+from sneaker_details_service.sneaker_details.services.sneaker_association import (
+    delete_sneaker_association,
+)
+
+from sneaker_details_service.sneaker_details.schemas.sneaker_association import (
+    SneakerAssocsDelete,
+)
 
 router = APIRouter()
 
@@ -31,23 +38,29 @@ async def call_create_sneaker_sizes(
 
 
 @router.delete("/delete_sneaker_sizes/")
-async def call_delete_sneaker_sizes(
-    sneaker_sizes_delete: SneakerSizesDelete,
+async def call_delete_sneaker_association(
+    sneaker_sizes_delete: SneakerAssocsDelete,
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-    await delete_sneaker_sizes(session, sneaker_sizes_delete)
+    await delete_sneaker_association(
+        session, sneaker_sizes_delete, SneakerSizeAssociation, "size_id"
+    )
     return "Размеры товара успешно удалены"
 
 
 @router.get("/read_sneaker_sizes/", response_model=list[SneakerSizesRead])
 async def call_read_sneaker_sizes(
-        sneaker_id: int,
-        session: AsyncSession = Depends(db_helper.session_getter)
+    sneaker_id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
 ):
     sizes = await read_sneaker_sizes(session, sneaker_id)
     return sizes
 
+
 @router.patch("/update_sneaker_sizes/")
-async def call_update_sneaker_sizes(sneaker_size_update: SneakerSizeUpdate, session: AsyncSession = Depends(db_helper.session_getter)):
+async def call_update_sneaker_sizes(
+    sneaker_size_update: SneakerSizeUpdate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
     await update_sneaker_sizes(session, sneaker_size_update)
     return "Размер был изменен корректно"
