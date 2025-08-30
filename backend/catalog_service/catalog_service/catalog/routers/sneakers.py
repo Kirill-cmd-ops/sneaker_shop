932da5dac770e8d2812fa55264deec7b1b0b0edf
Kirill <bondarenkokirill150208@gmail.com>
2025-08-30 22:39:20 +0300

    feat(catalog_service): Added schemes, functions and routers for sneaker(CUD): - Create - Update - Delete

--- backend/catalog_service/catalog_service/catalog/routers/sneakers.py
+++ backend/catalog_service/catalog_service/catalog/routers/sneakers.py
@@ -3,11 +3,49 @@ from typing import Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from catalog_service.catalog.services.sneakers import get_sneakers_details
-from catalog_service.catalog.models.db_helper import db_helper
+from catalog_service.catalog.models import db_helper
 
+from catalog_service.catalog.schemas.sneaker import (
+    SneakerCreate,
+    SneakerUpdate,
+)
+from catalog_service.catalog.services.sneakers import (
+    get_sneakers_details,
+    create_sneaker,
+    delete_sneaker,
+    update_sneaker,
+)
 
 router = APIRouter()
+
+
+@router.post("/create_sneaker/")
+async def call_create_sneaker(
+    sneaker_create: SneakerCreate,
+    sneaker_quantity: int = 0,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    await create_sneaker(session, sneaker_quantity, sneaker_create)
+    return "Товар успешно создан"
+
+
+@router.delete("/delete_sneaker/")
+async def call_delete_sneaker(
+    sneaker_id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    await delete_sneaker(session, sneaker_id)
+    return "Товар успешно удален"
+
+
+@router.patch("/update_sneaker/")
+async def call_update_sneaker(
+    sneaker_id: int,
+    sneaker_update: SneakerUpdate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    await update_sneaker(session, sneaker_id, sneaker_update)
+    return "Товар успешно обновлен"
 
 
 @router.get("/sneakers/")
@@ -22,19 +60,20 @@ async def call_get_sneakers_details(
     brand_name: Optional[str] = None,
     size: Optional[float] = None,
     sort_by: Optional[str] = None,
-    order: Optional[str] = "asc"):
+    order: Optional[str] = "asc",
+):
 
     sneakers_info = await get_sneakers_details(
-    session=session,
-    page=page,
-    limit=limit,
-    name=name,
-    min_price=min_price,
-    max_price=max_price,
-    gender=gender,
-    brand_name=brand_name,
-    size=size,
-    sort_by=sort_by,
-    order=order,
+        session=session,
+        page=page,
+        limit=limit,
+        name=name,
+        min_price=min_price,
+        max_price=max_price,
+        gender=gender,
+        brand_name=brand_name,
+        size=size,
+        sort_by=sort_by,
+        order=order,
     )
     return sneakers_info
--- backend/catalog_service/catalog_service/catalog/schemas/sneaker.py
+++ backend/catalog_service/catalog_service/catalog/schemas/sneaker.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, Field
+from decimal import Decimal
+from typing import Optional
+
+from pydantic import BaseModel, Field, condecimal
 
 from catalog_service.catalog.schemas.brand import BrandRead
 from catalog_service.catalog.schemas.size import SizeRead
@@ -17,6 +20,27 @@ class SneakerStandard(BaseModel):
 class SneakerOut(SneakerStandard):
     pass
 
+
 class SneakerRead(SneakerStandard):
     brand: BrandRead
     sizes: list[SizeRead]
+
+
+class SneakerUpdate(BaseModel):
+    name: Optional[str] = Field(None, max_length=100)
+    price: Optional[Decimal] = Field(None, ge=0, max_digits=10, decimal_places=2)
+    brand_id: Optional[int] = None
+    image_url: Optional[str] = Field(None, max_length=200)
+    is_active: Optional[bool] = None
+    gender: Optional[str] = None
+
+
+class SneakerCreate(BaseModel):
+    name: str = Field(max_length=100)
+    price: condecimal(max_digits=10, decimal_places=2, gt=0)
+    brand_id: int
+    image_url: str = Field(max_length=200)
+    is_active: bool = Field(default=True)
+    gender: str = Field(default="унисекс")
+
+    size_ids: list[int] = []
