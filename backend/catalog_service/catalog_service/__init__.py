379567cc83db4995a8b0fb621bf97e6584bf74c5
Kirill <bondarenkokirill150208@gmail.com>
2025-08-31 23:00:49 +0300

    refactor(catalog_service): Removed router connection in __init__.py: sneaker_sizes_router

--- backend/catalog_service/catalog_service/__init__.py
+++ backend/catalog_service/catalog_service/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
 from catalog_service.catalog.routers.sneakers import router as catalog_router
-from catalog_service.catalog.routers.sneaker_sizes import router as sneaker_sizes_router
 
 router = APIRouter()
 
 router.include_router(catalog_router)
-router.include_router(sneaker_sizes_router)
