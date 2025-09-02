ad723df0ef3a6253baec1668a3d4fafda5053c65
Kirill <bondarenkokirill150208@gmail.com>
2025-09-02 21:50:46 +0300

    feat(sneaker_details_service): Added work with producer to lifespan: -start producer work -set producer to microservice state -stop producer work

--- backend/sneaker_details_service/sneaker_details_service/main.py
+++ backend/sneaker_details_service/sneaker_details_service/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
+from kafka.producer import start_producer, close_producer
 from sneaker_details_service.sneaker_details.config import settings
 from sneaker_details_service.sneaker_details.models import db_helper
 from sneaker_details_service.add_middleware import add_middleware
@@ -11,9 +12,12 @@ from sneaker_details_service import router as sneaker_details_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    producer = await start_producer(settings.kafka_config.kafka_bootstrap_servers)
+    app.state.kafka_producer = producer
     # startup
     yield
     # shutdown
+    await close_producer(producer)
     await db_helper.dispose()
 
 
