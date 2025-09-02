0b5c4d14b03f3a866a8158131b3e1c9965c64180
Kirill <bondarenkokirill150208@gmail.com>
2025-09-02 21:43:46 +0300

    refactor(kafka): Minimally changed the logic of the function for starting the producer: start_producer. - Added the bootstrap_servers attribute so that the producer setting does not depend on a specific microservice

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,12 +2,10 @@ import json
 
 from aiokafka import AIOKafkaProducer
 
-from auth_service.auth.config import settings
 
-
-async def start_producer():
+async def start_producer(bootstrap_servers):
     producer = AIOKafkaProducer(
-        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        bootstrap_servers=bootstrap_servers,
         key_serializer=lambda d: d.encode("utf-8"),
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
