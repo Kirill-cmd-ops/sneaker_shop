40372fa793581521e63aa6de106b8330c7855c98
Kirill <bondarenkokirill150208@gmail.com>
2025-09-02 21:57:30 +0300

    refactor(sneaker_details_service): The topic name has been changed to the value from Kafka Config - sneaker_work_topic in the event publishing functions: -send_create_sneaker_data -send_delete_sneaker_data -send_update_sneaker_data

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/kafka/producer_event/delete_sneaker_data.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/kafka/producer_event/delete_sneaker_data.py
@@ -5,7 +5,7 @@ async def send_delete_sneaker_data(producer, sneaker_id: int):
     sneaker_delete_payload = {"event_type": "sneaker_deleted", "sneaker_id": sneaker_id}
 
     await producer.send_and_wait(
-        settings.kafka_config.sneaker_topic,
+        settings.kafka_config.sneaker_work_topic,
         key=str(sneaker_id),
         value=sneaker_delete_payload,
     )
--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/kafka/producer_event/update_sneaker_data.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/kafka/producer_event/update_sneaker_data.py
@@ -15,7 +15,7 @@ async def send_update_sneaker_data(
     }
 
     await producer.send_and_wait(
-        settings.kafka_config.sneaker_topic,
+        settings.kafka_config.sneaker_work_topic,
         key=str(sneaker_id),
         value=jsonable_encoder(sneaker_update_payload),
     )
