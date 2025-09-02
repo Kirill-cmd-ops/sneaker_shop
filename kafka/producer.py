87464f4e01a8bce6bb4c48c49b0d5d3fb65423c3
Kirill <bondarenkokirill150208@gmail.com>
2025-09-02 08:37:47 +0300

    feat(kafka): Added functions for producers to send events to topics: -send_create_sneaker_sizes_data -send_delete_sneaker_sizes_data -send_update_sneaker_sizes_data

--- kafka/producer.py
+++ kafka/producer.py
@@ -4,7 +4,13 @@ from aiokafka import AIOKafkaProducer
 from fastapi.encoders import jsonable_encoder
 
 from auth_service.auth.config import settings
-from sneaker_details_service.sneaker_details.schemas import SneakerCreate, SneakerUpdate
+from catalog_service.catalog.schemas import (
+    SneakerCreate,
+    SneakerUpdate,
+    SneakerSizesCreate,
+    SneakerAssocsDelete,
+    SneakerSizeUpdate,
+)
 
 
 # TODO: разобраться с импортом, так как создание тут функций это временное решение
@@ -67,13 +73,56 @@ async def send_update_sneaker_data(
 ):
     sneaker_update_payload = {
         "event_type": "sneaker_updated",
-        "data": sneaker_update.dict(
-            exclude={"description", "country_id"}
-        ),
+        "data": sneaker_update.dict(exclude={"description", "country_id"}),
     }
 
     await producer.send_and_wait(
         settings.kafka_config.sneaker_topic,
         key=str(sneaker_id),
         value=jsonable_encoder(sneaker_update_payload),
+    )
+
+
+async def send_create_sneaker_sizes_data(
+    producer,
+    sneaker_sizes_create: SneakerSizesCreate,
+):
+    sneaker_sizes_create_payload = {
+        "event_type": "sneaker_sizes_created",
+        "data": sneaker_sizes_create.dict(),
+    }
+    await producer.send_and_wait(
+        settings.kafka_config.sneaker_topic,
+        key=str(sneaker_sizes_create.sneaker_id),
+        value=sneaker_sizes_create_payload,
+    )
+
+
+async def send_delete_sneaker_sizes_data(
+    producer,
+    sneaker_sizes_delete: SneakerAssocsDelete,
+):
+    sneaker_sizes_delete_payload = {
+        "event_type": "sneaker_sizes_deleted",
+        "data": sneaker_sizes_delete.dict(),
+    }
+    await producer.send_and_wait(
+        settings.kafka_config.sneaker_topic,
+        key=str(sneaker_sizes_delete.sneaker_id),
+        value=sneaker_sizes_delete_payload,
+    )
+
+
+async def send_update_sneaker_sizes_data(
+    producer,
+    sneaker_size_update: SneakerSizeUpdate,
+):
+    sneaker_sizes_update_payload = {
+        "event_type": "sneaker_sizes_updated",
+        "data": sneaker_size_update.dict()
+    }
+    await producer.send_and_wait(
+        settings.kafka_config.sneaker_topic,
+        key=str(sneaker_size_update.sneaker_id),
+        value=sneaker_sizes_update_payload
     )
