a181ba87ae0513c4194f48584567952a3bbf9862
Kirill <bondarenkokirill150208@gmail.com>
2025-09-04 12:21:52 +0300

    feat(auth_service): The relationship attribute and viewonly have been added to the User model such as: -role_association -roles

--- backend/auth_service/auth_service/auth/models/user.py
+++ backend/auth_service/auth_service/auth/models/user.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -8,12 +10,25 @@ from auth_service.auth.models.oauth_account import OAuthAccount
 from auth_service.auth.models.base import Base
 from auth_service.auth.models.mixins import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from .user_role import UserRoleAssociation
+    from .role import Role
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     first_name: Mapped[str] = mapped_column(String(50), nullable=True)
     last_name: Mapped[str] = mapped_column(String(50), nullable=True)
 
     oauth_accounts = relationship("OAuthAccount", lazy="joined")
+
+    role_association: Mapped[list["UserRoleAssociation"]] = relationship(
+        "UserRoleAssociation",
+        back_populates="user",
+    )
+
+    roles: Mapped[list["Role"]] = relationship(
+        secondary="user_role_association",
+        viewonly=True
+    )
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
