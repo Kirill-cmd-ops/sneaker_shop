37a4dabe1c368f093a52d3d12e7bbc0d89bbd8f5
Kirill <bondarenkokirill150208@gmail.com>
2025-09-08 21:30:13 +0300

    feat(auth_service): Added a function to get the user role: get_user_role -verification that the user has a role -returns the role if there is one

---
+++ backend/auth_service/auth_service/auth/services/get_user_role.py
@@ -0,0 +1,20 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+
+from auth_service.auth.models import db_helper, UserRoleAssociation, Role
+
+
+async def get_user_role(user_id: int):
+    async with db_helper.session_context() as session:
+        stmt = (
+            select(Role.name)
+            .join(UserRoleAssociation, Role.id == UserRoleAssociation.role_id)
+            .where(UserRoleAssociation.user_id == user_id)
+        )
+        result = await session.execute(stmt)
+        role = result.scalar_one_or_none()
+        if not role:
+            raise HTTPException(
+                status_code=404, detail="У данного пользователя нету role_id"
+            )
+        return role
