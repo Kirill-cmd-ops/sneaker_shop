1a9136a87ce795da1a281b7fa1fab2608ef9fa08
Kirill <bondarenkokirill150208@gmail.com>
2025-09-14 23:08:04 +0300

    refactor(redis_data): The file for connecting to redis has been changed: -the rigid binding of values has been removed -added arguments: +password +host +port +db and their default value

--- redis_data/connection.py
+++ redis_data/connection.py
@@ -1,18 +1,24 @@
 import redis.asyncio as aioredis
 
-from auth_service.auth.config import settings
 
-async def get_redis():
-    redis_client = aioredis.Redis(
-        host="redis",
-        port=6379,
-        db=0,
-        password=settings.redis_config.redis_password,
-        protocol=3,
-        decode_responses=True,
-    )
-    try:
-        yield redis_client
-    finally:
-        await redis_client.aclose()
+def get_redis_factory(
+    password: str,
+    host: str = "redis",
+    port: int = 6379,
+    db=0,
+):
+    async def get_redis():
+        redis_client = aioredis.Redis(
+            host=host,
+            port=port,
+            db=db,
+            password=password,
+            protocol=3,
+            decode_responses=True,
+        )
+        try:
+            yield redis_client
+        finally:
+            await redis_client.aclose()
 
+    return get_redis
