45f528cf8e0c54c396c7c1536c525d6fe2dda9f1
Kirill <bondarenkokirill150208@gmail.com>
2025-09-18 22:41:04 +0300

    chore(auth_service): Variables for configuring redis have been added to the configuration file in RedisConfig: -redis_host - configuring the redis host -redis_port - configuring the redis port (for example, 6379) -redis_db - configuring the database that we work with in redis(0-16)

--- backend/auth_service/auth_service/auth/config.py
+++ backend/auth_service/auth_service/auth/config.py
@@ -96,6 +96,9 @@ class KafkaConfig(BaseModel):
 
 class RedisConfig(BaseModel):
     redis_password: str
+    redis_host: str
+    redis_port: int
+    redis_db: int
 
 
 class Settings(BaseSettings):
