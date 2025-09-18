8e7069c9eb4f69936902cd0e9261adb52035339b
Kirill <bondarenkokirill150208@gmail.com>
2025-09-18 23:04:13 +0300

    feat(sneaker_views_redis_writer): Added a new microservice: sneaker_views_redis_writer -Its functionality is that it should record data from a kafka event in redis

---
+++ backend/sneaker_views_redis_writer/sneaker_views_redis_writer/main.py
@@ -0,0 +1,37 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from kafka.consumer import start_consumer, close_consumer
+from sneaker_views_redis_writer.sneaker_views.config import settings
+from sneaker_views_redis_writer.add_middleware import add_middleware
+from sneaker_views_redis_writer.sneaker_views.kafka_handler.sneaker_views_handler import handle_sneaker_view_to_redis
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    sneaker_views_consumer, sneaker_views_task = await start_consumer(
+        settings.kafka_config.sneaker_viewed_topic,
+        settings.kafka_config.kafka_bootstrap_servers,
+        settings.kafka_config.sneaker_views_redis_group,
+        handle_sneaker_view_to_redis,
+    )
+    yield
+    await close_consumer(sneaker_views_consumer, sneaker_views_task)
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+add_middleware(app)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
