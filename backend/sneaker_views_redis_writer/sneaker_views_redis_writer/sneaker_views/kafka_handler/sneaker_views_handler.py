899ee061038d71fcdd1da7aac25299d299ebea99
Kirill <bondarenkokirill150208@gmail.com>
2025-09-18 22:15:04 +0300

    feat(sneaker_views_redis_writer): A handler has been created that records the user's viewed products in Redis. -Each user will have their own Sorted Set in Redis. -Each Sorted Set can store up to 50 records. -If an entry that is already there gets into the Sorted set, then the TTL and score are updated.

---
+++ backend/sneaker_views_redis_writer/sneaker_views_redis_writer/sneaker_views/kafka_handler/sneaker_views_handler.py
@@ -0,0 +1,37 @@
+import time
+
+import redis.asyncio as aioredis
+from fastapi import Depends
+from starlette.requests import Request
+
+from redis_data.connection import get_redis_factory
+from sneaker_views_redis_writer.sneaker_views.config import settings
+
+
+async def handle_sneaker_view_to_redis(
+    key: str | None,
+    value: dict,
+    redis_client: aioredis.Redis = Depends(
+        get_redis_factory(
+            settings.redis_config.redis_password,
+            settings.redis_config.redis_host,
+            settings.redis_config.redis_port,
+            settings.redis_config.redis_db,
+        )
+    ),
+):
+    user_id = value.get("user_id")
+    sneaker_id = value.get("sneaker_id")
+
+    score = int(time.time())
+
+    value_payload = {
+        f"sneaker_{sneaker_id}": score,
+    }
+
+    user_views_key = f"views:{user_id}"
+    async with redis_client.pipeline() as pipe:
+        await pipe.zadd(user_views_key, value_payload)
+        await pipe.expire(user_views_key, 2592000)
+        await pipe.zremrangebyrank(user_views_key, 0, -51)
+        await pipe.execute()
