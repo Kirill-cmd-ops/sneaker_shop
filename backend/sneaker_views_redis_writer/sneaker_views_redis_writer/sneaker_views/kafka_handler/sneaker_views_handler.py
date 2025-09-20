b7c4851a99b75f5aaa439219d9f2aac63a4010c8
Kirill <bondarenkokirill150208@gmail.com>
2025-09-20 13:00:16 +0300

    fix(sneaker_views_redis_writer): -Unsuitable get_redis_factory dependency removed -Instead, the get_redis function has been added to receive the redis client

--- backend/sneaker_views_redis_writer/sneaker_views_redis_writer/sneaker_views/kafka_handler/sneaker_views_handler.py
+++ backend/sneaker_views_redis_writer/sneaker_views_redis_writer/sneaker_views/kafka_handler/sneaker_views_handler.py
@@ -1,9 +1,6 @@
 import time
 
-import redis.asyncio as aioredis
-from fastapi import Depends
-
-from redis_client.redis_connection.connection import get_redis_factory
+from redis_client.redis_connection.client import get_redis
 
 from sneaker_views_redis_writer.sneaker_views.config import settings
 
@@ -11,15 +8,14 @@ from sneaker_views_redis_writer.sneaker_views.config import settings
 async def handle_sneaker_view_to_redis(
     key: str | None,
     value: dict,
-    redis_client: aioredis.Redis = Depends(
-        get_redis_factory(
-            settings.redis_config.redis_password,
-            settings.redis_config.redis_host,
-            settings.redis_config.redis_port,
-            settings.redis_config.redis_db,
-        )
-    ),
 ):
+    redis_client = await get_redis(
+        settings.redis_config.redis_password,
+        settings.redis_config.redis_host,
+        settings.redis_config.redis_port,
+        settings.redis_config.redis_db,
+    )
+
     user_id = value.get("user_id")
     sneaker_id = value.get("sneaker_id")
 
@@ -30,6 +26,7 @@ async def handle_sneaker_view_to_redis(
     }
 
     user_views_key = f"views:{user_id}"
+
     async with redis_client.pipeline() as pipe:
         await pipe.zadd(user_views_key, value_payload)
         await pipe.expire(user_views_key, 2592000)
