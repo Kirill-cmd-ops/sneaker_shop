5bb9f586f0caa1953da671ced0b0c444576c8bc9
Kirill <bondarenkokirill150208@gmail.com>
2025-09-22 08:15:37 +0300

    fix(sneaker_details_service): Depending on get_user_by_header, we took into account that the user_id value may not be there - the header was not forwarded, then we return None

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/get_current_user.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/get_current_user.py
@@ -1,8 +1,10 @@
 from fastapi import Header, HTTPException
 
 
-async def get_user_by_header(user_id: str = Header(None, alias="X-User-Id")) -> int:
+async def get_user_by_header(user_id: str = Header(None, alias="X-User-Id")) -> int | None:
     try:
-        return int(user_id)
+        if user_id:
+            return int(user_id)
+        return None
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid X-User-Id Header")
