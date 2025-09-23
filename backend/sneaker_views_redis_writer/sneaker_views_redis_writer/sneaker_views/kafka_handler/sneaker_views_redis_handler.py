fcf43a5e869c48e21ea4bb07788312e0f3262540
Kirill <bondarenkokirill150208@gmail.com>
2025-09-23 19:08:49 +0300

    refactor(sneaker_views_redis_writer): The handle handle_sneaker_view_to_redis has been moved from the file: sneaker_views_handler to: sneaker_views_redis_handler

---
+++ backend/sneaker_views_redis_writer/sneaker_views_redis_writer/sneaker_views/kafka_handler/sneaker_views_redis_handler.py
@@ -0,0 +1,38 @@
+import time
+
+from redis_client.redis_connection.factory import get_redis_factory
+
+from sneaker_views_redis_writer.sneaker_views.config import settings
+
+
+redis_factory = get_redis_factory(
+    settings.redis_config.redis_password,
+    settings.redis_config.redis_host,
+    settings.redis_config.redis_port,
+    settings.redis_config.redis_db,
+)
+
+
+async def handle_sneaker_view_to_redis(
+    key: str | None,
+    value: dict,
+):
+
+    async for redis_client in redis_factory():
+
+        user_id = value.get("user_id")
+        sneaker_id = value.get("sneaker_id")
+
+        score = int(time.time())
+
+        value_payload = {
+            f"sneaker_{sneaker_id}": score,
+        }
+
+        user_views_key = f"views:{user_id}"
+
+        async with redis_client.pipeline() as pipe:
+            await pipe.zadd(user_views_key, value_payload)
+            await pipe.expire(user_views_key, 2592000)
+            await pipe.zremrangebyrank(user_views_key, 0, -51)
+            await pipe.execute()
