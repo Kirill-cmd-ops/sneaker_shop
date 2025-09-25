416bbd3efa93447bc78f4de154ab33ff9700fe28
Kirill <bondarenkokirill150208@gmail.com>
2025-09-25 18:26:02 +0300

    feat(sneaker_views_clickhouse_writer): Added DatabaseHelper, which contains: -engine - an engine for connecting to ClickHouse -session_factory - session factory -session_getter - outputs sessions from session_factory -session_content - also outputs sessions from session_factory, but can be used as a context manager

---
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/models/db_helper.py
@@ -0,0 +1,52 @@
+from sneaker_views_clickhouse_writer.sneaker_views.config import settings
+
+from contextlib import contextmanager
+from typing import Generator
+
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
+        self.engine: Engine = create_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: sessionmaker[Session] = sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def dispose(self) -> None:
+        self.engine.dispose()
+
+    def session_getter(self) -> Generator[Session, None, None]:
+        with self.session_factory() as session:
+            yield session
+
+    @contextmanager
+    def session_context(self) -> Generator[Session, None, None]:
+        with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.clickhouse_config.clickhouse_url),
+    echo=settings.clickhouse_config.echo,
+    echo_pool=settings.clickhouse_config.echo_pool,
+    pool_size=settings.clickhouse_config.pool_size,
+    max_overflow=settings.clickhouse_config.max_overflow,
+)
