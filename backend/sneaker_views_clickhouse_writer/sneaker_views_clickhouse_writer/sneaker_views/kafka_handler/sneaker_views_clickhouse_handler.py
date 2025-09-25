375f467b0293bd9752f94b339f44011db1a3e1ea
Kirill <bondarenkokirill150208@gmail.com>
2025-09-25 18:55:08 +0300

    refactor(sneaker_views_clickhouse_writer): The handle-handle_sneaker_view_to_clickhouse has been rewritten from pure SQL code to an approach with orm methods

--- backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
@@ -1,14 +1,6 @@
-from datetime import datetime
-
-from clickhouse.clickhouse_connection.factory import get_clickhouse_factory
-from sneaker_views_clickhouse_writer.sneaker_views.config import settings
-
-clickhouse_factory = get_clickhouse_factory(
-    user=settings.clickhouse_config.clickhouse_user,
-    password=settings.clickhouse_config.clickhouse_password,
-    host=settings.clickhouse_config.clickhouse_host,
-    port=settings.clickhouse_config.clickhouse_port,
-    secure=settings.clickhouse_config.clickhouse_secure,
+from sneaker_views_clickhouse_writer.sneaker_views.models.db_helper import db_helper
+from sneaker_views_clickhouse_writer.sneaker_views.models.sneaker_views_history import (
+    SneakerViewsHistory,
 )
 
 
@@ -16,15 +8,14 @@ async def handle_sneaker_view_to_clickhouse(
     key: str | None,
     value: dict,
 ):
-    async for clickhouse_client in clickhouse_factory():
+    with db_helper.session_context() as session:
         user_id = value.get("user_id")
         sneaker_id = value.get("sneaker_id")
 
-        clickhouse_client.execute(
-            "INSERT INTO sneaker_views_db.sneaker_views_history VALUES",
-            [{
-                "user_id": user_id,
-                "sneaker_id": sneaker_id,
-                "view_timestamp": datetime.now()
-            }]
+        sneaker_view = SneakerViewsHistory(
+            user_id=user_id,
+            sneaker_id=sneaker_id,
         )
+
+        session.add(sneaker_view)
+        session.commit()
