0c8cfbed360737281cb98ad87c343efa6037e90a
Kirill <bondarenkokirill150208@gmail.com>
2025-09-27 22:13:39 +0300

    feat(sneaker_views_clickhouse_writer): The imports of all models were made in the init package and the models were registered in __all__ so that you can specify the imports of models only before the models package

---
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/models/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "Base",
+    "db_helper",
+    "SneakerViewsHistory",
+]
+
+from .base import Base
+from .db_helper import db_helper
+from .sneaker_views_history import SneakerViewsHistory
