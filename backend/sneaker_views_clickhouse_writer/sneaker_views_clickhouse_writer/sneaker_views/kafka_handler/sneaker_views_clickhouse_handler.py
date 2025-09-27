ef5110cfdfa8c75ad0b64739e90614a434aee79d
Kirill <bondarenkokirill150208@gmail.com>
2025-09-27 22:26:09 +0300

    feat(sneaker_views_clickhouse_writer): The handle operation has been changed a bit Before: -Events are processed synchronously, overlapping the event loop, which crashes after processing one event and does not process anything else. After: -Events are processed in a thread pool, meaning each event is processed by a separate thread without closing the main thread, and everything works quickly.

--- backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
@@ -1,5 +1,7 @@
-from sneaker_views_clickhouse_writer.sneaker_views.models.db_helper import db_helper
-from sneaker_views_clickhouse_writer.sneaker_views.models.sneaker_views_history import (
+import asyncio
+
+from sneaker_views_clickhouse_writer.sneaker_views.models import (
+    db_helper,
     SneakerViewsHistory,
 )
 
@@ -8,10 +10,16 @@ async def handle_sneaker_view_to_clickhouse(
     key: str | None,
     value: dict,
 ):
-    with db_helper.session_context() as session:
-        user_id = value.get("user_id")
-        sneaker_id = value.get("sneaker_id")
+    user_id = value.get("user_id")
+    sneaker_id = value.get("sneaker_id")
 
+    await asyncio.get_event_loop().run_in_executor(
+        None, lambda: _sync_insert(user_id, sneaker_id)
+    )
+
+
+def _sync_insert(user_id: int, sneaker_id: int):
+    with db_helper.session_context() as session:
         sneaker_view = SneakerViewsHistory(
             user_id=user_id,
             sneaker_id=sneaker_id,
