8cd412611029b76b66ac175b1dff1ecda5da7f6e
Kirill <bondarenkokirill150208@gmail.com>
2025-09-30 00:09:33 +0300

    feat(user_history_service): Added the check_role_permissions dependency -Checks whether the user, or rather his role, has access to call this handle -Permissions are stored in Redis, so we make a request to Redis to get them

---
+++ backend/user_history_service/user_history_service/user_history/dependencies/check_permissions.py
@@ -0,0 +1,38 @@
+import redis.asyncio as aioredis
+from fastapi import HTTPException, Depends
+from starlette.requests import Request
+
+from user_history_service.user_history.config import settings
+from user_history_service.user_history.dependencies.get_current_user_role import get_user_role_by_header
+from redis_client.redis_connection.factory import get_redis_factory
+
+
+
+def check_role_permissions(
+    permission: str,
+):
+
+    async def checker(
+        request: Request,
+        redis_client: aioredis.Redis = Depends(
+            get_redis_factory(
+                settings.redis_config.redis_password,
+                settings.redis_config.redis_host,
+                settings.redis_config.redis_port,
+                settings.redis_config.redis_db,
+            )
+        ),
+        user_role: str = Depends(get_user_role_by_header),
+    ):
+        request.state.redis_client = redis_client
+        request.state.user_role = user_role
+
+        if not user_role:
+            raise HTTPException(status_code=401, detail="Не указан X-User-Role")
+
+        key = f"role:{user_role}"
+        has_permission = await redis_client.sismember(key, permission)
+
+        if not has_permission:
+            raise HTTPException(status_code=403, detail="У пользователя нет доступа")
+    return checker
