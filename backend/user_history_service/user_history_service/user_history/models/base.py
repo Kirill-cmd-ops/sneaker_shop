37ae22a64535def93d8b0edbce059e0a193b99e5
Kirill <bondarenkokirill150208@gmail.com>
2025-09-29 22:54:50 +0300

    feat(user_history_service): Added the Base model -All other models that are used as a structure for the table will be inherited from it. -Inheriting from the Base model, gives the tables the correct name when they are created, giving the model name from camel case to snake case

---
+++ backend/user_history_service/user_history_service/user_history/models/base.py
@@ -0,0 +1,25 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
+from user_history_service.user_history.config import settings
+from user_history_service.user_history.utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.clickhouse_config.naming_convention)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        name = camel_case_to_snake_case(cls.__name__)
+        if name.endswith("y"):
+            return f"{name[:-1]}ies"
+        return f"{name}s"
+
+    @declared_attr.directive
+    def __table_args__(cls):
+        return {
+            "clickhouse_engine": "MergeTree",
+            "clickhouse_order_by": "id" if hasattr(cls, "id") else "tuple()",
+        }
