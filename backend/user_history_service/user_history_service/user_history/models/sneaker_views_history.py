928973aead37181d60adf4c28cce34d82bfd7d5e
Kirill <bondarenkokirill150208@gmail.com>
2025-09-29 23:08:25 +0300

    feat(user_history_service): Added the SneakerViewsHistory model -Will be used in the request to get data from ClickHouse -Fields: id: UInt64, primary key user_id: Uint32 sneaker_id: UInt32 view_timestamp: DateTime, default=datetime.utcnow
    -Engine: ReplacingMergeTree with:
    -order_by by user_id and sneaker_id
    -version by view_timestamp

---
+++ backend/user_history_service/user_history_service/user_history/models/sneaker_views_history.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from user_history_service.user_history.models import Base
+from clickhouse_sqlalchemy import types as ch_types, engines
+
+
+class SneakerViewsHistory(Base):
+    id: Mapped[int] = mapped_column(ch_types.UInt64, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ch_types.UInt32)
+    sneaker_id: Mapped[int] = mapped_column(ch_types.UInt32)
+    view_timestamp: Mapped[datetime] = mapped_column(
+        ch_types.DateTime, default=datetime.utcnow
+    )
+
+    __table_args__ = (
+        engines.ReplacingMergeTree(
+            order_by=("user_id", "sneaker_id"),
+            version="view_timestamp",
+        ),
+    )
