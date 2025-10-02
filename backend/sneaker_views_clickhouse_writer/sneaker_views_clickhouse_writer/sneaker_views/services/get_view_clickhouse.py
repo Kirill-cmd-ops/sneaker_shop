0024ae847f62ec74dcc3afff27c2b0827be9382d
Kirill <bondarenkokirill150208@gmail.com>
2025-10-02 19:12:01 +0300

    feat(sneaker_views_clickhouse_writer): Added the get_sneaker_view_click house function Accepts input parameters: -user_id - user ID -sneaker_id - ID of a pair of sneakers
    Why it is needed:
    The function checks whether there is a ClickHouse entry in the sneaker_views_histories table with a specific user_id and sneaker_id.
    
    What it returns:
    -If there is a record, then one record with the largest value of the version field is returned.
    -If there is no record, it returns None.

---
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/services/get_view_clickhouse.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from clickhouse_sqlalchemy import select
+from sqlalchemy import desc
+
+from sneaker_views_clickhouse_writer.sneaker_views.models import (
+    SneakerViewsHistory,
+    db_helper,
+)
+
+
+async def get_sneaker_view_clickhouse(
+    user_id: int,
+    sneaker_id: int,
+):
+    with db_helper.session_context() as session:
+
+        stmt = (
+            select(SneakerViewsHistory)
+            .where(
+                SneakerViewsHistory.user_id == user_id,
+                SneakerViewsHistory.sneaker_id == sneaker_id,
+                SneakerViewsHistory.sign == 1,
+            )
+            .order_by(desc(SneakerViewsHistory.version))
+            .limit(1)
+        )
+
+        record_history = await asyncio.get_event_loop().run_in_executor(
+            None, lambda: session.execute(stmt).scalar_one_or_none()
+        )
+
+    return record_history
