9b26c3cfad271030bff7b93765609ad9990d912b
Kirill <bondarenkokirill150208@gmail.com>
2025-10-02 21:36:40 +0300

    feat(sneaker_views_clickhouse_writer): Added the clickhouse_insert function Accepts input parameters: -user_id - user ID -sneaker_id - ID of a pair of sneakers -sign - the field value can be -1 or 1, used to delete old records. -version - responsible for the novelty of the data, the higher the value, the newer the data. -view_timestamp - Record creation time, None by default
    Why it is needed:
    Creates an entry in the sneaker_views_histories table
    
    What returns:
    In general, nothing, add and coomit do, that's it.

---
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/services/add_data_clickhouse.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sneaker_views_clickhouse_writer.sneaker_views.models import (
+    db_helper,
+    SneakerViewsHistory,
+)
+
+
+def clickhouse_insert(
+    user_id: int,
+    sneaker_id: int,
+    sign: int,
+    version: int,
+    view_timestamp: datetime = None,
+):
+    with db_helper.session_context() as session:
+
+        if view_timestamp is None:
+            view_timestamp = datetime.utcnow()
+
+        try:
+            sneaker_view = SneakerViewsHistory(
+                user_id=user_id,
+                sneaker_id=sneaker_id,
+                view_timestamp=view_timestamp,
+                sign=sign,
+                version=version,
+            )
+
+            session.add(sneaker_view)
+            session.commit()
+        except Exception as e:
+            print(e)
