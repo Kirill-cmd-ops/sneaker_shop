a151c178149d921b39de0ce3ee192bcfd6402427
Kirill <bondarenkokirill150208@gmail.com>
2025-10-02 21:56:20 +0300

    feat(sneaker_views_clickhouse_writer): Changed handle handle_sneaker_view_to_click house The functionality used to be: She called a function that made an entry in the sneaker_views_histories table in ClickHouse with the data that was passed to her.
    Updated functionality:
    Initially, a ClickHouse request is made to the sneaker_views_histories table in order to check if there is already a record with a specific user_id and sneaker_id in it.
    
    Then there are 2 scenarios:
    
    1) The record was not in the table
    Since there is no record in the table, we can simply create it, call the clickhouse_insert function and create a record with the necessary data.
    
    2) The record ended up in the table
    First of all, an entry is created in ClickHouse with the same data as the previous entry, only the value version = version + 1 and sign = -1. Why we do this -> read the full description of the feature.
    The second step is to create an entry in ClickHouse, but with new data, that is, a new view_timestamp and the values of the required fields: sign = 1, version = version + 1.

--- backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/sneaker_views/kafka_handler/sneaker_views_clickhouse_handler.py
@@ -1,12 +1,15 @@
 import asyncio
 
-from sneaker_views_clickhouse_writer.sneaker_views.models import (
-    db_helper,
-    SneakerViewsHistory,
+from sqlalchemy.orm import Session
+
+from sneaker_views_clickhouse_writer.sneaker_views.services.add_data_clickhouse import (
+    clickhouse_insert,
+)
+from sneaker_views_clickhouse_writer.sneaker_views.services.get_view_clickhouse import (
+    get_sneaker_view_clickhouse,
 )
 
 
-# TODO: без try-except почему-то обработчик падает, обрабатывая только одно событиее
 async def handle_sneaker_view_to_clickhouse(
     key: str | None,
     value: dict,
@@ -15,19 +18,39 @@ async def handle_sneaker_view_to_clickhouse(
         user_id = value.get("user_id")
         sneaker_id = value.get("sneaker_id")
 
-        await asyncio.get_event_loop().run_in_executor(
-            None, lambda: _sync_insert(user_id, sneaker_id)
-        )
+        record = await get_sneaker_view_clickhouse(user_id, sneaker_id)
+
+        if record is None:
+            await asyncio.get_event_loop().run_in_executor(
+                None,
+                lambda: clickhouse_insert(
+                    user_id=user_id, sneaker_id=sneaker_id, sign=1, version=1
+                ),
+            )
+        else:
+            record_view_timestamp = record.view_timestamp
+            record_version = record.version
+            record_version_next = record_version + 1
+
+            await asyncio.get_event_loop().run_in_executor(
+                None,
+                lambda: clickhouse_insert(
+                    user_id=user_id,
+                    sneaker_id=sneaker_id,
+                    sign=-1,
+                    version=record_version_next,
+                    view_timestamp=record_view_timestamp,
+                ),
+            )
+
+            await asyncio.get_event_loop().run_in_executor(
+                None,
+                lambda: clickhouse_insert(
+                    user_id=user_id,
+                    sneaker_id=sneaker_id,
+                    sign=1,
+                    version=record_version_next,
+                ),
+            )
     except Exception as e:
         print(e)
-
-
-def _sync_insert(user_id: int, sneaker_id: int):
-    with db_helper.session_context() as session:
-        sneaker_view = SneakerViewsHistory(
-            user_id=user_id,
-            sneaker_id=sneaker_id,
-        )
-
-        session.add(sneaker_view)
-        session.commit()
