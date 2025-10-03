6621f94a69a4ed96ac5c7435ac35da0f68e767d7
Kirill <bondarenkokirill150208@gmail.com>
2025-10-03 08:22:09 +0300

    refactor(user_history_service): Added a custom connection to Redis for the sneaker_view_to_redis function This task will run in the background, so it will not be able to take this connection to Redis from the point, it will simply close.

--- backend/user_history_service/user_history_service/user_history/services/write_sneaker_views_redis.py
+++ backend/user_history_service/user_history_service/user_history/services/write_sneaker_views_redis.py
@@ -1,35 +1,41 @@
 import time
 
-import redis.asyncio as aioredis
 
-from user_history_service.user_history.models import SneakerViewsHistory
+from redis_client.redis_connection.factory import get_redis_factory
+from user_history_service.user_history.config import settings
 
 
-async def sneaker_view_to_redis(
-    records,
-    redis_client: aioredis.Redis,
-):
-    user_data = {}
+redis_factory = get_redis_factory(
+    settings.redis_config.redis_password,
+    settings.redis_config.redis_host,
+    settings.redis_config.redis_port,
+    settings.redis_config.redis_db,
+)
 
-    for record in records:
-        user_id = record.user_id
-        sneaker_id = record.sneaker_id
 
-        if user_id not in user_data:
-            user_data[user_id] = []
+async def sneaker_view_to_redis(records):
+    async for redis_client in redis_factory():
+        user_data = {}
 
-        user_data[user_id].append(sneaker_id)
+        for record in records:
+            user_id = record.user_id
+            sneaker_id = record.sneaker_id
 
-    async with redis_client.pipeline(transaction=True) as pipe:
-        for user_id, sneaker_ids in user_data.items():
+            if user_id not in user_data:
+                user_data[user_id] = []
 
-            user_views_key = f"views:{user_id}"
-            score = int(time.time())
+            user_data[user_id].append(sneaker_id)
 
-            value_payload = {
-                f"sneaker_{sneaker_id}": score for sneaker_id in sneaker_ids
-            }
-            await pipe.zadd(user_views_key, value_payload)
-            await pipe.expire(user_views_key, 2592000)
-            await pipe.zremrangebyrank(user_views_key, 0, -31)
-        await pipe.execute()
+        async with redis_client.pipeline(transaction=True) as pipe:
+            for user_id, sneaker_ids in user_data.items():
+
+                user_views_key = f"views:{user_id}"
+                score = int(time.time())
+
+                value_payload = {
+                    f"sneaker_{sneaker_id}": score for sneaker_id in sneaker_ids
+                }
+                await pipe.zadd(user_views_key, value_payload)
+                await pipe.expire(user_views_key, 2592000)
+                await pipe.zremrangebyrank(user_views_key, 0, -31)
+            await pipe.execute()
