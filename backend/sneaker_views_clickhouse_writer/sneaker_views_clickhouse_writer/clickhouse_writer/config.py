eac106d4c103345f2d48296fde3023531bb4f512
Kirill <bondarenkokirill150208@gmail.com>
2025-10-03 09:58:36 +0300

    refactor(sneaker_views_clickhouse_writer): Duplicate code has been removed in the configuration file

--- backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/clickhouse_writer/config.py
+++ backend/sneaker_views_clickhouse_writer/sneaker_views_clickhouse_writer/clickhouse_writer/config.py
@@ -42,9 +42,6 @@ class ClickHouseConfig(BaseModel):
     }
 
 
-ENV_DIR = Path(__file__).parent.parent.parent
-
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(str(ENV_DIR / ".env.template"), str(ENV_DIR / ".env")),
