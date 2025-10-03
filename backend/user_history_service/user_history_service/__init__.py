646eee4b9933bdb5212149ca903665715b035879
Kirill <bondarenkokirill150208@gmail.com>
2025-10-03 08:45:09 +0300

    refactor(user_history_service): The import path of the recent_sneakers_views_router router has been changed

--- backend/user_history_service/user_history_service/__init__.py
+++ backend/user_history_service/user_history_service/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from user_history_service.user_history.routers.check_history import router as recent_sneakers_views_router
+from user_history_service.user_history.routers.get_history import router as recent_sneakers_views_router
 
 router = APIRouter()
 
