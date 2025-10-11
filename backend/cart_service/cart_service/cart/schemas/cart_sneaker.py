c4a104ed672dee0ec90baf2a8c96b1bac2c3e7fb
Kirill <bondarenkokirill150208@gmail.com>
2025-10-11 09:25:15 +0300

    feat(cart_service): Added a CartSneakerDelete schema with the following fields: -sneaker_id - the ID of the sneakers -sneaker_size - the size of the sneakers

--- backend/cart_service/cart_service/cart/schemas/cart_sneaker.py
+++ backend/cart_service/cart_service/cart/schemas/cart_sneaker.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
+
 class CartSneakerCreate(BaseModel):
     sneaker_id: int
     sneaker_size: float
+
+
+class CartSneakerDelete(CartSneakerCreate):
+    pass
+
 
 class CartSneakerUpdate(BaseModel):
     sneaker_size: float
