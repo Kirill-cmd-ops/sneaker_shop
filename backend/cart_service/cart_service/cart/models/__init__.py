744d95c4982f60b89b3e6c40b16fd5a50f4e5afc
Kirill <bondarenkokirill150208@gmail.com>
2025-10-13 16:17:34 +0300

    feat(cart_service): In the init package of models, imports of other models that have been added recently have been added to __all__, these include models that store information about sneakers, their sizes and brands

--- backend/cart_service/cart_service/cart/models/__init__.py
+++ backend/cart_service/cart_service/cart/models/__init__.py
@@ -2,10 +2,18 @@ __all__ = (
     "Base",
     "Cart",
     "CartSneakerAssociation",
+    "Brand",
+    "Size",
+    "SneakerSizeAssociation",
+    "Sneaker",
     "db_helper",
 )
 
 from .base import Base
+from .brand import Brand
 from .cart import Cart
 from .cart_sneaker import CartSneakerAssociation
 from .db_helper import db_helper
+from .size import Size
+from .sneaker import Sneaker
+from .sneaker_size import SneakerSizeAssociation
