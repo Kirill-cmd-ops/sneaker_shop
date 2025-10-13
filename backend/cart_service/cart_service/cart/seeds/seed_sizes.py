c65d12869fea23424a00e2ad60f3f766e5246d6a
Kirill <bondarenkokirill150208@gmail.com>
2025-10-13 17:52:17 +0300

    feat(cart_service): Added seeds for the following tables: - brands - sizes - sneaker_sizes - sneakers

---
+++ backend/cart_service/cart_service/cart/seeds/seed_sizes.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from cart_service.cart.models import Size
+
+async def seed_sizes(db: AsyncSession):
+    sizes = [Size(eu_size=size) for size in range(15, 51)]
+    db.add_all(sizes)
+    await db.commit()
---
+++ backend/cart_service/cart_service/cart/seeds/seed_sneaker_sizes.py
@@ -0,0 +1,22 @@
+import random
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from cart_service.cart.models import SneakerSizeAssociation, Sneaker, Size
+
+
+async def seed_sneaker_sizes(db: AsyncSession):
+    sneakers = (await db.execute(select(Sneaker))).scalars().all()
+    sizes = (await db.execute(select(Size))).scalars().all()
+
+    associations = []
+
+    for sneaker in sneakers:
+        assigned_sizes = random.sample(sizes, min(3, len(sizes)))
+
+        for size in assigned_sizes:
+            quantity = random.randint(1, 10)
+            associations.append(SneakerSizeAssociation(sneaker_id=sneaker.id, size_id=size.id, quantity=quantity))
+
+    db.add_all(associations)
+    await db.flush()
+    await db.commit()
