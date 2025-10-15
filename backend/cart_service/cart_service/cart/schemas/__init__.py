82b9d0d0649ddc74f60adc5a8a666104407061ea
Kirill <bondarenkokirill150208@gmail.com>
2025-10-15 14:43:31 +0300

    feat(cart_service): Schema imports added to the init package, in __all__
    This principle of working with schemas allows you to work with them at the package level without importing them directly from the file.
    Using this approach provides greater resilience when changing import routes.

--- backend/cart_service/cart_service/cart/schemas/__init__.py
+++ backend/cart_service/cart_service/cart/schemas/__init__.py
@@ -1,6 +1,28 @@
 __all__ = (
     "CartSneakerCreate",
     "CartSneakerUpdate",
+    "SneakerOut",
+    "SneakerRead",
+    "SneakerUpdate",
+    "SneakerCreate",
+    "SneakerAssocsDelete",
+    "SneakerSizesCreate",
+    "SneakerSizeUpdate",
+    "SneakerSizesRead",
+
 )
 
 from .cart_sneaker import CartSneakerCreate, CartSneakerUpdate
+from .sneaker_association import SneakerAssocsDelete
+
+from .sneaker import (
+    SneakerOut,
+    SneakerRead,
+    SneakerUpdate,
+    SneakerCreate,
+)
+from .sneaker_sizes import (
+    SneakerSizesCreate,
+    SneakerSizeUpdate,
+    SneakerSizesRead,
+)
