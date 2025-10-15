3c43ee37da205b051c0ce6eec061f11a9573e54c
Kirill <bondarenkokirill150208@gmail.com>
2025-10-15 17:46:05 +0300

    feat(cart_service): Launching 2 new consumers
    Added the launch of two new consumers, each working on their own topic, independently of each other.
    The launch of the consumers takes place at the time of the launch of the microservice.
    The consumers process the events until we turn off the microservice.

--- backend/cart_service/cart_service/main.py
+++ backend/cart_service/cart_service/main.py
@@ -1,9 +1,13 @@
+import asyncio
+from asyncio import create_task
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
 from cart_service.cart.config import settings
+from cart_service.cart.kafka.kafka_handlers.sneaker_handler import handle_sneaker
+from cart_service.cart.kafka.kafka_handlers.sneaker_sizes_handler import handle_sneaker_sizes
 from cart_service.cart.models import db_helper
 from cart_service.add_middleware import add_middleware
 from cart_service import router as cart_router
@@ -11,16 +15,24 @@ from cart_service import router as cart_router
 from kafka.consumer import start_consumer, close_consumer
 
 from cart_service.cart.kafka.kafka_handlers.cart_handler import handle_cart
-from kafka.producer import start_producer, close_producer
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
-    cart_producer = start_producer(
+    sneaker_consumer, task_sneaker = await start_consumer(
+        settings.kafka_config.sneaker_work_topic,
         settings.kafka_config.kafka_bootstrap_servers,
+        settings.kafka_config.sneaker_group_id,
+        handle_sneaker,
     )
-    app.state.kafka_producer = cart_producer
+
+    sneaker_sizes_consumer, task_sneaker_sizes = await start_consumer(
+        settings.kafka_config.sneaker_sizes_work_topic,
+        settings.kafka_config.kafka_bootstrap_servers,
+        settings.kafka_config.sneaker_sizes_group_id,
+        handle_sneaker_sizes,
+    )
 
     cart_consumer, task_cart = await start_consumer(
         settings.kafka_config.registered_topic,
@@ -29,9 +41,12 @@ async def lifespan(app: FastAPI):
         handle_cart,
     )
     yield
-    # shutdown
-    await close_producer(cart_producer)
-    await close_consumer(cart_consumer, task_cart)
+    task1 = create_task(close_consumer(sneaker_consumer, task_sneaker))
+    task2 = create_task(close_consumer(sneaker_sizes_consumer, task_sneaker_sizes))
+    task3 = create_task(close_consumer(cart_consumer, task_cart))
+
+    await asyncio.gather(task1, task2, task3)
+
     await db_helper.dispose()
 
 
