63f8e2dc53d17a74d7dcfed9937d22164cab0216
Kirill <bondarenkokirill150208@gmail.com>
2025-10-16 10:25:17 +0300

    feat(cart_service): Adding Model imports
    In the init package of models, imports of other models that have been added recently have been added to __all__, these include models that store information about sneakers, their sizes and brands

--- backend/favorite_service/favorite_service/favorite/models/__init__.py
+++ backend/favorite_service/favorite_service/favorite/models/__init__.py
@@ -3,9 +3,17 @@ __all__ = (
     "Favorite",
     "FavoriteSneakerAssociation",
     "db_helper",
+    "Brand",
+    "Size",
+    "SneakerSizeAssociation",
+    "Sneaker",
 )
 
 from .base import Base
 from .favorite import Favorite
 from .favorite_sneaker import FavoriteSneakerAssociation
 from .db_helper import db_helper
+from .brand import Brand
+from .size import Size
+from .sneaker_size import SneakerSizeAssociation
+from .sneaker import Sneaker
