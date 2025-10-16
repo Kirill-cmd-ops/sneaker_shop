f4b1b6be6faac9dee8d642b69e20353646d37166
Kirill <bondarenkokirill150208@gmail.com>
2025-10-16 16:51:01 +0300

    feat(favorite_service): Seed files added
    Added files that fill the tables with seeds - test data.
    The data is recorded in the following tables:
    - brands
    - sizes
    - sneakers
    - sneaker_size_associations

---
+++ backend/favorite_service/favorite_service/favorite/seeds/seed_brands.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from favorite_service.favorite.models import Brand
+
+async def seed_brands(db: AsyncSession):
+    brands = [
+        Brand(name="Nike", image_url="/uploads/brands/nike.png"),
+        Brand(name="Adidas", image_url="/uploads/brands/adidas.png"),
+        Brand(name="Puma", image_url="/uploads/brands/puma.png"),
+        Brand(name="Vans", image_url="/uploads/brands/vans.png"),
+        Brand(name="Salomon", image_url="/uploads/brands/salomon.png"),
+        Brand(name="Asics", image_url="/uploads/brands/asics.png"),
+        Brand(name="New-balance", image_url="/uploads/brands/new-balance.png"),
+    ]
+    db.add_all(brands)
+    await db.flush()
+    await db.commit()
