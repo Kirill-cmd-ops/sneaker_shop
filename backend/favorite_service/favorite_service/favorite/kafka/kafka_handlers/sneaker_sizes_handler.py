64062ce4e394d902ffe9b0795df76dd5e6982022
Kirill <bondarenkokirill150208@gmail.com>
2025-10-16 20:24:25 +0300

    feat(favorite_service): Аdded kafka handler
    Added a handler for the consumer, which is responsible for working with the sneaker_size_association objects of the table when receiving an event from the producer.
    That is, it handles the following events:
    
    Creating a sneaker_size object:
    Creates an entry in the sneaker_size_association table
    
    Updating the sneaker_size object:
    Updates an entry in the sneaker_size_association table
    
    Deleting a sneaker_size object
    Deletes an entry in the sneaker_size_association table

---
+++ backend/favorite_service/favorite_service/favorite/kafka/kafka_handlers/sneaker_sizes_handler.py
@@ -0,0 +1,35 @@
+from favorite_service.favorite.models import db_helper, SneakerSizeAssociation
+from favorite_service.favorite.schemas import (
+    SneakerSizesCreate,
+    SneakerSizeUpdate,
+    SneakerAssocsDelete,
+)
+from favorite_service.favorite.services.sneaker_association import (
+    delete_sneaker_association,
+)
+from favorite_service.favorite.services.sneaker_sizes import (
+    create_sneaker_sizes,
+    update_sneaker_sizes,
+)
+
+
+async def handle_sneaker_sizes(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "sneaker_sizes_created":
+                data = value.get("data")
+                sneaker_sizes_create = SneakerSizesCreate(**data)
+                await create_sneaker_sizes(session, sneaker_sizes_create)
+            elif event_type == "sneaker_sizes_updated":
+                data = value.get("data")
+                sneaker_sizes_update = SneakerSizeUpdate(**data)
+                await update_sneaker_sizes(session, sneaker_sizes_update)
+            else:
+                data = value.get("data")
+                sneaker_assoc_delete = SneakerAssocsDelete(**data)
+                await delete_sneaker_association(
+                    session, sneaker_assoc_delete, SneakerSizeAssociation, "size_id"
+                )
+    except Exception as e:
+        print("Ошибка: ", e)
