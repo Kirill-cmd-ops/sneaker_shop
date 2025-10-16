d5f45b6b004eed693e2f60cf43ec21eb73d1efe6
Kirill <bondarenkokirill150208@gmail.com>
2025-10-16 20:24:02 +0300

    feat(favorite_service): Аdded kafka handlers
    Added a handler for the consumer, which is responsible for working with sneaker objects when receiving an event from the producer.
    That is, it handles the following events:
    
    Creating a sneaker:
    Creates an entry in the sneakers and sneaker_size_association tables
    
    Sneaker update:
    Updates an entry in the sneakers and sneaker_size_association tables
    
    Removing sneaker
    Deletes an entry in the sneakers tables and related entries in the sneaker_size_association table

---
+++ backend/favorite_service/favorite_service/favorite/kafka/kafka_handlers/sneaker_handler.py
@@ -0,0 +1,29 @@
+from favorite_service.favorite.models import db_helper
+from favorite_service.favorite.schemas import SneakerCreate, SneakerUpdate
+from favorite_service.favorite.services.sneakers import (
+    create_sneaker,
+    update_sneaker,
+    delete_sneaker,
+)
+
+
+async def handle_sneaker(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "sneaker_created":
+                data = value.get("data")
+                sneaker_create = SneakerCreate(**data)
+                await create_sneaker(session, sneaker_create)
+
+            elif event_type == "sneaker_updated":
+                data = value.get("data")
+                sneaker_id = value.get("sneaker_id")
+                sneaker_update = SneakerUpdate(**data)
+                await update_sneaker(session, sneaker_id, sneaker_update)
+
+            else:
+                sneaker_id = value.get("sneaker_id")
+                await delete_sneaker(session, sneaker_id)
+    except Exception as e:
+        print("Ошибка:", e)
