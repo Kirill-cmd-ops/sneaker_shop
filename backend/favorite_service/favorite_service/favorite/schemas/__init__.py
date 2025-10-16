9a3149986d43eadc9c3115f45a5e454d8a63ec9d
Kirill <bondarenkokirill150208@gmail.com>
2025-10-16 20:11:25 +0300

    feat(favorite_service): Schema imports added to the init package, in __all__
    This principle of working with schemas allows you to work with them at the package level without importing them directly from the file.
    Using this approach provides greater resilience when changing import routes.

--- backend/favorite_service/favorite_service/favorite/schemas/__init__.py
+++ backend/favorite_service/favorite_service/favorite/schemas/__init__.py
@@ -1,5 +1,27 @@
-__all__ = [
-    "FavoriteSneakerCreate"
-]
+__all__ = (
+    "FavoriteSneakerCreate",
+    "SneakerOut",
+    "SneakerRead",
+    "SneakerUpdate",
+    "SneakerCreate",
+    "SneakerAssocsDelete",
+    "SneakerSizesCreate",
+    "SneakerSizeUpdate",
+    "SneakerSizesRead",
+
+)
 
 from .favorite_sneaker import FavoriteSneakerCreate
+from .sneaker_association import SneakerAssocsDelete
+
+from .sneaker import (
+    SneakerOut,
+    SneakerRead,
+    SneakerUpdate,
+    SneakerCreate,
+)
+from .sneaker_sizes import (
+    SneakerSizesCreate,
+    SneakerSizeUpdate,
+    SneakerSizesRead,
+)
