1b3d443d985cef6138b587c89bd17fa4b642d146
Kirill <bondarenkokirill150208@gmail.com>
2025-10-17 00:11:34 +0300

    refactor(favorite_service): Deleting the code and calling the function
    Instead of the code, the function is now being called, to which the functionality of calculating the total amount for the user's cart has been transferred.
    
    Thus, we can now reuse this function wherever we need it.

--- backend/cart_service/cart_service/cart/routers/cart.py
+++ backend/cart_service/cart_service/cart/routers/cart.py
@@ -7,7 +7,7 @@ from cart_service.cart.models import db_helper
 from cart_service.cart.dependencies.get_current_user import get_user_by_header
 from cart_service.cart.services.cart import read_cart
 from cart_service.cart.services.check_permissions import check_role_permissions
-
+from cart_service.cart.services.total_price import calculate_total_price
 
 router = APIRouter(
     prefix=settings.api.build_path(settings.api.root, settings.api.v1.prefix),
@@ -18,7 +18,6 @@ router = APIRouter(
 @router.get(
     "/view/",
     dependencies=(Depends(check_role_permissions("cart.view")),),
-    response_model=...,
 )
 async def call_get_cart(
     user_id: int = Depends(get_user_by_header),
@@ -26,8 +25,6 @@ async def call_get_cart(
 ):
     items = await read_cart(session, user_id=user_id)
 
-    total_price = 0
-    for cart_association in items.sneaker_associations:
-        total_price += cart_association.sneaker.price * cart_association.quantity
+    total_price = calculate_total_price(items)
 
     return {"Цена корзины: ": total_price, "Кроссовки": items}
