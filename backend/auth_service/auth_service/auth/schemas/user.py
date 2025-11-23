1d8cb732aaf2e279d5b67a4c71289ce1930ce7a0
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-11-24 02:58:42 +0300

    feat(auth_service): Schema Changes
    - The model name was changed from UserCreate to UserCreateWithoutConfirm
    - UserCreate model was added, inheriting from the UserCreateWithoutConfirm model, and adding a password_confirm field with additional validation via field_validator

--- backend/auth_service/auth_service/auth/schemas/user.py
+++ backend/auth_service/auth_service/auth/schemas/user.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
+from fastapi import HTTPException
 from fastapi_users import schemas
+from pydantic import field_validator
 
 from auth_service.auth.types.user_id import UserIdType
 
@@ -10,9 +12,21 @@ class UserRead(schemas.BaseUser[UserIdType]):
     last_name: Optional[str] = None
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreateWithoutConfirm(schemas.BaseUserCreate):
     first_name: str
     last_name: str
+
+
+class UserCreate(UserCreateWithoutConfirm):
+    confirm_password: str
+
+    @field_validator("confirm_password")
+    @classmethod
+    def check_passwords(cls, confirm_password, user_model):
+        password = user_model.data.get("password")
+        if password and confirm_password != password:
+            raise HTTPException(status_code=422, detail="Пароли не совпадают")
+        return confirm_password
 
 
 class UserUpdate(schemas.BaseUserUpdate):
