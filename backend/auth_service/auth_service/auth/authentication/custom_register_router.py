7949835b6925205525fb71f3aaafae81e9afad46
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-11-24 03:06:09 +0300

    feat(auth_service): Сreate custom registration handler
    - A custom registration handle has been created
    - The pen has very similar functionality to the standard registration pen provided by FastAPI Users
    - The changes were as follows:
    The confirm_password field was deleted to avoid writing it to the database.

---
+++ backend/auth_service/auth_service/auth/authentication/custom_register_router.py
@@ -0,0 +1,79 @@
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+
+from fastapi_users import exceptions, models, schemas
+from fastapi_users.manager import BaseUserManager, UserManagerDependency
+from fastapi_users.router.common import ErrorCode, ErrorModel
+
+from auth_service.auth.schemas.user import UserCreateWithoutConfirm
+
+
+def get_register_router_custom(
+    get_user_manager: UserManagerDependency[models.UP, models.ID],
+    user_schema: type[schemas.U],
+    user_create_schema: type[schemas.UC],
+) -> APIRouter:
+    """Generate a router with the register route."""
+    router = APIRouter()
+
+    @router.post(
+        "/register",
+        response_model=user_schema,
+        status_code=status.HTTP_201_CREATED,
+        name="register:register",
+        responses={
+            status.HTTP_400_BAD_REQUEST: {
+                "model": ErrorModel,
+                "content": {
+                    "application/json": {
+                        "examples": {
+                            ErrorCode.REGISTER_USER_ALREADY_EXISTS: {
+                                "summary": "A user with this email already exists.",
+                                "value": {
+                                    "detail": ErrorCode.REGISTER_USER_ALREADY_EXISTS
+                                },
+                            },
+                            ErrorCode.REGISTER_INVALID_PASSWORD: {
+                                "summary": "Password validation failed.",
+                                "value": {
+                                    "detail": {
+                                        "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                                        "reason": "Password should be"
+                                        "at least 3 characters",
+                                    }
+                                },
+                            },
+                        }
+                    }
+                },
+            },
+        },
+    )
+    async def register(
+        request: Request,
+        user_create: user_create_schema,  # type: ignore
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    ):
+        try:
+            user_create_dict = user_create.model_dump(exclude={"confirm_password"})
+            user_create_without_confirm = UserCreateWithoutConfirm(**user_create_dict)
+
+            created_user = await user_manager.create(
+                user_create_without_confirm, safe=True, request=request
+            )
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+            )
+        except exceptions.InvalidPasswordException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                    "reason": e.reason,
+                },
+            )
+
+        return schemas.model_validate(user_schema, created_user)
+
+    return router
