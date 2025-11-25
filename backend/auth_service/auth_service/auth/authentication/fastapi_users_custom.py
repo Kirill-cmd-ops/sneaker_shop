ed9bbfc69c528ba301fc0e965b8b82d44555cdba
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-11-25 22:12:43 +0300

    feat(auth_service): Added 2 functions to FastAPIUsersCustom
    - get_verify_router
    returns the result of the get_verify_router_custom function, i.e. connects its endpoints
    
    - get_reset_password_router
    returns the result of the get_reset_password_router function, i.e. connects its endpoints

--- backend/auth_service/auth_service/auth/authentication/fastapi_users_custom.py
+++ backend/auth_service/auth_service/auth/authentication/fastapi_users_custom.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from fastapi_users import FastAPIUsers, schemas
 
 from auth_service.auth.authentication.custom_register_router import get_register_router_custom
+from auth_service.auth.authentication.custom_reset_router import get_reset_password_router_custom
+from auth_service.auth.authentication.custom_verify_router import get_verify_router_custom
 
 
 class FastAPIUsersCustom(FastAPIUsers):
@@ -11,3 +13,9 @@ class FastAPIUsersCustom(FastAPIUsers):
         return get_register_router_custom(
             self.get_user_manager, user_schema, user_create_schema
         )
+
+    def get_verify_router(self, user_schema: type[schemas.U]) -> APIRouter:
+        return get_verify_router_custom(self.get_user_manager, user_schema)
+
+    def get_reset_password_router(self) -> APIRouter:
+        return get_reset_password_router_custom(self.get_user_manager)
