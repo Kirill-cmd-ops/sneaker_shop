8513d567a66ab28028e34662d58b0cbad50ee2fb
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-03 15:00:16 +0300

    feat(auth_service): Redefining get_users_router
    - get_users_router has been redefined in FastAPIUsersCustom to use get_users_router_custom

--- backend/auth_service/auth_service/auth/authentication/fastapi_users_custom.py
+++ backend/auth_service/auth_service/auth/authentication/fastapi_users_custom.py
@@ -3,6 +3,7 @@ from fastapi_users import FastAPIUsers, schemas
 
 from auth_service.auth.authentication.custom_register_router import get_register_router_custom
 from auth_service.auth.authentication.custom_reset_router import get_reset_password_router_custom
+from auth_service.auth.authentication.custom_users_router import get_users_router_custom
 from auth_service.auth.authentication.custom_verify_router import get_verify_router_custom
 
 
@@ -19,3 +20,17 @@ class FastAPIUsersCustom(FastAPIUsers):
 
     def get_reset_password_router(self) -> APIRouter:
         return get_reset_password_router_custom(self.get_user_manager)
+
+    def get_users_router(
+        self,
+        user_schema: type[schemas.U],
+        user_update_schema: type[schemas.UU],
+        requires_verification: bool = False,
+    ) -> APIRouter:
+        return get_users_router_custom(
+            self.get_user_manager,
+            user_schema,
+            user_update_schema,
+            self.authenticator,
+            requires_verification,
+        )
