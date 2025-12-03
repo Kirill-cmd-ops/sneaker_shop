13cda5ef4befb342d580d4e8f9573f9fc85039b9
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-03 14:09:46 +0300

    chore(auth_service): Deleting the users router

--- backend/auth_service/auth_service/__init__.py
+++ backend/auth_service/auth_service/__init__.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter
 
 from auth_service.auth.authentication.routers import router as auth_router
-from auth_service.auth.authentication.users import router as users_router
 from auth_service.auth.routers.role_permissions import router as role_permissions_router
 
 
 router = APIRouter()
 
 router.include_router(auth_router)
-router.include_router(users_router)
 router.include_router(role_permissions_router)
