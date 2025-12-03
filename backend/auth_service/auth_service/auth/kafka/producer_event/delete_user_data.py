e037e01bed03a8e07b34c255a4053a0dbf877eef
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-03 13:16:51 +0300

    feat(auth_service): Added kafka event publishers
    - send_create_user_data: events for creating a new user
    - send_update_user_data: events for updating user data
    - send_delete_user_data: events for deleting a user

---
+++ backend/auth_service/auth_service/auth/kafka/producer_event/delete_user_data.py
@@ -0,0 +1,11 @@
+from auth_service.auth.config import settings
+
+
+async def send_delete_user_data(producer, user_id: int):
+    user_delete_payload = {"event_type": "user_deleted", "user_id": user_id}
+
+    await producer.send_and_wait(
+        settings.kafka_config.user_work_topic,
+        key=str(user_id),
+        value=user_delete_payload,
+    )
