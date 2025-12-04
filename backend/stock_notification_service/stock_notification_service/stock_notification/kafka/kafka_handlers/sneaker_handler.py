21d7ec9a46b1763a3543f0df0672adfd62e1377f
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-04 20:00:44 +0300

    feat(stock_notification_service): Adding an event handler from kafka
    - Handles all published events from the sneaker-work topic related to data changes in the sneakers table:
    > Record creation: create_sneaker
    > Deleting an entry: delete_sneaker
    > Record update: update_sneaker

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/kafka/kafka_handlers/sneaker_handler.py
@@ -0,0 +1,32 @@
+from stock_notification_service.stock_notification.models import db_helper
+from stock_notification_service.stock_notification.schemas import (
+    SneakerCreate,
+    SneakerUpdate,
+)
+from stock_notification_service.stock_notification.services.sneakers import (
+    create_sneaker,
+    update_sneaker,
+    delete_sneaker,
+)
+
+
+async def handle_sneaker(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "sneaker_created":
+                data = value.get("data")
+                sneaker_create = SneakerCreate.model_validate(data, strict=False)
+                await create_sneaker(session, sneaker_create)
+
+            elif event_type == "sneaker_updated":
+                data = value.get("data")
+                sneaker_id = value.get("sneaker_id")
+                sneaker_update = SneakerUpdate.model_validate(data, strict=False)
+                await update_sneaker(session, sneaker_id, sneaker_update)
+
+            else:
+                sneaker_id = value.get("sneaker_id")
+                await delete_sneaker(session, sneaker_id)
+    except Exception as e:
+        print("Ошибка:", e)
