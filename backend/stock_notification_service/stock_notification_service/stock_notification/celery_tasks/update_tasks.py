3feb0900310444f2926f1f4ab58b6121824ea1cd
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-06 11:14:29 +0300

    feat(stock_notification_service): Adding a Celery task handler
    - Configured the Celery client
    - Added the handle_update_quantity handler, which handles all update.quantity tasks

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/celery_tasks/update_tasks.py
@@ -0,0 +1,29 @@
+from celery_client.celery_connection.celery_getter import get_celery
+from message_sender.send_message import send_message
+
+celery_client = get_celery("sneaker_details_service", broker="redis://:Meteor906587@redis_queue:6379/1")
+
+
+@celery_client.task(name="update.quantity")
+def handle_update_quantity(
+    hostname: str,
+    port: int,
+    start_tls: bool,
+    username: str,
+    password: str,
+    sender_gmail: str,
+    recipient_gmail: str,
+    email_title: str,
+    body_title: str,
+):
+    send_message(
+        hostname=hostname,
+        port=port,
+        start_tls=start_tls,
+        username=username,
+        password=password,
+        sender_gmail=sender_gmail,
+        recipient_gmail=recipient_gmail,
+        email_title=email_title,
+        body_title=body_title,
+    )
