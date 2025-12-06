ed3ea62a80ca6a0d69cba620ffab2681e9edb167
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-06 20:32:50 +0300

    feat(sneaker_details_service): Adding Kafka Event Publishers
    2 event publishers have been added:
    send_create_size_data publishes an event when a size is created
    send_delete_size_data publishes an event when a size is deleted

---
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/kafka/producer_event/create_size_data.py
@@ -0,0 +1,21 @@
+from fastapi.encoders import jsonable_encoder
+
+from sneaker_details_service.sneaker_details.config import settings
+from sneaker_details_service.sneaker_details.schemas import SizeCreate
+
+
+async def send_create_size_data(
+    producer,
+    size_id: int,
+    size_create: SizeCreate,
+):
+    size_create_payload = {
+        "event_type": "size_created",
+        "data": size_create.dict(),
+    }
+
+    await producer.send_and_wait(
+        settings.kafka_config.size_work_topic,
+        key=str(size_id),
+        value=jsonable_encoder(size_create_payload),
+    )
