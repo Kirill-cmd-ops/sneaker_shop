544a5fbd6c005f5eaef36b7908157da58efadfae
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-07 17:32:03 +0300

    feat(feat_service): Implement Pydantic schemas
    Add create/update schemas for:
    - Brand (create/update)
    - Size (create)

--- backend/favorite_service/favorite_service/favorite/schemas/size.py
+++ backend/favorite_service/favorite_service/favorite/schemas/size.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel, Field
 
+from pydantic import condecimal
+
+
+class SizeCreate(BaseModel):
+    eu_size: condecimal(max_digits=3, decimal_places=1)
+
+
 class SizeRead(BaseModel):
     id: int
     eu_size: float = Field(ge=15, le=50)
