320da6d85c8b6d7e6683d359c60652cd20a07f84
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-07 16:51:19 +0300

    feat(cart_service): Added Kafka event handler
    - Added the handle_brand event handler
    - Depending on the type of event, different processing logic will be triggered:
    > brand_created -> calling the create_record coroutine
    > brand_deleted -> calling the delete_record coroutine

---
+++ backend/cart_service/cart_service/cart/kafka/kafka_handlers/brand_handler.py
@@ -0,0 +1,20 @@
+from cart_service.cart.models import db_helper, Brand
+from cart_service.cart.schemas import BrandCreate
+from cart_service.cart.services.record import create_record, delete_record
+
+
+
+async def handle_brand(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "brand_created":
+                data = value.get("data")
+                brand_create = BrandCreate(**data)
+                await create_record(session, Brand, brand_create)
+
+            elif event_type == "brand_deleted":
+                brand_id = value.get("brand_id")
+                await delete_record(session, Brand, brand_id)
+    except Exception as e:
+        print("Ошибка:", e)
