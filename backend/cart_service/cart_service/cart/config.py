63f0c1d436bc07b4e34d477a54e7ce8569a380d0
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-07 16:32:20 +0300

    build(cart_service): Updating the configuration
    The following environment variables have been added to the KafkaConfig model:
    - brand_work_topic
    - size_work_topic
    - brand_group_id
    - size_group_id

--- backend/cart_service/cart_service/cart/config.py
+++ backend/cart_service/cart_service/cart/config.py
@@ -69,8 +69,12 @@ class KafkaConfig(BaseModel):
     registered_topic: str
     sneaker_work_topic: str
     sneaker_sizes_work_topic: str
+    brand_work_topic: str
+    size_work_topic: str
     sneaker_group_id: str
     sneaker_sizes_group_id: str
+    brand_group_id: str
+    size_group_id: str
     cart_group_id: str
 
 
