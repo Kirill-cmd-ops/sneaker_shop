c3a10a4baa60b076aa9d5483d86ee9f275e43180
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-08 10:56:58 +0300

    feat(catalog_service): Added schema import paths to __all__

--- backend/catalog_service/catalog_service/catalog/schemas/__init__.py
+++ backend/catalog_service/catalog_service/catalog/schemas/__init__.py
@@ -9,6 +9,8 @@ __all__ = (
     "SneakerSizesCreate",
     "SneakerSizeUpdate",
     "SneakerSizesRead",
+    "BrandCreate",
+    "SizeCreate",
 )
 
 from .brand import BrandRead
@@ -16,3 +18,5 @@ from .size import SizeRead
 from .sneaker import SneakerOut, SneakerRead, SneakerUpdate, SneakerCreate
 from .sneaker_association import SneakerAssocsDelete
 from .sneaker_sizes import SneakerSizesCreate, SneakerSizeUpdate, SneakerSizesRead
+from .brand import BrandCreate
+from .size import SizeCreate
