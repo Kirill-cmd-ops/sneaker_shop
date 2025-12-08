6b822f0fa136b2ec20cfb51c6b14d3104d42da88
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-08 10:53:21 +0300

    feat(catalog_service): Added Kafka event handler
    - Added the handle_size event handler
    - Depending on the type of event, different processing logic will be triggered:
    > size_created -> calling the create_record coroutine
    > size_deleted -> calling the delete_record coroutine

---
+++ backend/catalog_service/catalog_service/catalog/kafka/kafka_handlers/size_handler.py
@@ -0,0 +1,20 @@
+from catalog_service.catalog.models import db_helper, Size
+from catalog_service.catalog.schemas import SizeCreate
+from catalog_service.catalog.services.record import create_record, delete_record
+
+
+
+async def handle_size(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "size_created":
+                data = value.get("data")
+                size_create = SizeCreate(**data)
+                await create_record(session, Size, size_create)
+
+            elif event_type == "size_deleted":
+                size_id = value.get("size_id")
+                await delete_record(session, Size, size_id)
+    except Exception as e:
+        print("Ошибка:", e)
