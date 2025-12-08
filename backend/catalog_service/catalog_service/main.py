ad0f0a8f45bb721ab6645b9cea3e1d153e47c05d
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-08 10:54:24 +0300

    feat(catalog_service): Added the launch of two consumers
    Added 2 consumers:
    - First consumer:
    > handles tasks from the topic that is specified in the environment variable brand_work_topic
    > uses the task handler handle_brand
    
    - Second consumer:
    > handles tasks from the topic that is specified in the environment variable size_work_topic
    > uses the task handler handle_size

--- backend/catalog_service/catalog_service/main.py
+++ backend/catalog_service/catalog_service/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from asyncio import create_task
 from contextlib import asynccontextmanager
 
@@ -5,6 +6,8 @@ import uvicorn
 from fastapi import FastAPI
 
 from catalog_service.catalog.config import settings
+from catalog_service.catalog.kafka.kafka_handlers.brand_handler import handle_brand
+from catalog_service.catalog.kafka.kafka_handlers.size_handler import handle_size
 from catalog_service.catalog.kafka.kafka_handlers.sneaker_handler import handle_sneaker
 from catalog_service.catalog.kafka.kafka_handlers.sneaker_sizes_handler import (
     handle_sneaker_sizes,
@@ -24,6 +27,20 @@ async def lifespan(app: FastAPI):
         handle_sneaker,
     )
 
+    brand_consumer, task_brand = await start_consumer(
+        settings.kafka_config.brand_work_topic,
+        settings.kafka_config.kafka_bootstrap_servers,
+        settings.kafka_config.brand_group_id,
+        handle_brand,
+    )
+
+    size_consumer, task_size = await start_consumer(
+        settings.kafka_config.size_work_topic,
+        settings.kafka_config.kafka_bootstrap_servers,
+        settings.kafka_config.size_group_id,
+        handle_size,
+    )
+
     sneaker_sizes_consumer, task_sneaker_sizes = await start_consumer(
         settings.kafka_config.sneaker_sizes_work_topic,
         settings.kafka_config.kafka_bootstrap_servers,
@@ -33,9 +50,11 @@ async def lifespan(app: FastAPI):
 
     yield
     task1 = create_task(close_consumer(sneaker_consumer, task_sneaker))
-    task2 = create_task(close_consumer(sneaker_sizes_consumer, task_sneaker_sizes))
-    await task1
-    await task2
+    task2 = create_task(close_consumer(brand_consumer, task_brand))
+    task3 = create_task(close_consumer(size_consumer, task_size))
+    task4 = create_task(close_consumer(sneaker_sizes_consumer, task_sneaker_sizes))
+
+    await asyncio.gather(task1, task2, task3, task4)
 
     await db_helper.dispose()
 
