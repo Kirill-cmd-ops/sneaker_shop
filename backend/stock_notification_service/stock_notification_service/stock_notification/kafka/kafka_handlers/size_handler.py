619400aea5f3b0c64a0c7060b14b37d9921fab98
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-08 11:11:56 +0300

    feat(stock_notification_service): Added Kafka event handler
    - Added the handle_size event handler
    - Depending on the type of event, different processing logic will be triggered:
    > size_created -> calling the create_record coroutine
    > size_deleted -> calling the delete_record coroutine

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/kafka/kafka_handlers/size_handler.py
@@ -0,0 +1,22 @@
+from stock_notification_service.stock_notification.models import db_helper, Size
+from stock_notification_service.stock_notification.schemas import SizeCreate
+from stock_notification_service.stock_notification.services.record import create_record, delete_record
+
+
+
+async def handle_size(key: str | None, value: dict):
+    try:
+        event_type = value.get("event_type")
+        async with db_helper.session_context() as session:
+            if event_type == "size_created":
+                data = value.get("data")
+                size_create = SizeCreate(**data)
+                await create_record(session, Size, size_create)
+
+            elif event_type == "size_deleted":
+                size_id = value.get("size_id")
+                await delete_record(session, Size, size_id)
+    except Exception as e:
+        print("Ошибка:", e)
+
+
