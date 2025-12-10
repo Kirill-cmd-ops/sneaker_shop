a6b7c1768e56ec82f1ebaaac67d4f0ddb99cd57f
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-10 14:23:57 +0300

    feat(sneaker_details_service): Connecting routers
    The following routers are connected:
    - brand_router
    - color_router
    - country_router
    - material_router
    - size_router

--- backend/sneaker_details_service/sneaker_details_service/__init__.py
+++ backend/sneaker_details_service/sneaker_details_service/__init__.py
@@ -6,6 +6,12 @@ from sneaker_details_service.sneaker_details.routers.sneaker_sizes import router
 from sneaker_details_service.sneaker_details.routers.sneaker_colors import router as sneaker_colors_router
 from sneaker_details_service.sneaker_details.routers.sneaker_materials import router as sneaker_materials_router
 
+from sneaker_details_service.sneaker_details.routers.brand import router as brand_router
+from sneaker_details_service.sneaker_details.routers.color import router as color_router
+from sneaker_details_service.sneaker_details.routers.country import router as country_router
+from sneaker_details_service.sneaker_details.routers.material import router as material_router
+from sneaker_details_service.sneaker_details.routers.size import router as size_router
+
 
 router = APIRouter()
 
@@ -13,3 +19,8 @@ router.include_router(sneaker_router)
 router.include_router(sneaker_sizes_router)
 router.include_router(sneaker_colors_router)
 router.include_router(sneaker_materials_router)
+router.include_router(brand_router)
+router.include_router(color_router)
+router.include_router(country_router)
+router.include_router(material_router)
+router.include_router(size_router)
