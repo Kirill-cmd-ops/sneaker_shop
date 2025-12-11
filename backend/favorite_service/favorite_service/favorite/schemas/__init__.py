4d6fa4e47a9b4396e62786be988275a533a06c19
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-11 12:28:44 +0300

    feat(favorite_service): Recycling schemas
    - The sneaker_id field in models has been removed.
    - The Sneaker model has been removed

--- backend/favorite_service/favorite_service/favorite/schemas/__init__.py
+++ backend/favorite_service/favorite_service/favorite/schemas/__init__.py
@@ -1,13 +1,10 @@
 __all__ = (
     "FavoriteSneakerCreate",
-    "SneakerOut",
-    "SneakerRead",
     "SneakerUpdate",
     "SneakerCreate",
     "SneakerAssocsDelete",
     "SneakerSizesCreate",
     "SneakerSizeUpdate",
-    "SneakerSizesRead",
     "BrandCreate",
     "SizeCreate",
 )
@@ -16,15 +13,12 @@ from .favorite_sneaker import FavoriteSneakerCreate
 from .sneaker_association import SneakerAssocsDelete
 
 from .sneaker import (
-    SneakerOut,
-    SneakerRead,
     SneakerUpdate,
     SneakerCreate,
 )
 from .sneaker_sizes import (
     SneakerSizesCreate,
     SneakerSizeUpdate,
-    SneakerSizesRead,
 )
 
 from .brand import BrandCreate
