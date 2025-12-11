19bedab3adc47199b7f32e85180bce1e3c1a6c1c
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-11 17:56:09 +0300

    feat(catalog_service): Recycling schemas
    - The sneaker_id field in models has been removed.
    - The Sneaker model has been removed

--- backend/catalog_service/catalog_service/catalog/schemas/__init__.py
+++ backend/catalog_service/catalog_service/catalog/schemas/__init__.py
@@ -1,22 +1,15 @@
 __all__ = (
-    "BrandRead",
-    "SizeRead",
-    "SneakerOut",
-    "SneakerRead",
     "SneakerUpdate",
     "SneakerCreate",
     "SneakerAssocsDelete",
     "SneakerSizesCreate",
     "SneakerSizeUpdate",
-    "SneakerSizesRead",
     "BrandCreate",
     "SizeCreate",
 )
 
-from .brand import BrandRead
-from .size import SizeRead
-from .sneaker import SneakerOut, SneakerRead, SneakerUpdate, SneakerCreate
+from .sneaker import SneakerUpdate, SneakerCreate
 from .sneaker_association import SneakerAssocsDelete
-from .sneaker_sizes import SneakerSizesCreate, SneakerSizeUpdate, SneakerSizesRead
+from .sneaker_sizes import SneakerSizesCreate, SneakerSizeUpdate
 from .brand import BrandCreate
 from .size import SizeCreate
