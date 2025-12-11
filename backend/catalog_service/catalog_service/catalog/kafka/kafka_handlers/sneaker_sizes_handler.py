8750134205b953b8017d896d24502ef3d28858a3
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-11 18:00:34 +0300

    feat(catalog_service): Updating the Kafka handle_sneaker_sizes event publisher
    - The sneaker_id parameter has been added to the coroutines
    - In the event publisher, int(key) is passed as the sneaker_id parameter for the coroutines being called

--- backend/catalog_service/catalog_service/catalog/kafka/kafka_handlers/sneaker_sizes_handler.py
+++ backend/catalog_service/catalog_service/catalog/kafka/kafka_handlers/sneaker_sizes_handler.py
@@ -20,16 +20,20 @@ async def handle_sneaker_sizes(key: str | None, value: dict):
             if event_type == "sneaker_sizes_created":
                 data = value.get("data")
                 sneaker_sizes_create = SneakerSizesCreate(**data)
-                await create_sneaker_sizes(session, sneaker_sizes_create)
+                await create_sneaker_sizes(session, int(key), sneaker_sizes_create)
             elif event_type == "sneaker_sizes_updated":
                 data = value.get("data")
                 sneaker_sizes_update = SneakerSizeUpdate(**data)
-                await update_sneaker_sizes(session, sneaker_sizes_update)
+                await update_sneaker_sizes(session, int(key), sneaker_sizes_update)
             else:
                 data = value.get("data")
                 sneaker_assoc_delete = SneakerAssocsDelete(**data)
                 await delete_sneaker_association(
-                    session, sneaker_assoc_delete, SneakerSizeAssociation, "size_id"
+                    session,
+                    int(key),
+                    sneaker_assoc_delete,
+                    SneakerSizeAssociation,
+                    "size_id",
                 )
     except Exception as e:
         print("Ошибка: ", e)
