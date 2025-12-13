88b1bda902ac28c40d27299003e2421a85879431
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-13 13:12:33 +0300

    feat(favorite_service): Adding endpoint favorite sneakers
    - Adding endpoint PUT /favorite/sneakers/{favorite_sneaker_id}
    - Updates the size_id field to one available from the catalog

--- backend/favorite_service/favorite_service/favorite/routers/favorite_sneaker.py
+++ backend/favorite_service/favorite_service/favorite/routers/favorite_sneaker.py
@@ -5,14 +5,18 @@ from favorite_service.favorite.models import (
     db_helper,
 )
 from favorite_service.favorite.schemas import FavoriteSneakerCreate
+from favorite_service.favorite.schemas.favorite_sneaker import FavoriteSneakerUpdate
 from favorite_service.favorite.services.check_favorite import check_favorite_exists
 from favorite_service.favorite.services.check_permissions import check_role_permissions
+from favorite_service.favorite.services.check_sneaker import check_sneaker_exists
 from favorite_service.favorite.services.check_sneaker_in_favorite import (
     check_sneaker_in_favorite_exists,
 )
+from favorite_service.favorite.services.check_sneaker_size import check_sneaker_size_exists
 from favorite_service.favorite.services.favorite_sneaker import (
     create_sneaker_to_favorite,
     delete_sneaker_to_favorite,
+    update_sneaker_to_favorite,
 )
 from favorite_service.favorite.dependencies.get_current_user import get_user_by_header
 from favorite_service.favorite.config import settings
@@ -37,19 +41,38 @@ async def call_create_sneaker_to_favorite(
 ):
     async with session.begin():
         favorite_id = await check_favorite_exists(session, user_id)
+        await check_sneaker_exists(session, item_create)
+        await check_sneaker_size_exists(session, item_create)
         sneaker_record = await check_sneaker_in_favorite_exists(
             session, favorite_id, item_create
         )
 
-        if not sneaker_record:
-            new_item = await create_sneaker_to_favorite(
+        if sneaker_record is None:
+            await create_sneaker_to_favorite(
                 session,
                 favorite_id=favorite_id,
                 sneaker_id=item_create.sneaker_id,
+                size_id=item_create.size_id,
             )
-            return {"status": "Элемент добавлен", "item_id": new_item.id}
+            return {"status": "Элемент добавлен"}
 
     return {"status": "Такая запись уже есть в избранном"}
+
+
+@router.put(
+    "/{favorite_sneaker_id}",
+    response_model=dict,
+)
+async def call_update_sneaker_to_favorite(
+    favorite_sneaker_id: int,
+    item_data: FavoriteSneakerUpdate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        updated_item = await update_sneaker_to_favorite(
+            session, favorite_sneaker_id=favorite_sneaker_id, size_id=item_data.size_id
+        )
+        return {"status": "Элемент обновлён", "item_id": updated_item.id}
 
 
 @router.delete(
--- backend/favorite_service/favorite_service/favorite/services/favorite_sneaker.py
+++ backend/favorite_service/favorite_service/favorite/services/favorite_sneaker.py
@@ -2,21 +2,53 @@ from fastapi import HTTPException
 from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from favorite_service.favorite.models import Favorite, FavoriteSneakerAssociation
+from favorite_service.favorite.models import (
+    Favorite,
+    FavoriteSneakerAssociation,
+    SneakerSizeAssociation,
+)
 
 
 async def create_sneaker_to_favorite(
     session: AsyncSession,
     favorite_id: int,
     sneaker_id: int,
+    size_id: int,
 ):
     new_sneaker = FavoriteSneakerAssociation(
         favorite_id=favorite_id,
         sneaker_id=sneaker_id,
+        size_id=size_id,
     )
     session.add(new_sneaker)
-
     return new_sneaker
+
+
+async def update_sneaker_to_favorite(
+    session: AsyncSession, favorite_sneaker_id: int, size_id: int
+) -> FavoriteSneakerAssociation:
+    request_get_sneaker = select(FavoriteSneakerAssociation).where(
+        FavoriteSneakerAssociation.id == favorite_sneaker_id
+    )
+    result = await session.execute(request_get_sneaker)
+    current_sneaker = result.scalar()
+    if not current_sneaker.sneaker_id:
+        raise HTTPException(status_code=404, detail="Элемент корзины не найден")
+
+    request_get_sneaker_sizes = select(SneakerSizeAssociation.size_id).where(
+        SneakerSizeAssociation.sneaker_id == current_sneaker.sneaker_id
+    )
+    result_sneaker_sizes = await session.execute(request_get_sneaker_sizes)
+    allowed_sneaker_sizes = result_sneaker_sizes.scalars().all()
+
+    if size_id in allowed_sneaker_sizes:
+        current_sneaker.size_id = size_id
+    else:
+        raise HTTPException(
+            status_code=404, detail="У данной модели кроссовок этот размер отсутствует"
+        )
+
+    return current_sneaker
 
 
 async def delete_sneaker_to_favorite(
@@ -24,14 +56,11 @@ async def delete_sneaker_to_favorite(
     user_id: int,
     favorite_sneaker_id: int,
 ):
-    stmt = (
-        delete(FavoriteSneakerAssociation)
-        .where(
-            FavoriteSneakerAssociation.id == favorite_sneaker_id,
-            FavoriteSneakerAssociation.favorite_id.in_(
-                select(Favorite.id).where(Favorite.user_id == user_id)
-            )
-        )
+    stmt = delete(FavoriteSneakerAssociation).where(
+        FavoriteSneakerAssociation.id == favorite_sneaker_id,
+        FavoriteSneakerAssociation.favorite_id.in_(
+            select(Favorite.id).where(Favorite.user_id == user_id)
+        ),
     )
     result = await session.execute(stmt)
 
