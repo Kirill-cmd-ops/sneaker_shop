b0c45eef47c6962285bcdac5d59033ef2557f8de
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-16 20:41:51 +0300

    feat(kafka): Revision of the producer
    Improvements made:
    - Idempotence
    - Retry requests
    - Batch requests
    - Type of compression

--- kafka/producer.py
+++ kafka/producer.py
@@ -8,6 +8,13 @@ async def start_producer(bootstrap_servers):
         bootstrap_servers=bootstrap_servers,
         key_serializer=lambda d: d.encode("utf-8"),
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        acks="all",
+        enable_idempotence=True,
+        retry_backoff_ms=200,
+        request_timeout_ms=30000,
+        linger_ms=20,
+        max_batch_size=65536,
+        compression_type="lz4"
     )
     await producer.start()
     return producer
