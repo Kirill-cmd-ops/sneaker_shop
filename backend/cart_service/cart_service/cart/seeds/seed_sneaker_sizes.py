f2a7d2b4a762818e556a03eaf26b5da4f4ac82ec
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-19 12:35:56 +0300

    fix(cart_service): Bulk insert fix
    - We use insert with values in execute
    - Previously, add_all was used, which did not provide full bulk

--- backend/cart_service/cart_service/cart/seeds/seed_sneaker_sizes.py
+++ backend/cart_service/cart_service/cart/seeds/seed_sneaker_sizes.py
@@ -1,12 +1,13 @@
 import random
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from cart_service.cart.models import SneakerSizeAssociation, Sneaker, Size
 
 
-async def seed_sneaker_sizes(db: AsyncSession):
-    sneakers = (await db.execute(select(Sneaker))).scalars().all()
-    sizes = (await db.execute(select(Size))).scalars().all()
+async def seed_sneaker_sizes(session: AsyncSession):
+    sneakers = (await session.execute(select(Sneaker))).scalars().all()
+    sizes = (await session.execute(select(Size))).scalars().all()
 
     associations = []
 
@@ -15,8 +16,10 @@ async def seed_sneaker_sizes(db: AsyncSession):
 
         for size in assigned_sizes:
             quantity = random.randint(1, 10)
-            associations.append(SneakerSizeAssociation(sneaker_id=sneaker.id, size_id=size.id, quantity=quantity))
+            associations.append(
+                {"sneaker_id": sneaker.id, "size_id": size.id, "quantity": quantity}
+            )
 
-    db.add_all(associations)
-    await db.flush()
-    await db.commit()
+    stmt = insert(SneakerSizeAssociation).values(associations)
+    await session.execute(stmt)
+    await session.commit()
