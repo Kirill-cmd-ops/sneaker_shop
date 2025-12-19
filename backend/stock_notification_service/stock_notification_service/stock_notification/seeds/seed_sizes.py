196c604101b62a9aeff4f9d4360383b7b3e9ac14
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-19 12:40:32 +0300

    fix(stock_notification_service): Bulk insert fix
    - We use insert with values in execute
    - Previously, add_all was used, which did not provide full bulk

--- backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_sizes.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_sizes.py
@@ -1,7 +1,10 @@
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from stock_notification_service.stock_notification.models import Size
 
-async def seed_sizes(db: AsyncSession):
-    sizes = [Size(eu_size=size) for size in range(15, 51)]
-    db.add_all(sizes)
-    await db.commit()
+
+async def seed_sizes(session: AsyncSession):
+    sizes = [{"eu_size": size} for size in range(15, 51)]
+    stmt = insert(Size).values(sizes)
+    await session.execute(stmt)
+    await session.commit()
--- backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_users.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_users.py
@@ -1,15 +1,17 @@
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from stock_notification_service.stock_notification.models import User
 
-async def seed_users(db: AsyncSession):
+
+async def seed_users(session: AsyncSession):
     users = [
-        User(email="bondarenkokirill150208@gmail.com"),
-        User(email="pubginvolker@gmail.com"),
-        User(email="snulat90@gmail.com"),
-        User(email="bondarenko15022008@gmail.com"),
-        User(email="eenn45895@gmail.com"),
-        User(email="ki15022008@gmail.com"),
+        {"email": "bondarenkokirill150208@gmail.com"},
+        {"email": "pubginvolker@gmail.com"},
+        {"email": "snulat90@gmail.com"},
+        {"email": "bondarenko15022008@gmail.com"},
+        {"email": "eenn45895@gmail.com"},
+        {"email": "ki15022008@gmail.com"},
     ]
-    db.add_all(users)
-    await db.flush()
-    await db.commit()
+    stmt = insert(User).values(users)
+    await session.execute(stmt)
+    await session.commit()
