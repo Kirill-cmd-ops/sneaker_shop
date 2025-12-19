4c06eeb3d5e826ec09e79730ed31fe8cfcb7f5ff
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-19 13:13:34 +0300

    refactor(favorite_service): Changing models
    - The IntIdPkMixin is now used instead of the id field

--- backend/favorite_service/favorite_service/favorite/models/favorite.py
+++ backend/favorite_service/favorite_service/favorite/models/favorite.py
@@ -2,13 +2,14 @@ from favorite_service.favorite.models import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
+from .mixins import IntIdPkMixin
+
 if TYPE_CHECKING:
     from .favorite_sneaker import FavoriteSneakerAssociation
     from .sneaker import Sneaker
 
 
-class Favorite(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+class Favorite(Base, IntIdPkMixin):
     user_id: Mapped[int] = mapped_column(unique=True, index=True)
 
     sneaker_associations: Mapped[list["FavoriteSneakerAssociation"]] = relationship(
--- backend/favorite_service/favorite_service/favorite/models/favorite_sneaker.py
+++ backend/favorite_service/favorite_service/favorite/models/favorite_sneaker.py
@@ -4,13 +4,14 @@ from favorite_service.favorite.models import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
+from .mixins import IntIdPkMixin
+
 if TYPE_CHECKING:
     from .favorite import Favorite
     from .sneaker import Sneaker
 
 
-class FavoriteSneakerAssociation(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+class FavoriteSneakerAssociation(Base, IntIdPkMixin):
     favorite_id: Mapped[int] = mapped_column(ForeignKey("favorites.id", ondelete="CASCADE"))
     sneaker_id: Mapped[int] = mapped_column(ForeignKey("sneakers.id", ondelete="RESTRICT"))
     size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id", ondelete="RESTRICT"))
