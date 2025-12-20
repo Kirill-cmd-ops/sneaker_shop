94d6cae6fb57cce71cce80d141440a71631d86ae
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-20 08:45:36 +0300

    perf(cart_service): Adding indexes to Foreign Model Keys
    - CartSneakerAssociation: cart_id, sneaker_id and size_id
    - SneakerSizeAssociation: sneaker_id and size_id

--- backend/cart_service/cart_service/cart/models/cart_sneaker.py
+++ backend/cart_service/cart_service/cart/models/cart_sneaker.py
@@ -11,10 +11,10 @@ if TYPE_CHECKING:
     from .sneaker import Sneaker
 
 class CartSneakerAssociation(Base, IntIdPkMixin):
-    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id", ondelete="CASCADE"))
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id", ondelete="CASCADE"), index=True)
     quantity: Mapped[int] = mapped_column(default=1)
-    sneaker_id: Mapped[int] = mapped_column(ForeignKey("sneakers.id", ondelete="RESTRICT"))
-    size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id", ondelete="RESTRICT"))
+    sneaker_id: Mapped[int] = mapped_column(ForeignKey("sneakers.id", ondelete="RESTRICT"), index=True)
+    size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id", ondelete="RESTRICT"), index=True)
 
     cart: Mapped["Cart"] = relationship(
         "Cart",
