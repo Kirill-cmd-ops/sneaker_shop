916d7a4ebee6a6f47edcce737dd02e4306339c04
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-20 13:13:51 +0300

    fix(cart_service): Modification of models
    Brand:
    - name: remove index
    - image_url: add unique
    
    Cart:
    - user_id: remove index and add not null
    
    CartSneakerAssociation:
    - cart_id: add not null
    - quantity: add not null
    - sneaker_id: add not null
    - size_id: add not null
    - added a composite unique to the fields: cart_id, sneaker_id and sneaker_id
    
    Size:
    - the check on the eu_size field has been removed
    
    Sneaker:
    - name: remove index and add unique
    - brand_id: add not null
    - image_url: add unique
    - is_active: add not null
    - gender: add not null
    
    SneakerSizeAssociation:
    - sneaker_id: add not null
    - size_id: add not null
    - quantity: add not null

--- backend/cart_service/cart_service/cart/models/cart_sneaker.py
+++ backend/cart_service/cart_service/cart/models/cart_sneaker.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 
 from cart_service.cart.models import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -11,11 +11,14 @@ if TYPE_CHECKING:
     from .sneaker import Sneaker
 
 class CartSneakerAssociation(Base, IntIdPkMixin):
-    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id", ondelete="CASCADE"), index=True)
-    quantity: Mapped[int] = mapped_column(default=1)
-    sneaker_id: Mapped[int] = mapped_column(ForeignKey("sneakers.id", ondelete="RESTRICT"), index=True)
-    size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id", ondelete="RESTRICT"), index=True)
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id", ondelete="CASCADE"), nullable=False, index=True)
+    quantity: Mapped[int] = mapped_column(nullable=False, default=1)
+    sneaker_id: Mapped[int] = mapped_column(ForeignKey("sneakers.id", ondelete="RESTRICT"), nullable=False, index=True)
+    size_id: Mapped[int] = mapped_column(ForeignKey("sizes.id", ondelete="RESTRICT"), nullable=False, index=True)
 
+    __table_args__ = (
+        UniqueConstraint("cart_id", "sneaker_id", "ç", name="uq_cart_sneaker_size")
+    )
     cart: Mapped["Cart"] = relationship(
         "Cart",
         back_populates="sneaker_associations",
