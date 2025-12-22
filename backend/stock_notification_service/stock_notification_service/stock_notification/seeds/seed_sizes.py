34fc283644865db75eef0abb6a1652b2b5b32ba4
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-22 14:11:48 +0300

    chore(stock_notification_service): Adding additional data to all models

--- backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_sizes.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/seeds/seed_sizes.py
@@ -4,7 +4,7 @@ from stock_notification_service.stock_notification.models import Size
 
 
 async def seed_sizes(session: AsyncSession):
-    sizes = [{"eu_size": size} for size in range(15, 51)]
+    sizes = [{"eu_size": size} for size in range(35, 47)]
     stmt = insert(Size).values(sizes)
     await session.execute(stmt)
     await session.commit()
