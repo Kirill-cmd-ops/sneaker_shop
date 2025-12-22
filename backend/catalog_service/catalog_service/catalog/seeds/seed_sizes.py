23508b4ed45310e210d336e0f47aec2f4c8b46b1
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-22 14:09:44 +0300

    chore(catalog_service): Adding additional data to all models

--- backend/catalog_service/catalog_service/catalog/seeds/seed_sizes.py
+++ backend/catalog_service/catalog_service/catalog/seeds/seed_sizes.py
@@ -4,7 +4,7 @@ from catalog_service.catalog.models import Size
 
 
 async def seed_sizes(session: AsyncSession):
-    sizes = [{"eu_size": size} for size in range(15, 51)]
+    sizes = [{"eu_size": size} for size in range(35, 47)]
     stmt = insert(Size).values(sizes)
     await session.execute(stmt)
     await session.commit()
