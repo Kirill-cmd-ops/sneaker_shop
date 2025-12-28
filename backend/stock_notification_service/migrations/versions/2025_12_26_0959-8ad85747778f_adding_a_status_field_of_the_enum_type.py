f2d8160e39b1c85a694971b63bcd4dd048d78ee2
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-28 18:30:26 +0300

    feat(stock_notification_service): Creating a migration
    The status field, which is an Enum, is added to the user_sneaker_subscriptions table

---
+++ backend/stock_notification_service/migrations/versions/2025_12_26_0959-8ad85747778f_adding_a_status_field_of_the_enum_type.py
@@ -0,0 +1,53 @@
+"""Adding a status field of the Enum type
+
+Revision ID: 8ad85747778f
+Revises: 263a4db61f4a
+Create Date: 2025-12-26 09:59:08.434827
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = '8ad85747778f'
+down_revision: Union[str, None] = '263a4db61f4a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("""
+        CREATE TYPE statusenum AS ENUM (
+            'ACTIVE', 
+            'INACTIVE_BY_USER', 
+            'INACTIVE_BY_SYSTEM'
+        )
+    """)
+
+    op.add_column('user_sneaker_subscriptions',
+        sa.Column('status',
+            postgresql.ENUM('ACTIVE', 'INACTIVE_BY_USER', 'INACTIVE_BY_SYSTEM',
+                          name='statusenum', create_type=False),
+            nullable=True
+        )
+    )
+
+    op.execute("""
+        UPDATE user_sneaker_subscriptions 
+        SET status = 'ACTIVE'::statusenum
+        WHERE status IS NULL
+    """)
+
+    op.alter_column('user_sneaker_subscriptions', 'status',
+        nullable=False,
+        server_default='ACTIVE'
+    )
+
+
+def downgrade() -> None:
+    op.drop_column('user_sneaker_subscriptions', 'status')
+
+    op.execute("DROP TYPE statusenum")
