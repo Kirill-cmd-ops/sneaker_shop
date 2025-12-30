2f9c77d3f2c981b364ee059269d770c14b9906b3
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2025-12-30 22:38:41 +0300

    feat(stock_notification_service): Adding the is_sneaker_active korutina
    Korutina checks the recording activity in the Sneaker model
    - If the record is not active, then an exception is called with status_code 404

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/sneakers.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/sneakers.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from stock_notification_service.stock_notification.models import Sneaker
 
 from sqlalchemy import delete
@@ -37,3 +39,15 @@ async def update_sneaker(
 
     session.add(sneaker)
     await session.commit()
+
+
+async def is_sneaker_active(
+    session: AsyncSession,
+    sneaker_id: int,
+):
+    current_sneaker = await session.get(Sneaker, sneaker_id)
+    if not current_sneaker.is_active:
+        raise HTTPException(
+            status_code=404,
+            detail="Данный товар неактивен, подписка невозможна",
+        )
