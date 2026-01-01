881e1c7077fd92e17c56a15ca601aef25c0eedb4
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-02 01:33:11 +0300

    feat(stock_notification_service): Adding coroutine to bulk is an update related to deactivating permanent subscriptions
    - deactivate_all_user_subscriptions: deactivating all user permanent subscriptions
    - deactivate_all_sneaker_subscriptions: deactivating all permanent subscriptions to this sneaker model

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/subscription/permanent/deactivate_bulk.py
@@ -0,0 +1,36 @@
+from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from stock_notification_service.stock_notification.enums import SubscriptionStatus
+from stock_notification_service.stock_notification.models import UserSneakerSubscription
+
+
+async def deactivate_all_user_subscriptions(
+    user_id: int,
+    session: AsyncSession,
+):
+    await session.execute(
+        update(UserSneakerSubscription)
+        .where(
+            UserSneakerSubscription.user_id == user_id,
+            UserSneakerSubscription.status == SubscriptionStatus.ACTIVE,
+        )
+        .values(status=SubscriptionStatus.INACTIVE_BY_USER)
+    )
+
+    return {"records was deactivate"}
+
+
+async def deactivate_all_sneaker_subscriptions(
+    session: AsyncSession,
+    sneaker_id: int,
+):
+    await session.execute(
+        update(UserSneakerSubscription)
+        .where(
+            UserSneakerSubscription.sneaker_id == sneaker_id,
+            UserSneakerSubscription.status == SubscriptionStatus.ACTIVE,
+        )
+        .values(status=SubscriptionStatus.INACTIVE_BY_SYSTEM)
+    )
+    await session.commit()
