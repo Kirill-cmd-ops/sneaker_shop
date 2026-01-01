8557cda7e9d529afbe8be6d63e959efef40be3bf
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-01 21:57:54 +0300

    feat(stock_notification_service): Adding a check for the availability of a certain size for a certain sneaker model
    - If the size is missing or active:
    > 404 - This size is missing from this model, or it is active

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/checkers.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from stock_notification_service.stock_notification.models import SneakerSizeAssociation
+
+
+async def is_size_available_for_sneaker(
+    session: AsyncSession,
+    sneaker_id: int,
+    size_id: int,
+):
+    current_sneaker_sizes = await session.scalar(
+        select(SneakerSizeAssociation).where(
+            SneakerSizeAssociation.sneaker_id == sneaker_id,
+            SneakerSizeAssociation.size_id == size_id,
+            SneakerSizeAssociation.is_active == False,
+        )
+    )
+    if not current_sneaker_sizes:
+        raise HTTPException(
+            status_code=404,
+            detail="Данный размер отсутствует у данной модели, либо же он активен",
+        )
