6ffdf79dc9b85dcb52b87edb248ff15db2b2dcce
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-01 21:52:24 +0300

    refactor(stock_notification_service): Splitting the code of the sneaker_sizes module into several modules logically: - create: create_sneaker_sizes - delete: delete_sneaker_association - update: update_sneaker_sizes

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/update.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/update.py
@@ -1,38 +1,21 @@
-from typing import Type
-
-from fastapi import HTTPException
-from sqlalchemy import select, delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from stock_notification_service.stock_notification.config import settings
+from stock_notification_service.stock_notification.enums import SubscriptionStatus
 from stock_notification_service.stock_notification.models import (
-    Base,
+    Sneaker,
     SneakerSizeAssociation,
 )
-from stock_notification_service.stock_notification.schemas import (
-    SneakerSizeUpdate,
-    SneakerSizesCreate,
-    SneakerAssocsDelete,
-)
-from stock_notification_service.stock_notification.services.subscribed_users import (
-    get_subscribed_users,
-)
+from stock_notification_service.stock_notification.schemas import SneakerSizeUpdate
+from stock_notification_service.stock_notification.services.subscription.one_time.fetch import \
+    get_sneaker_active_one_time_subscriptions
+from stock_notification_service.stock_notification.services.subscription.permanent.fetch import \
+    get_sneaker_active_subscriptions
+
 from stock_notification_service.stock_notification.celery_tasks.update_tasks import (
     handle_update_quantity,
 )
-
-
-async def create_sneaker_sizes(
-    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
-):
-    for size_data in sneaker_sizes_create.sizes:
-        sneaker_size = SneakerSizeAssociation(
-            sneaker_id=sneaker_id,
-            size_id=size_data.size_id,
-            quantity=size_data.quantity,
-        )
-        session.add(sneaker_size)
-    await session.commit()
 
 
 async def update_sneaker_sizes(
@@ -73,29 +56,3 @@ async def update_sneaker_sizes(
                 email_title="Уведомление о поступлении товара",
                 body_title="Данная модель кроссовок поступила в наличие",
             )
-
-
-async def delete_sneaker_association(
-    session: AsyncSession,
-    sneaker_id: int,
-    sneaker_assoc_delete: SneakerAssocsDelete,
-    sneaker_association_model: Type[Base],
-    field_name: str,
-):
-    """
-    Функция для одаления записи в ассоциативной таблице
-    """
-    field = getattr(sneaker_association_model, field_name)
-    stmt = (
-        delete(sneaker_association_model)
-        .where(sneaker_association_model.sneaker_id == sneaker_id)
-        .where(field.in_(sneaker_assoc_delete.assoc_ids))
-    )
-    result = await session.execute(stmt)
-
-    if result.rowcount == 0:
-        raise HTTPException(
-            status_code=404, detail="Ничего не найдено по вашим параметрам"
-        )
-
-    await session.commit()
