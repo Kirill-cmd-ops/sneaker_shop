23a1b9f36b8185354b696026765ddefd6cd26ebc
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-04 01:38:25 +0300

    refactor(sneaker_details_service): Splitting coroutines into different modules according to the logic of the task being performed
    Source modules:
    - sneaker_sizes
    
    Updated architecture:
    create module:
    - create_sneaker_sizes
    
    update module:
    - update_sneaker_sizes

---
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/services/sneaker_size/create.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from sneaker_details_service.sneaker_details.models import SneakerSizeAssociation
+from sneaker_details_service.sneaker_details.schemas import SneakerSizesCreate
+
+
+async def create_sneaker_sizes(
+    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
+):
+    for size_data in sneaker_sizes_create.sizes:
+        sneaker_size = SneakerSizeAssociation(
+            sneaker_id=sneaker_id,
+            size_id=size_data.size_id,
+            quantity=size_data.quantity,
+        )
+        session.add(sneaker_size)
+    await session.commit()
--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/services/sneaker_size/update.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/services/sneaker_size/update.py
@@ -1,25 +1,8 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from sneaker_details_service.sneaker_details.schemas import (
-    SneakerSizesCreate,
-    SneakerSizeUpdate,
-)
-
 from sneaker_details_service.sneaker_details.models import SneakerSizeAssociation
-
-
-async def create_sneaker_sizes(
-    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
-):
-    for size_data in sneaker_sizes_create.sizes:
-        sneaker_size = SneakerSizeAssociation(
-            sneaker_id=sneaker_id,
-            size_id=size_data.size_id,
-            quantity=size_data.quantity,
-        )
-        session.add(sneaker_size)
-    await session.commit()
+from sneaker_details_service.sneaker_details.schemas import SneakerSizeUpdate
 
 
 async def update_sneaker_sizes(
