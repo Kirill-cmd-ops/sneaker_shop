dc9200ccb6ca537e67fef4ee15c52b60f93e4f83
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-03 23:46:02 +0300

    refactor(catalog_service): Splitting coroutines into different modules according to the logic of the task being performed
    Source modules:
    - sneaker_sizes
    
    Updated architecture:
    create module:
    - create_sneaker_sizes
    update module:
    - update_sneaker_sizes

--- backend/catalog_service/catalog_service/catalog/services/sneaker_size/update.py
+++ backend/catalog_service/catalog_service/catalog/services/sneaker_size/update.py
@@ -1,25 +1,8 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from catalog_service.catalog.schemas import (
-    SneakerSizesCreate,
-    SneakerSizeUpdate,
-)
-
 from catalog_service.catalog.models import SneakerSizeAssociation
-
-
-async def create_sneaker_sizes(
-    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
-):
-    for size_data in sneaker_sizes_create.sizes:
-        sneaker_size = SneakerSizeAssociation(
-            sneaker_id=sneaker_id,
-            size_id=size_data.size_id,
-            quantity=size_data.quantity,
-        )
-        session.add(sneaker_size)
-    await session.commit()
+from catalog_service.catalog.schemas import SneakerSizeUpdate
 
 
 async def update_sneaker_sizes(
