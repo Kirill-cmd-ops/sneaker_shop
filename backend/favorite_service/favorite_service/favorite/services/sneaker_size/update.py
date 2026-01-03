b4e875844b8ed528589d8568515aff595510dfab
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-04 01:23:14 +0300

    refactor(favorite_service): Splitting coroutines into different modules according to the logic of the task being performed
    Source modules:
    - sneaker_sizes
    - check_sneaker_size
    
    Updated architecture:
    create module:
    - create_sneaker_sizes
    
    update module:
    - update_sneaker_sizes
    
    checkers module:
    - check_sneaker_size_exists

--- backend/favorite_service/favorite_service/favorite/services/sneaker_size/update.py
+++ backend/favorite_service/favorite_service/favorite/services/sneaker_size/update.py
@@ -1,25 +1,8 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from favorite_service.favorite.schemas import (
-    SneakerSizesCreate,
-    SneakerSizeUpdate,
-)
-
 from favorite_service.favorite.models import SneakerSizeAssociation
-
-
-async def create_sneaker_sizes(
-    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
-):
-    for size_data in sneaker_sizes_create.sizes:
-        sneaker_size = SneakerSizeAssociation(
-            sneaker_id=sneaker_id,
-            size_id=size_data.size_id,
-            quantity=size_data.quantity,
-        )
-        session.add(sneaker_size)
-    await session.commit()
+from favorite_service.favorite.schemas import SneakerSizeUpdate
 
 
 async def update_sneaker_sizes(
