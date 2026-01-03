111eed6ec4dbcf4d876d6ca02ce3d6c1d576156e
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-04 01:13:38 +0300

    refactor(favorite_service): Splitting coroutines into different modules according to the logic of the task being performed
    Source modules:
    - sneakers
    - check_sneaker
    
    Updated architecture:
    create module:
    - create_sneaker
    
    delete module:
    - delete_sneaker
    
    update module:
    - update_sneaker
    
    checkers:
    - check_sneaker_exists

---
+++ backend/favorite_service/favorite_service/favorite/services/sneaker/update.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from favorite_service.favorite.models import Sneaker
+from favorite_service.favorite.schemas import SneakerUpdate
+
+
+async def update_sneaker(
+    session: AsyncSession, sneaker_id: int, sneaker_update: SneakerUpdate
+):
+    sneaker = await session.get(Sneaker, sneaker_id)
+    update_data = sneaker_update.dict(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(sneaker, field, value)
+
+    session.add(sneaker)
+    await session.commit()
