60eea97d78edfdd8d964551bdb62d17ff377a7c8
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-05 22:33:24 +0300

    refactor(cart_service): Splitting coroutines into different modules according to the logic of the task being performed
    Source modules:
    - record
    
    Updated architecture:
    create module:
    - create_record
    
    delete module:
    - delete_record

---
+++ backend/cart_service/cart_service/cart/services/record/delete.py
@@ -0,0 +1,13 @@
+from sqlalchemy import delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def delete_record(
+        session: AsyncSession,
+        table_name,
+        record_id,
+):
+    delete_record_request = delete(table_name).where(table_name.id == record_id)
+    await session.execute(delete_record_request)
+    await session.commit()
+    return {"result": "ok"}
