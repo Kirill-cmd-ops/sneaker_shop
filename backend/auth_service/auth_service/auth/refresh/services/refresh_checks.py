ad586e8a99f671cbc7869a73e6ad4d5e711e62a0
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-06 10:05:43 +0300

    refactor(auth_service): Rewriting select queries
    - Change of approach:
    previously used approach: session.execute(select) + scalar
    the following approach is now used: session.scalar(select)
    
    - Modified files:
    auth_service/auth/refresh/services/refresh_checks.py: check_refresh_token_valid_rotation

--- backend/auth_service/auth_service/auth/refresh/services/refresh_checks.py
+++ backend/auth_service/auth_service/auth/refresh/services/refresh_checks.py
@@ -7,21 +7,29 @@ from auth_service.auth.models import RefreshToken
 from auth_service.auth.refresh.utils.encode_token import encode_refresh_token
 
 
-async def check_refresh_token_valid_rotation(session: AsyncSession, token_hash, rotate: bool = False, extra_days: int = 0):
+async def check_refresh_token_valid_rotation(
+    session: AsyncSession,
+    token_hash,
+    rotate: bool = False,
+    extra_days: int = 0,
+):
     if rotate and extra_days > 0:
-        stmt = select(RefreshToken.user_id).where(
-            RefreshToken.token_hash == token_hash,
-            RefreshToken.revoked.is_(False),
-            RefreshToken.expires_at > (datetime.utcnow() + timedelta(days=extra_days))
+        return await session.scalar(
+            select(RefreshToken.user_id).where(
+                RefreshToken.token_hash == token_hash,
+                RefreshToken.revoked.is_(False),
+                RefreshToken.expires_at
+                > (datetime.utcnow() + timedelta(days=extra_days)),
+            )
         )
     else:
-        stmt = select(RefreshToken.user_id).where(
-            RefreshToken.token_hash == token_hash,
-            RefreshToken.revoked.is_(False),
-            RefreshToken.expires_at > datetime.utcnow(),
+        return await session.scalar(
+            select(RefreshToken.user_id).where(
+                RefreshToken.token_hash == token_hash,
+                RefreshToken.revoked.is_(False),
+                RefreshToken.expires_at > datetime.utcnow(),
+            )
         )
-    result = await session.execute(stmt)
-    return result.scalar_one_or_none()
 
 
 async def check_refresh_token_valid(session: AsyncSession, refresh_token: str):
