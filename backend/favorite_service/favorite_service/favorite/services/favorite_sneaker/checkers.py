472a25c8c2ce4cdeb77b50184c3212ca0fa91d4f
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-06 10:41:57 +0300

    refactor(favorite_service): Rewriting select queries
    Change of approach:
    - previously used approach: session.execute(select) + scalar(-s)
    - the following approach is now used: session.scalar(-s)(select)
    
    Modified files:
    - favorite_service/favorite/services/favorite_sneaker/checkers.py: check_sneaker_in_favorite_exists
    - favorite_service/favorite/services/favorite_sneaker/update.py: update_sneaker_to_favorite

--- backend/favorite_service/favorite_service/favorite/services/favorite_sneaker/checkers.py
+++ backend/favorite_service/favorite_service/favorite/services/favorite_sneaker/checkers.py
@@ -10,10 +10,9 @@ async def check_sneaker_in_favorite_exists(
     favorite_id: int,
     item_create: FavoriteSneakerCreate,
 ):
-    stmt = select(FavoriteSneakerAssociation).where(
-        FavoriteSneakerAssociation.favorite_id == favorite_id,
-        FavoriteSneakerAssociation.sneaker_id == item_create.sneaker_id,
+    return await session.scalar(
+        select(FavoriteSneakerAssociation).where(
+            FavoriteSneakerAssociation.favorite_id == favorite_id,
+            FavoriteSneakerAssociation.sneaker_id == item_create.sneaker_id,
+        )
     )
-    result = await session.execute(stmt)
-    sneaker_record = result.scalar_one_or_none()
-    return sneaker_record
--- backend/favorite_service/favorite_service/favorite/services/favorite_sneaker/update.py
+++ backend/favorite_service/favorite_service/favorite/services/favorite_sneaker/update.py
@@ -15,20 +15,21 @@ async def update_sneaker_to_favorite(
     size_id: int,
     user_id: int,
 ) -> FavoriteSneakerAssociation:
-    request_get_sneaker = select(FavoriteSneakerAssociation).where(
-        FavoriteSneakerAssociation.id == favorite_sneaker_id,
-        FavoriteSneakerAssociation.favorite_id.in_(
-            select(Favorite.id).where(Favorite.user_id == user_id),
-        ),
+    current_sneaker = await session.scalar(
+        select(FavoriteSneakerAssociation).where(
+            FavoriteSneakerAssociation.id == favorite_sneaker_id,
+            FavoriteSneakerAssociation.favorite_id.in_(
+                select(Favorite.id).where(Favorite.user_id == user_id),
+            ),
+        )
     )
-    result = await session.execute(request_get_sneaker)
-    current_sneaker = result.scalar()
 
-    request_get_sneaker_sizes = select(SneakerSizeAssociation.size_id).where(
-        SneakerSizeAssociation.sneaker_id == current_sneaker.sneaker_id
+    result_sneaker_sizes = await session.scalars(
+        select(SneakerSizeAssociation.size_id).where(
+            SneakerSizeAssociation.sneaker_id == current_sneaker.sneaker_id
+        )
     )
-    result_sneaker_sizes = await session.execute(request_get_sneaker_sizes)
-    allowed_sneaker_sizes = result_sneaker_sizes.scalars().all()
+    allowed_sneaker_sizes = result_sneaker_sizes.all()
 
     if size_id in allowed_sneaker_sizes:
         current_sneaker.size_id = size_id
