9c632e7b042078370490c01fef6448715b84469f
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-08 09:31:00 +0300

    refactor(sneaker_details_service): use named arguments for coroutine calls
    Scope of the changes: sneaker_details_service/sneaker_details/dependencies

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/check_permissions.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/check_permissions.py
@@ -3,16 +3,26 @@ from fastapi import HTTPException, Depends
 from starlette.requests import Request
 
 from sneaker_details_service.sneaker_details.config import settings
-from sneaker_details_service.sneaker_details.dependencies.get_current_user_role import get_user_role_by_header
+from sneaker_details_service.sneaker_details.dependencies.get_current_user_role import (
+    get_user_role_by_header,
+)
 from redis_client.redis_connection.factory import get_redis_factory
 
 
 def check_role_permissions(
     permission: str,
 ):
+
     async def checker(
         request: Request,
-        redis_client: aioredis.Redis = Depends(get_redis_factory(settings.redis_config.redis_password)),
+        redis_client: aioredis.Redis = Depends(
+            get_redis_factory(
+                password=settings.redis_config.redis_password,
+                host=settings.redis_config.redis_host,
+                port=settings.redis_config.redis_port,
+                db=settings.redis_config.redis_db,
+            )
+        ),
         user_role: str = Depends(get_user_role_by_header),
     ):
         request.state.redis_client = redis_client
@@ -26,4 +36,5 @@ def check_role_permissions(
 
         if not has_permission:
             raise HTTPException(status_code=403, detail="У пользователя нет доступа")
+
     return checker
--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/get_current_user.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/get_current_user.py
@@ -1,7 +1,9 @@
 from fastapi import Header, HTTPException
 
 
-async def get_user_by_header(user_id: str = Header(None, alias="X-User-Id")) -> int | None:
+async def get_user_by_header(
+    user_id: str = Header(None, alias="X-User-Id")
+) -> int | None:
     try:
         if user_id:
             return int(user_id)
