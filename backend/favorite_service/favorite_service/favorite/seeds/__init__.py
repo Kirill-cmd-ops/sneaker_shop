f16dbbefe833f5efb58e130d5485e94a559a32b0
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-08 09:28:25 +0300

    refactor(favorite_service): use named arguments for coroutine calls
    Scope of the changes: favorite_service/favorite/seeds

--- backend/favorite_service/favorite_service/favorite/seeds/__init__.py
+++ backend/favorite_service/favorite_service/favorite/seeds/__init__.py
@@ -5,15 +5,16 @@ from favorite_service.favorite.seeds.seed_sizes import seed_sizes
 from favorite_service.favorite.seeds.seed_sneakers import seed_sneakers
 from favorite_service.favorite.seeds.seed_sneaker_sizes import seed_sneaker_sizes
 
+
 async def run_seeds():
     session_gen = db_helper.session_getter()
     session: AsyncSession = await anext(session_gen)
 
     try:
-        await seed_brands(session)
-        await seed_sizes(session)
-        await seed_sneakers(session)
-        await seed_sneaker_sizes(session)
+        await seed_brands(session=session)
+        await seed_sizes(session=session)
+        await seed_sneakers(session=session)
+        await seed_sneaker_sizes(session=session)
         await session.commit()
         print("All seeds completed successfully!")
     except Exception as e:
@@ -22,4 +23,3 @@ async def run_seeds():
         raise
     finally:
         await session.close()
-
