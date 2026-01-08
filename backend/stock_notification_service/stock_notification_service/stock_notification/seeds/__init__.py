03dad88956eda1aabb24f54f8aa85fad9845ed64
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-08 11:11:04 +0300

    refactor(stock_notification_service): use named arguments for coroutine calls
    Scope of the changes: stock_notification_service/stock_notification/seeds

--- backend/stock_notification_service/stock_notification_service/stock_notification/seeds/__init__.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/seeds/__init__.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from stock_notification_service.stock_notification.models import db_helper
 from stock_notification_service.stock_notification.seeds.seed_brands import seed_brands
 from stock_notification_service.stock_notification.seeds.seed_sizes import seed_sizes
-from stock_notification_service.stock_notification.seeds.seed_sneaker_sizes import seed_sneaker_sizes
+from stock_notification_service.stock_notification.seeds.seed_sneaker_sizes import (
+    seed_sneaker_sizes,
+)
 from stock_notification_service.stock_notification.seeds.seed_sneakers import (
     seed_sneakers,
 )
@@ -14,11 +16,11 @@ async def run_seeds():
     session: AsyncSession = await anext(session_gen)
 
     try:
-        await seed_brands(session)
-        await seed_sizes(session)
-        await seed_sneakers(session)
-        await seed_sneaker_sizes(session)
-        await seed_users(session)
+        await seed_brands(session=session)
+        await seed_sizes(session=session)
+        await seed_sneakers(session=session)
+        await seed_sneaker_sizes(session=session)
+        await seed_users(session=session)
         await session.commit()
         print("All seeds completed successfully!")
     except Exception as e:
