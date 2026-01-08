61247424cd7c1c281fd02d148b6cfa1cdb23632a
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-08 09:36:40 +0300

    refactor(sneaker_details_service): use named arguments for coroutine calls
    Scope of the changes: sneaker_details_service/sneaker_details/seeds

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
@@ -3,27 +3,34 @@ from sneaker_details_service.sneaker_details.models import db_helper
 from sneaker_details_service.sneaker_details.seeds.seed_brands import seed_brands
 from sneaker_details_service.sneaker_details.seeds.seed_sizes import seed_sizes
 from sneaker_details_service.sneaker_details.seeds.seed_sneakers import seed_sneakers
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_sizes import seed_sneaker_sizes
+from sneaker_details_service.sneaker_details.seeds.seed_sneaker_sizes import (
+    seed_sneaker_sizes,
+)
 from sneaker_details_service.sneaker_details.seeds.seed_countries import seed_countries
 from sneaker_details_service.sneaker_details.seeds.seed_colors import seed_colors
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_colors import seed_sneaker_colors
+from sneaker_details_service.sneaker_details.seeds.seed_sneaker_colors import (
+    seed_sneaker_colors,
+)
 from sneaker_details_service.sneaker_details.seeds.seed_materials import seed_materials
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_materials import seed_sneaker_materials
+from sneaker_details_service.sneaker_details.seeds.seed_sneaker_materials import (
+    seed_sneaker_materials,
+)
+
 
 async def run_seeds():
     session_gen = db_helper.session_getter()
     session: AsyncSession = await anext(session_gen)
 
     try:
-        await seed_brands(session)
-        await seed_sizes(session)
-        await seed_countries(session)
-        await seed_sneakers(session)
-        await seed_sneaker_sizes(session)
-        await seed_colors(session)
-        await seed_sneaker_colors(session)
-        await seed_materials(session)
-        await seed_sneaker_materials(session)
+        await seed_brands(session=session)
+        await seed_sizes(session=session)
+        await seed_countries(session=session)
+        await seed_sneakers(session=session)
+        await seed_sneaker_sizes(session=session)
+        await seed_colors(session=session)
+        await seed_sneaker_colors(session=session)
+        await seed_materials(session=session)
+        await seed_sneaker_materials(session=session)
         await session.commit()
         print("All seeds completed successfully!")
     except Exception as e:
@@ -32,4 +39,3 @@ async def run_seeds():
         raise
     finally:
         await session.close()
-
