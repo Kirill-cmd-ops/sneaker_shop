db4818b696b20f988d9d466d73ee8fdccdb6e0f6
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-09 19:59:14 +0300

    build(stock_notification_service): Added environment variables to the RedisConfig model
    - redis_host - redis startup host
    - redis_port - redis launch port
    - redis_db - the database number used in redis

--- backend/stock_notification_service/stock_notification_service/stock_notification/config.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/config.py
@@ -70,6 +70,9 @@ class KafkaConfig(BaseModel):
 
 class RedisConfig(BaseModel):
     redis_password: str
+    redis_host: str
+    redis_port: int
+    redis_db: int
 
 
 class SMTPConfig(BaseModel):
