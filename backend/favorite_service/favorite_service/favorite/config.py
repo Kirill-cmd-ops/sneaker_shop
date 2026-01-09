c0277fab4406058e81aceb7427e0a7563d7e5806
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-09 19:58:19 +0300

    build(favorite_service): Added environment variables to the RedisConfig model
    - redis_host - redis startup host
    - redis_port - redis launch port
    - redis_db - the database number used in redis

--- backend/favorite_service/favorite_service/favorite/config.py
+++ backend/favorite_service/favorite_service/favorite/config.py
@@ -80,6 +80,9 @@ class KafkaConfig(BaseModel):
 
 class RedisConfig(BaseModel):
     redis_password: str
+    redis_host: str
+    redis_port: int
+    redis_db: int
 
 
 class Settings(BaseSettings):
