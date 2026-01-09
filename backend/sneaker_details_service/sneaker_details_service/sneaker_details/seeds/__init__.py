dab33b0c92526c280cdef6b102174f65bf59a265
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-09 20:31:40 +0300

    refactor(sneaker_details_service): Renaming modules, prefix removed: seed_
    rename seed_* modules to clean names
    - seed_brands.py -> brands.py
    - seed_colors.py -> colors.py
    - seed_countries.py -> countries.py
    - seed_materials.py -> materials.py
    - seed_sizes.py -> sizes.py
    - seed_sneakers.py -> sneakers.py
    - seed_sneaker_colors.py -> sneaker_colors.py
    - seed_sneaker_materials.py -> sneaker_materials.py
    - seed_sneaker_sizes.py -> sneaker_sizes.py

--- backend/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
+++ backend/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
@@ -1,18 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sneaker_details_service.sneaker_details.models import db_helper
-from sneaker_details_service.sneaker_details.seeds.seed_brands import seed_brands
-from sneaker_details_service.sneaker_details.seeds.seed_sizes import seed_sizes
-from sneaker_details_service.sneaker_details.seeds.seed_sneakers import seed_sneakers
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_sizes import (
+from sneaker_details_service.sneaker_details.seeds.brands import seed_brands
+from sneaker_details_service.sneaker_details.seeds.sizes import seed_sizes
+from sneaker_details_service.sneaker_details.seeds.sneakers import seed_sneakers
+from sneaker_details_service.sneaker_details.seeds.sneaker_sizes import (
     seed_sneaker_sizes,
 )
-from sneaker_details_service.sneaker_details.seeds.seed_countries import seed_countries
-from sneaker_details_service.sneaker_details.seeds.seed_colors import seed_colors
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_colors import (
+from sneaker_details_service.sneaker_details.seeds.countries import seed_countries
+from sneaker_details_service.sneaker_details.seeds.colors import seed_colors
+from sneaker_details_service.sneaker_details.seeds.sneaker_colors import (
     seed_sneaker_colors,
 )
-from sneaker_details_service.sneaker_details.seeds.seed_materials import seed_materials
-from sneaker_details_service.sneaker_details.seeds.seed_sneaker_materials import (
+from sneaker_details_service.sneaker_details.seeds.materials import seed_materials
+from sneaker_details_service.sneaker_details.seeds.sneaker_materials import (
     seed_sneaker_materials,
 )
 
