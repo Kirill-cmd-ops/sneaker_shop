6cfcb5611e8bb4d355158dec9923339e275b7510
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-10 23:44:33 +0300

    feat(stock_notification_service): Adding endpoints to work with a one-time subscription
    Endpoints:
    POST /api/v1/one_time_subscriptions/ - creating a subscription
    PATCH /api/v1/one_time_subscriptions/{subscription_id}/deactivate - deactivating a specific subscription
    PATCH /api/v1/one_time_subscriptions/deactivate - deactivating all user subscriptions
    PATCH /api/v1/one_time_subscriptions/{subscription_id}/reactivate - reactivating all user subscriptions
    GET /api/v1/one_time_subscriptions/ - sampling of all active user subscriptions

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/routers/one_time_subscription.py
@@ -0,0 +1,214 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from stock_notification_service.stock_notification.config import settings
+from stock_notification_service.stock_notification.dependencies.get_current_user import (
+    get_user_by_header,
+)
+from stock_notification_service.stock_notification.models import (
+    db_helper,
+)
+from stock_notification_service.stock_notification.schemas.subscription import (
+    SubscriptionCreate,
+)
+from stock_notification_service.stock_notification.services.sneaker.checkers import (
+    check_sneaker_active_service,
+)
+from stock_notification_service.stock_notification.services.sneaker_size.checkers import (
+    is_size_available_for_sneaker,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.checkers import (
+    check_active_one_time_subscription_service,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.create import (
+    create_user_one_time_subscription_service,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.deactivate import (
+    deactivate_user_one_time_subscription_service,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.deactivate_bulk import (
+    deactivate_all_one_time_subscriptions_for_user_service,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.fetch import (
+    get_active_one_time_subscriptions_for_user_service,
+    get_inactive_one_time_subscription_for_user_service,
+)
+from stock_notification_service.stock_notification.services.subscription.one_time.reactivate import (
+    reactivate_user_one_time_subscription_service,
+    reactivate_one_time_subscription_service,
+)
+from stock_notification_service.stock_notification.services.subscription.permanent.checkers import (
+    check_active_permanent_subscription_service,
+)
+
+router = APIRouter(
+    prefix=settings.api.build_path(
+        settings.api.root,
+        settings.api.v1.one_time_prefix,
+    ),
+    tags=["One Time Subscriptions"],
+)
+
+
+@router.post("/")
+async def create_user_one_time_subscription(
+    subscription_create: SubscriptionCreate,
+    user_id: int = Depends(get_user_by_header),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        try:
+            # проверка активности sneaker
+            await check_sneaker_active_service(
+                session=session,
+                sneaker_id=subscription_create.sneaker_id,
+            )
+
+            # проверка доступности размера для sneaker и проверка неактивности размера
+            await is_size_available_for_sneaker(
+                session=session,
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+            )
+
+            # проверка активной одноразовой подписки
+            await check_active_one_time_subscription_service(
+                session=session,
+                user_id=user_id,
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+            )
+
+            # проверка активной перманентной подписки
+            await check_active_permanent_subscription_service(
+                session=session,
+                user_id=user_id,
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+            )
+
+            # реактивация деактивированной разовой подписки
+            update_subscription = await reactivate_one_time_subscription_service(
+                session=session,
+                user_id=user_id,
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+            )
+            if update_subscription:
+                return update_subscription
+
+            # создание разовой подписки
+            result = await create_user_one_time_subscription_service(
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+                user_id=user_id,
+                session=session,
+            )
+            return result
+
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=404,
+                detail="Не удалось найти требуемую модель кроссовок",
+            )
+
+
+@router.patch("/{subscription_id}/deactivate")
+async def deactivate_user_one_time_subscription(
+    subscription_id: int,
+    user_id: int = Depends(get_user_by_header),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        try:
+            return await deactivate_user_one_time_subscription_service(
+                subscription_id=subscription_id,
+                user_id=user_id,
+                session=session,
+            )
+
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=404,
+                detail="Не удалось найти требуемую модель кроссовок",
+            )
+
+
+@router.patch("/deactivate")
+async def deactivate_all_one_time_subscriptions_for_user(
+    user_id: int = Depends(get_user_by_header),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        try:
+            return deactivate_all_one_time_subscriptions_for_user_service(
+                user_id=user_id,
+                session=session,
+            )
+
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=404,
+                detail="Не удалось найти требуемуемые модели кроссовок",
+            )
+
+
+@router.patch("/{subscription_id}/reactivate")
+async def reactivate_all_one_time_subscriptions_for_user(
+    subscription_id: int,
+    user_id: int = Depends(get_user_by_header),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        inactive_subscription = (
+            await get_inactive_one_time_subscription_for_user_service(
+                session=session,
+                user_id=user_id,
+                subscription_id=subscription_id,
+            )
+        )
+
+        await check_active_one_time_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=inactive_subscription.sneaker_id,
+            size_id=inactive_subscription.size_id,
+        )
+        await check_active_permanent_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=inactive_subscription.sneaker_id,
+            size_id=inactive_subscription.size_id,
+        )
+
+        try:
+            return reactivate_user_one_time_subscription_service(
+                subscription_id=subscription_id,
+                user_id=user_id,
+                session=session,
+            )
+
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=404,
+                detail="Не удалось найти требуемую модель кроссовок",
+            )
+
+
+@router.get("/")
+async def get_active_one_time_subscriptions_for_user(
+    user_id: int = Depends(get_user_by_header),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    async with session.begin():
+        try:
+            return await get_active_one_time_subscriptions_for_user_service(
+                user_id=user_id,
+                session=session,
+            )
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=404,
+                detail="Не удалось найти требуемуемые модели кроссовок",
+            )
