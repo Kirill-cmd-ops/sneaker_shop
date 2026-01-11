dc24803567277e6c6a2eef94dafda7f0b59ab5c3
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-12 00:01:35 +0300

    refactor(catalog_service): Rename modules containing kafka event handlers and the handlers themselves for a consistent name
    catalog_service/catalog/kafka/handlers: brand_handler.py -> brands.py
    catalog_service/catalog/kafka/handlers/brands.py: handle_brand -> handle_brand_event
    
    catalog_service/catalog/kafka/handlers: size_handler.py -> sizes.py
    catalog_service/catalog/kafka/handlers/sizes.py: handle_size -> handle_size_event
    
    catalog_service/catalog/kafka/handlers: sneaker_handler.py -> sneakers.py
    catalog_service/catalog/kafka/handlers/sneakers.py: handle_sneaker -> handle_sneaker_event
    
    catalog_service/catalog/kafka/handlers: sneaker_sizes_handler.py -> sneaker_sizes.py
    catalog_service/catalog/kafka/handlers/sneaker_sizes.py: handle_sneaker_sizes -> handle_sneaker_sizes_event

--- backend/catalog_service/catalog_service/main.py
+++ backend/catalog_service/catalog_service/main.py
@@ -6,11 +6,11 @@ import uvicorn
 from fastapi import FastAPI
 
 from catalog_service.catalog.config import settings
-from catalog_service.catalog.kafka.kafka_handlers.brand_handler import handle_brand
-from catalog_service.catalog.kafka.kafka_handlers.size_handler import handle_size
-from catalog_service.catalog.kafka.kafka_handlers.sneaker_handler import handle_sneaker
-from catalog_service.catalog.kafka.kafka_handlers.sneaker_sizes_handler import (
-    handle_sneaker_sizes,
+from catalog_service.catalog.kafka.handlers.brands import handle_brand_event
+from catalog_service.catalog.kafka.handlers.sizes import handle_size_event
+from catalog_service.catalog.kafka.handlers.sneakers import handle_sneaker_event
+from catalog_service.catalog.kafka.handlers.sneaker_sizes import (
+    handle_sneaker_sizes_event,
 )
 from catalog_service.catalog.models import db_helper
 from catalog_service.add_middleware import add_middleware
@@ -24,28 +24,28 @@ async def lifespan(app: FastAPI):
         topic=settings.kafka_config.sneaker_work_topic,
         bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
         group_id=settings.kafka_config.sneaker_group_id,
-        handler=handle_sneaker,
+        handler=handle_sneaker_event,
     )
 
     brand_consumer, task_brand = await start_consumer(
         topic=settings.kafka_config.brand_work_topic,
         bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
         group_id=settings.kafka_config.brand_group_id,
-        handler=handle_brand,
+        handler=handle_brand_event,
     )
 
     size_consumer, task_size = await start_consumer(
         topic=settings.kafka_config.size_work_topic,
         bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
         group_id=settings.kafka_config.size_group_id,
-        handler=handle_size,
+        handler=handle_size_event,
     )
 
     sneaker_sizes_consumer, task_sneaker_sizes = await start_consumer(
         topic=settings.kafka_config.sneaker_sizes_work_topic,
         bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
         group_id=settings.kafka_config.sneaker_sizes_group_id,
-        handler=handle_sneaker_sizes,
+        handler=handle_sneaker_sizes_event,
     )
 
     yield
