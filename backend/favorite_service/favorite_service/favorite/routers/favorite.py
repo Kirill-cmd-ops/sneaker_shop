7187c3d05a581c0aca5a434e5696509389fce477
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-13 13:27:09 +0300

    refactor(favorite_service): Rename the endpoints to a consistent name and update the coroutine calls to the updated ones
    favorite_service/favorite/routers/favorite.py:
     - call_get_favorite -> get_favorite
     - call_delete_favorite -> delete_favorite
    
    favorite_service/favorite/routers/favorite_sneaker.py:
     - call_create_sneaker_to_favorite -> create_sneaker_to_favorite
     - call_update_sneaker_to_favorite -> update_sneaker_in_favorite
     - call_delete_sneaker_to_favorite -> delete_sneaker_from_favorite

--- backend/favorite_service/favorite_service/favorite/routers/favorite.py
+++ backend/favorite_service/favorite_service/favorite/routers/favorite.py
@@ -4,12 +4,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from favorite_service.favorite.config import settings
 
 from favorite_service.favorite.models import db_helper
-from favorite_service.favorite.dependencies.get_current_user import get_user_by_header
-from favorite_service.favorite.dependencies.check_permissions import (
+from favorite_service.favorite.dependencies.user_id import get_current_user_id
+from favorite_service.favorite.dependencies.permissions import (
     check_role_permissions,
 )
-from favorite_service.favorite.services.favorite.delete import delete_favorite
-from favorite_service.favorite.services.favorite.fetch import read_favorite
+from favorite_service.favorite.services.favorite.delete import delete_favorite_service
+from favorite_service.favorite.services.favorite.fetch import get_favorite_service
 
 router = APIRouter(
     prefix=settings.api.build_path(settings.api.root, settings.api.v1.prefix),
@@ -21,12 +21,12 @@ router = APIRouter(
     "/",
     dependencies=(Depends(check_role_permissions("favorite.view")),),
 )
-async def call_get_favorite(
-    user_id: int = Depends(get_user_by_header),
+async def get_favorite(
+    user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     async with session.begin():
-        return await read_favorite(
+        return await get_favorite_service(
             session=session,
             user_id=user_id,
         )
@@ -35,12 +35,12 @@ async def call_get_favorite(
 @router.delete(
     "/",
 )
-async def call_delete_favorite(
-    user_id: int = Depends(get_user_by_header),
+async def delete_favorite(
+    user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     async with session.begin():
-        return await delete_favorite(
+        return await delete_favorite_service(
             session=session,
             user_id=user_id,
         )
--- backend/favorite_service/favorite_service/favorite/routers/favorite_sneaker.py
+++ backend/favorite_service/favorite_service/favorite/routers/favorite_sneaker.py
@@ -6,29 +6,33 @@ from favorite_service.favorite.models import (
 )
 from favorite_service.favorite.schemas import FavoriteSneakerCreate
 from favorite_service.favorite.schemas.favorite_sneaker import FavoriteSneakerUpdate
-from favorite_service.favorite.dependencies.check_permissions import (
+from favorite_service.favorite.dependencies.permissions import (
     check_role_permissions,
 )
 
 
-from favorite_service.favorite.dependencies.get_current_user import get_user_by_header
+from favorite_service.favorite.dependencies.user_id import get_current_user_id
 from favorite_service.favorite.config import settings
-from favorite_service.favorite.services.favorite.checkers import check_favorite_exists
+from favorite_service.favorite.services.favorite.fetch import (
+    get_user_favorite_id_service,
+)
 from favorite_service.favorite.services.favorite_sneaker.checkers import (
-    check_sneaker_in_favorite_exists,
+    get_sneaker_in_favorite_service,
 )
 from favorite_service.favorite.services.favorite_sneaker.create import (
-    create_sneaker_to_favorite,
+    add_sneaker_to_favorite_service,
 )
 from favorite_service.favorite.services.favorite_sneaker.delete import (
-    delete_sneaker_to_favorite,
+    delete_sneaker_from_favorite_service,
 )
 from favorite_service.favorite.services.favorite_sneaker.update import (
-    update_sneaker_to_favorite,
+    update_sneaker_in_favorite_service,
 )
-from favorite_service.favorite.services.sneaker.checkers import check_sneaker_exists
+from favorite_service.favorite.services.sneaker.checkers import (
+    check_sneaker_exists_service,
+)
 from favorite_service.favorite.services.sneaker_size.checkers import (
-    check_sneaker_size_exists,
+    check_sneaker_has_size_service,
 )
 
 router = APIRouter(
@@ -44,32 +48,32 @@ router = APIRouter(
     response_model=dict,
     dependencies=(Depends(check_role_permissions("favorite.sneaker.add")),),
 )
-async def call_create_sneaker_to_favorite(
+async def create_sneaker_to_favorite(
     item_create: FavoriteSneakerCreate,
-    user_id: int = Depends(get_user_by_header),
+    user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     async with session.begin():
-        favorite_id = await check_favorite_exists(
+        favorite_id = await get_user_favorite_id_service(
             session=session,
             user_id=user_id,
         )
-        await check_sneaker_exists(
+        await check_sneaker_exists_service(
             session=session,
             item_create=item_create,
         )
-        await check_sneaker_size_exists(
+        await check_sneaker_has_size_service(
             session=session,
             item_create=item_create,
         )
-        sneaker_record = await check_sneaker_in_favorite_exists(
+        sneaker_record = await get_sneaker_in_favorite_service(
             session=session,
             favorite_id=favorite_id,
             item_create=item_create,
         )
 
         if sneaker_record is None:
-            await create_sneaker_to_favorite(
+            await add_sneaker_to_favorite_service(
                 session=session,
                 favorite_id=favorite_id,
                 sneaker_id=item_create.sneaker_id,
@@ -84,14 +88,14 @@ async def call_create_sneaker_to_favorite(
     "/{favorite_sneaker_id}",
     response_model=dict,
 )
-async def call_update_sneaker_to_favorite(
+async def update_sneaker_in_favorite(
     favorite_sneaker_id: int,
     item_data: FavoriteSneakerUpdate,
-    user_id: int = Depends(get_user_by_header),
+    user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     async with session.begin():
-        updated_item = await update_sneaker_to_favorite(
+        updated_item = await update_sneaker_in_favorite_service(
             session=session,
             favorite_sneaker_id=favorite_sneaker_id,
             size_id=item_data.size_id,
@@ -105,13 +109,13 @@ async def call_update_sneaker_to_favorite(
     response_model=dict,
     dependencies=(Depends(check_role_permissions("favorite.sneaker.delete")),),
 )
-async def call_delete_sneaker_to_favorite(
+async def delete_sneaker_from_favorite(
     favorite_sneaker_id: int,
-    user_id: int = Depends(get_user_by_header),
+    user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     async with session.begin():
-        await delete_sneaker_to_favorite(
+        await delete_sneaker_from_favorite_service(
             session=session,
             user_id=user_id,
             favorite_sneaker_id=favorite_sneaker_id,
