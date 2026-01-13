c8ec36c79623adf32cfa4bd64e9b3bf28778fcca
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-13 13:20:38 +0300

    refactor(favorite_service): Rename modules containing dependencies and the dependencies themselves for a consistent name
    favorite_service/favorite/dependencies: check_permissions.py -> permissions.py
    
    favorite_service/favorite/dependencies: get_current_user.py -> user_id.py
    favorite_service/favorite/dependencies/user_id.py: get_user_by_header -> get_current_user_id
    
    favorite_service/favorite/dependencies: get_current_user_role.py -> user_role.py
    favorite_service/favorite/dependencies/user_role.py: get_user_role_by_header -> get_current_user_role

--- backend/favorite_service/favorite_service/favorite/dependencies/permissions.py
+++ backend/favorite_service/favorite_service/favorite/dependencies/permissions.py
@@ -3,8 +3,8 @@ from fastapi import HTTPException, Depends
 from starlette.requests import Request
 
 from favorite_service.favorite.config import settings
-from favorite_service.favorite.dependencies.get_current_user_role import (
-    get_user_role_by_header,
+from favorite_service.favorite.dependencies.user_role import (
+    get_current_user_role,
 )
 from redis_client.redis_connection.factory import get_redis_factory
 
@@ -23,7 +23,7 @@ def check_role_permissions(
                 db=settings.redis_config.redis_db,
             )
         ),
-        user_role: str = Depends(get_user_role_by_header),
+        user_role: str = Depends(get_current_user_role),
     ):
         request.state.redis_client = redis_client
         request.state.user_role = user_role
