6c7939b038e4d79ea8bbba46a73785ecd15e12b7
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-14 21:45:30 +0300

    feat(stock_notification_service): Add a router connection that gives endpoints associated with a one-time subscription

--- backend/stock_notification_service/stock_notification_service/__init__.py
+++ backend/stock_notification_service/stock_notification_service/__init__.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-from stock_notification_service.stock_notification.routers.stock_notification import router as stock_notification_router
+from stock_notification_service.stock_notification.routers.permanent_subscription import (
+    router as permanent_subscriptions_router,
+)
+from stock_notification_service.stock_notification.routers.one_time_subscription import (
+    router as one_time_subscriptions_router,
+)
 
 router = APIRouter()
 
-router.include_router(stock_notification_router)
+router.include_router(permanent_subscriptions_router)
+router.include_router(one_time_subscriptions_router)
