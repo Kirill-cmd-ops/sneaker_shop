e20c350f779b6422e0df03d21210382ff86eef23
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-14 21:18:07 +0300

    refactor(stock_notification_service): Change the names of the used kafka handlers to updated ones

--- backend/stock_notification_service/stock_notification_service/main.py
+++ backend/stock_notification_service/stock_notification_service/main.py
@@ -7,22 +7,23 @@ from fastapi import FastAPI
 
 from kafka.consumer import start_consumer, close_consumer
 from stock_notification_service.stock_notification.config import settings
-from stock_notification_service.stock_notification.kafka.kafka_handlers.brand_handler import (
-    handle_brand,
+from stock_notification_service.stock_notification.kafka.handlers.brands import (
+    handle_brand_event,
 )
-from stock_notification_service.stock_notification.kafka.kafka_handlers.size_handler import (
-    handle_size,
+from stock_notification_service.stock_notification.kafka.handlers.sizes import (
+    handle_size_event,
 )
-from stock_notification_service.stock_notification.kafka.kafka_handlers.sneaker_active_handler import \
-    handle_sneaker_active
-from stock_notification_service.stock_notification.kafka.kafka_handlers.sneaker_handler import (
-    handle_sneaker,
+from stock_notification_service.stock_notification.kafka.handlers.sneaker_active import (
+    handle_sneaker_active_event,
 )
-from stock_notification_service.stock_notification.kafka.kafka_handlers.sneaker_sizes_handler import (
-    handle_sneaker_sizes,
+from stock_notification_service.stock_notification.kafka.handlers.sneakers import (
+    handle_sneaker_event,
 )
-from stock_notification_service.stock_notification.kafka.kafka_handlers.user_handler import (
-    handle_user,
+from stock_notification_service.stock_notification.kafka.handlers.sneaker_sizes import (
+    handle_sneaker_sizes_event,
+)
+from stock_notification_service.stock_notification.kafka.handlers.users import (
+    handle_user_event,
 )
 from stock_notification_service.stock_notification.models import db_helper
 from stock_notification_service.add_middleware import add_middleware
@@ -34,53 +35,83 @@ from stock_notification_service import router as stock_notification_router
 async def lifespan(app: FastAPI):
     # startup
     sneaker_consumer, task_sneaker = await start_consumer(
-        settings.kafka_config.sneaker_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.sneaker_group_id,
-        handle_sneaker,
+        topic=settings.kafka_config.sneaker_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.sneaker_group_id,
+        handler=handle_sneaker_event,
     )
 
     sneaker_active_consumer, task_sneaker_active = await start_consumer(
-        settings.kafka_config.sneaker_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.sneaker_active_group_id,
-        handle_sneaker_active,
+        topic=settings.kafka_config.sneaker_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.sneaker_active_group_id,
+        handler=handle_sneaker_active_event,
     )
 
     brand_consumer, task_brand = await start_consumer(
-        settings.kafka_config.brand_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.brand_group_id,
-        handle_brand,
+        topic=settings.kafka_config.brand_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.brand_group_id,
+        handler=handle_brand_event,
     )
 
     size_consumer, task_size = await start_consumer(
-        settings.kafka_config.size_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.size_group_id,
-        handle_size,
+        topic=settings.kafka_config.size_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.size_group_id,
+        handler=handle_size_event,
     )
 
     sneaker_sizes_consumer, task_sneaker_sizes = await start_consumer(
-        settings.kafka_config.sneaker_sizes_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.sneaker_sizes_group_id,
-        handle_sneaker_sizes,
+        topic=settings.kafka_config.sneaker_sizes_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.sneaker_sizes_group_id,
+        handler=handle_sneaker_sizes_event,
     )
 
     user_consumer, task_user = await start_consumer(
-        settings.kafka_config.user_work_topic,
-        settings.kafka_config.kafka_bootstrap_servers,
-        settings.kafka_config.user_group_id,
-        handle_user,
+        topic=settings.kafka_config.user_work_topic,
+        bootstrap_servers=settings.kafka_config.kafka_bootstrap_servers,
+        group_id=settings.kafka_config.user_group_id,
+        handler=handle_user_event,
     )
     yield
-    task1 = create_task(close_consumer(sneaker_consumer, task_sneaker))
-    task2 = create_task(close_consumer(sneaker_active_consumer, task_sneaker_active))
-    task3 = create_task(close_consumer(brand_consumer, task_brand))
-    task4 = create_task(close_consumer(size_consumer, task_size))
-    task5 = create_task(close_consumer(sneaker_sizes_consumer, task_sneaker_sizes))
-    task6 = create_task(close_consumer(user_consumer, task_user))
+    task1 = create_task(
+        close_consumer(
+            consumer=sneaker_consumer,
+            task=task_sneaker,
+        )
+    )
+    task2 = create_task(
+        close_consumer(
+            consumer=sneaker_active_consumer,
+            task=task_sneaker_active,
+        )
+    )
+    task3 = create_task(
+        close_consumer(
+            consumer=brand_consumer,
+            task=task_brand,
+        )
+    )
+    task4 = create_task(
+        close_consumer(
+            consumer=size_consumer,
+            task=task_size,
+        )
+    )
+    task5 = create_task(
+        close_consumer(
+            consumer=sneaker_sizes_consumer,
+            task=task_sneaker_sizes,
+        )
+    )
+    task6 = create_task(
+        close_consumer(
+            consumer=user_consumer,
+            task=task_user,
+        )
+    )
 
     await asyncio.gather(task1, task2, task3, task4, task5, task6)
 
@@ -90,14 +121,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-add_middleware(app)
+add_middleware(app=app)
 
 app.include_router(stock_notification_router)
 
 # TODO: удалить, он лишний, мы все равно запускаем через docker compose
 if __name__ == "__main__":
     uvicorn.run(
-        "stock_notification_service.main:app",
+        app="stock_notification_service.main:app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
