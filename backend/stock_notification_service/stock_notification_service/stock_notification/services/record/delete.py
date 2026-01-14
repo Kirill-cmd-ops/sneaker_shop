2a8746853d84e6f0b6daffd2d96e7fc3ec5a66ba
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-14 18:58:40 +0300

    refactor(stock_notification_service): Rename the coroutines associated with the record to get a consistent name
    stock_notification_service/stock_notification/services/record/create.py: create_record -> create_record_service
    stock_notification_service/stock_notification/services/record/delete.py: delete_record -> delete_record_service

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/record/delete.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/record/delete.py
@@ -4,10 +4,10 @@ from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def delete_record(
-        session: AsyncSession,
-        table_name: Callable,
-        record_id,
+async def delete_record_service(
+    session: AsyncSession,
+    table_name: Callable,
+    record_id,
 ):
     delete_record_request = delete(table_name).where(table_name.id == record_id)
     await session.execute(delete_record_request)
