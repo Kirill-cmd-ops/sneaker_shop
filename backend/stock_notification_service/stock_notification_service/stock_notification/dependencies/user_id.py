eb16de32a942ab036a143e222d5a22eb40c3996c
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-14 21:20:31 +0300

    refactor(stock_notification_service): Rename modules containing dependencies and the dependencies themselves for a consistent name
    stock_notification_service/stock_notification/dependencies: get_current_user.py -> user_id.py
    sneaker_details_service/sneaker_details/dependencies/user_id.py: get_user_by_header -> get_current_user_id
    
    stock_notification_service/stock_notification/dependencies: get_current_user_role.py -> user_role.py
    stock_notification_service/stock_notification/dependencies/user_role.py: get_user_role_by_header -> get_current_user_role

---
+++ backend/stock_notification_service/stock_notification_service/stock_notification/dependencies/user_id.py
@@ -0,0 +1,8 @@
+from fastapi import Header, HTTPException
+
+
+async def get_current_user_id(user_id: str = Header(..., alias="X-User-Id")) -> int:
+    try:
+        return int(user_id)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid X-User-Id Header")
