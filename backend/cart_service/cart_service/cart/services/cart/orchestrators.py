700edab04fe0bde1e181e1fd0dcffd91855f990a
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-19 20:34:14 +0300

    refactor(sneaker_details_service): Transfer the database transaction from the orchestrator to the get_cart_service coroutine

--- backend/cart_service/cart_service/cart/services/cart/orchestrators.py
+++ backend/cart_service/cart_service/cart/services/cart/orchestrators.py
@@ -5,12 +5,11 @@ from cart_service.cart.services.cart.price import get_cart_total_service
 
 
 async def get_cart_orchestrator(session: AsyncSession, user_id: int):
-    async with session.begin():
-        items = await get_cart_service(
-            session=session,
-            user_id=user_id,
-        )
+    items = await get_cart_service(
+        session=session,
+        user_id=user_id,
+    )
 
-        total_price = get_cart_total_service(items=items)
+    total_price = get_cart_total_service(items=items)
 
-        return {"Цена корзины: ": total_price, "Кроссовки": items}
+    return {"Цена корзины: ": total_price, "Кроссовки": items}
