dc21032f8c22de6a8bc89980d77ba6924893942b
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 21:29:50 +0300

    refactor(cart_service): Add explicit transaction usage to coroutines related to sneaker associations
    Modified modules:
    - cart_service/cart/services/sneaker_association/delete.py

--- backend/cart_service/cart_service/cart/services/sneaker_association/delete.py
+++ backend/cart_service/cart_service/cart/services/sneaker_association/delete.py
@@ -4,12 +4,11 @@ from fastapi import HTTPException
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from cart_service.cart.models import Base
+from cart_service.cart.models import Base, db_helper
 from cart_service.cart.schemas import SneakerAssocsDelete
 
 
 async def delete_sneaker_associations_service(
-    session: AsyncSession,
     sneaker_id: int,
     sneaker_assoc_delete: SneakerAssocsDelete,
     sneaker_association_model: Type[Base],
@@ -18,17 +17,17 @@ async def delete_sneaker_associations_service(
     """
     Функция для одаления записи в ассоциативной таблице
     """
-    field = getattr(sneaker_association_model, field_name)
-    stmt = (
-        delete(sneaker_association_model)
-        .where(sneaker_association_model.sneaker_id == sneaker_id)
-        .where(field.in_(sneaker_assoc_delete.assoc_ids))
-    )
-    result = await session.execute(stmt)
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            field = getattr(sneaker_association_model, field_name)
+            stmt = (
+                delete(sneaker_association_model)
+                .where(sneaker_association_model.sneaker_id == sneaker_id)
+                .where(field.in_(sneaker_assoc_delete.assoc_ids))
+            )
+            result = await session.execute(stmt)
 
-    if result.rowcount == 0:
-        raise HTTPException(
-            status_code=404, detail="Ничего не найдено по вашим параметрам"
-        )
-
-    await session.commit()
+            if result.rowcount == 0:
+                raise HTTPException(
+                    status_code=404, detail="Ничего не найдено по вашим параметрам"
+                )
