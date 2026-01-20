17c7cdc53753442fe370e069402a15235ad4612d
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 22:15:23 +0300

    refactor(favorite_service): Add explicit transaction usage to record-related coroutines
    Modified modules:
    - favorite_service/favorite/services/record/create.py
    - favorite_service/favorite/services/record/delete.py

--- backend/favorite_service/favorite_service/favorite/services/record/create.py
+++ backend/favorite_service/favorite_service/favorite/services/record/create.py
@@ -1,14 +1,13 @@
 from typing import Callable
 
-from sqlalchemy.ext.asyncio import AsyncSession
+from favorite_service.favorite.models import db_helper
 
 
 async def create_record_service(
-    session: AsyncSession,
     table_name: Callable,
     schema_create,
 ):
-    new_record = table_name(**schema_create.dict())
-    session.add(new_record)
-    await session.commit()
-    return new_record
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            new_record = table_name(**schema_create.dict())
+            session.add(new_record)
--- backend/favorite_service/favorite_service/favorite/services/record/delete.py
+++ backend/favorite_service/favorite_service/favorite/services/record/delete.py
@@ -1,13 +1,14 @@
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from favorite_service.favorite.models import db_helper
+
 
 async def delete_record_service(
-        session: AsyncSession,
-        table_name,
-        record_id,
+    table_name,
+    record_id,
 ):
-    delete_record_request = delete(table_name).where(table_name.id == record_id)
-    await session.execute(delete_record_request)
-    await session.commit()
-    return {"result": "ok"}
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            delete_record_request = delete(table_name).where(table_name.id == record_id)
+            await session.execute(delete_record_request)
