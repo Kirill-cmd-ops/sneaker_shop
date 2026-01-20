7b76242a58c4e0f42a253e9e916acc040f93de93
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 21:31:46 +0300

    refactor(cart_service): Add explicit use of transactions to shoe-related and size-related coroutines
    Modified modules:
    - cart_service/cart/services/sneaker_size/create.py
    - cart_service/cart/services/sneaker_size/update.py

--- backend/cart_service/cart_service/cart/services/sneaker_size/create.py
+++ backend/cart_service/cart_service/cart/services/sneaker_size/create.py
@@ -1,24 +1,23 @@
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from cart_service.cart.models import SneakerSizeAssociation
+from cart_service.cart.models import SneakerSizeAssociation, db_helper
 from cart_service.cart.schemas import SneakerSizesCreate
 
 
 async def add_sizes_to_sneaker_service(
-    session: AsyncSession,
     sneaker_id: int,
     sneaker_sizes_create: SneakerSizesCreate,
 ):
-    sneaker_sizes = [
-        {
-            "sneaker_id": sneaker_id,
-            "size_id": size_data.size_id,
-            "quantity": size_data.quantity,
-        }
-        for size_data in sneaker_sizes_create.sizes
-    ]
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            sneaker_sizes = [
+                {
+                    "sneaker_id": sneaker_id,
+                    "size_id": size_data.size_id,
+                    "quantity": size_data.quantity,
+                }
+                for size_data in sneaker_sizes_create.sizes
+            ]
 
-    await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
-
-    await session.commit()
+            await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
--- backend/cart_service/cart_service/cart/services/sneaker_size/update.py
+++ backend/cart_service/cart_service/cart/services/sneaker_size/update.py
@@ -1,22 +1,24 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from cart_service.cart.models import SneakerSizeAssociation
+from cart_service.cart.models import SneakerSizeAssociation, db_helper
 from cart_service.cart.schemas import SneakerSizeUpdate
 
 
 async def update_sneaker_size_quantity_service(
-    session: AsyncSession,
     sneaker_id: int,
     sneaker_size_update: SneakerSizeUpdate,
 ):
-    sneaker_size = await session.scalar(
-        select(SneakerSizeAssociation)
-        .where(SneakerSizeAssociation.sneaker_id == sneaker_id)
-        .where(SneakerSizeAssociation.size_id == sneaker_size_update.size.size_id)
-    )
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            sneaker_size = await session.scalar(
+                select(SneakerSizeAssociation)
+                .where(SneakerSizeAssociation.sneaker_id == sneaker_id)
+                .where(
+                    SneakerSizeAssociation.size_id == sneaker_size_update.size.size_id
+                )
+            )
 
-    sneaker_size.quantity = sneaker_size_update.size.quantity
+            sneaker_size.quantity = sneaker_size_update.size.quantity
 
-    session.add(sneaker_size)
-    await session.commit()
+            session.add(sneaker_size)
