4c7526fd8d838831d87f667e987869e52a5d10bc
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 21:28:27 +0300

    refactor(cart_service): Add explicit transaction usage to sneaker-related coroutines
    Modified modules:
    - cart_service/cart/services/sneaker/create.py
    - cart_service/cart/services/sneaker/delete.py
    - cart_service/cart/services/sneaker/update.py

--- backend/cart_service/cart_service/cart/services/sneaker/delete.py
+++ backend/cart_service/cart_service/cart/services/sneaker/delete.py
@@ -1,18 +1,15 @@
 from sqlalchemy import delete
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from cart_service.cart.models import SneakerSizeAssociation, Sneaker
+from cart_service.cart.models import SneakerSizeAssociation, Sneaker, db_helper
 
 
 async def delete_sneaker_service(
-    session: AsyncSession,
     sneaker_id: int,
 ):
-    assoc_table = SneakerSizeAssociation
-    stmt = delete(assoc_table).where(assoc_table.sneaker_id == sneaker_id)
-    await session.execute(stmt)
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            assoc_table = SneakerSizeAssociation
+            stmt = delete(assoc_table).where(assoc_table.sneaker_id == sneaker_id)
+            await session.execute(stmt)
 
-    stmt = delete(Sneaker).where(Sneaker.id == sneaker_id)
-    await session.execute(stmt)
-
-    await session.commit()
+            stmt = delete(Sneaker).where(Sneaker.id == sneaker_id)
+            await session.execute(stmt)
--- backend/cart_service/cart_service/cart/services/sneaker/update.py
+++ backend/cart_service/cart_service/cart/services/sneaker/update.py
@@ -1,18 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from cart_service.cart.models import Sneaker
+from cart_service.cart.models import Sneaker, db_helper
 from cart_service.cart.schemas import SneakerUpdate
 
 
 async def update_sneaker_service(
-    session: AsyncSession,
     sneaker_id: int,
     sneaker_update: SneakerUpdate,
 ):
-    sneaker = await session.get(Sneaker, sneaker_id)
-    update_data = sneaker_update.dict(exclude_unset=True)
-    for field, value in update_data.items():
-        setattr(sneaker, field, value)
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            sneaker = await session.get(Sneaker, sneaker_id)
+            update_data = sneaker_update.dict(exclude_unset=True)
+            for field, value in update_data.items():
+                setattr(sneaker, field, value)
 
-    session.add(sneaker)
-    await session.commit()
+            session.add(sneaker)
