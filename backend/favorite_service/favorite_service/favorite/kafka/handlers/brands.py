ce8d59da2ef757a1e47f93193c8148861f93d2e5
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 22:20:10 +0300

    refactor(favorite_service): Delete session creation in kafka event handlers
    Modified modules:
    - favorite_service/favorite/kafka/handlers/brands.py
    - favorite_service/favorite/kafka/handlers/favorites.py
    - favorite_service/favorite/kafka/handlers/sizes.py
    - favorite_service/favorite/kafka/handlers/sneaker_sizes.py
    - favorite_service/favorite/kafka/handlers/sneakers.py

--- backend/favorite_service/favorite_service/favorite/kafka/handlers/brands.py
+++ backend/favorite_service/favorite_service/favorite/kafka/handlers/brands.py
@@ -1,4 +1,4 @@
-from favorite_service.favorite.models import db_helper, Brand
+from favorite_service.favorite.models import Brand
 from favorite_service.favorite.schemas import BrandCreate
 from favorite_service.favorite.services.record.create import create_record_service
 from favorite_service.favorite.services.record.delete import delete_record_service
@@ -7,22 +7,19 @@ from favorite_service.favorite.services.record.delete import delete_record_servi
 async def handle_brand_event(key: str | None, value: dict):
     try:
         event_type = value.get("event_type")
-        async with db_helper.session_context() as session:
-            if event_type == "brand_created":
-                data = value.get("data")
-                brand_create = BrandCreate(**data)
-                await create_record_service(
-                    session=session,
-                    table_name=Brand,
-                    schema_create=brand_create,
-                )
+        if event_type == "brand_created":
+            data = value.get("data")
+            brand_create = BrandCreate(**data)
+            await create_record_service(
+                table_name=Brand,
+                schema_create=brand_create,
+            )
 
-            elif event_type == "brand_deleted":
-                brand_id = value.get("brand_id")
-                await delete_record_service(
-                    session=session,
-                    table_name=Brand,
-                    record_id=brand_id,
-                )
+        elif event_type == "brand_deleted":
+            brand_id = value.get("brand_id")
+            await delete_record_service(
+                table_name=Brand,
+                record_id=brand_id,
+            )
     except Exception as e:
         print("Ошибка:", e)
--- backend/favorite_service/favorite_service/favorite/kafka/handlers/favorites.py
+++ backend/favorite_service/favorite_service/favorite/kafka/handlers/favorites.py
@@ -4,8 +4,4 @@ from favorite_service.favorite.services.favorite.create import create_favorite_s
 
 async def handle_favorite_event(key, value):
     user_id = key or value.get("id")
-    async with db_helper.session_context() as session:
-        await create_favorite_service(
-            session=session,
-            user_id=int(user_id),
-        )
+    await create_favorite_service(user_id=int(user_id))
