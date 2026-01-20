0741bcbd8ac20196558315c351ffac82bf1f3fc5
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-20 22:18:14 +0300

    refactor(favorite_service): Add explicit use of transactions to shoe-related and size-related coroutines
    Modified modules:
    - favorite_service/favorite/services/sneaker_size/create.py
    - favorite_service/favorite/services/sneaker_size/update.py

--- backend/favorite_service/favorite_service/favorite/services/sneaker_size/create.py
+++ backend/favorite_service/favorite_service/favorite/services/sneaker_size/create.py
@@ -1,23 +1,22 @@
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from favorite_service.favorite.models import SneakerSizeAssociation
+from favorite_service.favorite.models import SneakerSizeAssociation, db_helper
 from favorite_service.favorite.schemas import SneakerSizesCreate
 
 
 async def add_sizes_to_sneaker_service(
-    session: AsyncSession, sneaker_id: int, sneaker_sizes_create: SneakerSizesCreate
+    sneaker_id: int,
+    sneaker_sizes_create: SneakerSizesCreate,
 ):
-    sneaker_sizes = [
-        {
-            "sneaker_id": sneaker_id,
-            "size_id": size_data.size_id,
-            "quantity": size_data.quantity,
-        }
-        for size_data in sneaker_sizes_create.sizes
-    ]
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            sneaker_sizes = [
+                {
+                    "sneaker_id": sneaker_id,
+                    "size_id": size_data.size_id,
+                    "quantity": size_data.quantity,
+                }
+                for size_data in sneaker_sizes_create.sizes
+            ]
 
-    await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
-
-    await session.commit()
-
+            await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
