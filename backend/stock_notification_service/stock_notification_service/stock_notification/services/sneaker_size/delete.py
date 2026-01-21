aee66ee480f90fa9302aae3085c522e30b01cc80
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-21 17:32:46 +0300

    refactor(stock_notification_service): Add explicit use of transactions to shoe-related and size-related coroutines
    Modified modules:
    - stock_notification_service/stock_notification/services/sneaker_size/create.py
    - stock_notification_service/stock_notification/services/sneaker_size/delete.py

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/delete.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/delete.py
@@ -2,32 +2,31 @@ from typing import Type
 
 from fastapi import HTTPException
 from sqlalchemy import delete
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from stock_notification_service.stock_notification.models import Base
+from stock_notification_service.stock_notification.models import Base, db_helper
 from stock_notification_service.stock_notification.schemas import SneakerAssocsDelete
 
 
 async def delete_sizes_from_sneaker_service(
-    session: AsyncSession,
     sneaker_id: int,
     sneaker_assoc_delete: SneakerAssocsDelete,
     sneaker_association_model: Type[Base],
     field_name: str,
 ):
     """
-    Функция для удаления записи в ассоциативной таблице
+    Функция для одаления записи в ассоциативной таблице
     """
-    field = getattr(sneaker_association_model, field_name)
-    stmt = delete(sneaker_association_model).where(
-        sneaker_association_model.sneaker_id == sneaker_id,
-        field.in_(sneaker_assoc_delete.assoc_ids),
-    )
-    result = await session.execute(stmt)
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            field = getattr(sneaker_association_model, field_name)
+            stmt = (
+                delete(sneaker_association_model)
+                .where(sneaker_association_model.sneaker_id == sneaker_id)
+                .where(field.in_(sneaker_assoc_delete.assoc_ids))
+            )
+            result = await session.execute(stmt)
 
-    if result.rowcount == 0:
-        raise HTTPException(
-            status_code=404, detail="Ничего не найдено по вашим параметрам"
-        )
-
-    await session.commit()
+            if result.rowcount == 0:
+                raise HTTPException(
+                    status_code=404, detail="Ничего не найдено по вашим параметрам"
+                )
