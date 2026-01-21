a590a98412d752bb3144778550102e9829366da2
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-21 17:35:45 +0300

    refactor(stock_notification_service): Divide the coroutine logic into 3 coroutines to comply with the SRP principle

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/update.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/sneaker_size/update.py
@@ -4,16 +4,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from stock_notification_service.stock_notification.config import settings
 from stock_notification_service.stock_notification.models import (
     SneakerSizeAssociation,
+    db_helper,
 )
 from stock_notification_service.stock_notification.schemas import SneakerSizeUpdate
 
-from stock_notification_service.stock_notification.celery_tasks.update_tasks import (
-    handle_update_quantity,
+from stock_notification_service.stock_notification.celery_tasks.sneaker_size_quantity import (
+    process_sneaker_size_quantity_update,
 )
-from stock_notification_service.stock_notification.services.subscription.one_time.fetch import \
-    get_active_one_time_subscriptions_for_sneaker_service
-from stock_notification_service.stock_notification.services.subscription.permanent.fetch import \
-    get_active_permanent_subscriptions_for_sneaker_service
+from stock_notification_service.stock_notification.services.subscription.one_time.fetch import (
+    get_active_one_time_subscriptions_for_sneaker_service,
+)
+from stock_notification_service.stock_notification.services.subscription.permanent.fetch import (
+    get_active_permanent_subscriptions_for_sneaker_service,
+)
 
 
 async def update_sneaker_size_quantity_service(
@@ -33,46 +36,43 @@ async def update_sneaker_size_quantity_service(
     sneaker_size.quantity = sneaker_size_update.size.quantity
 
     session.add(sneaker_size)
-    await session.commit()
 
-    if sneaker_size_quantity_old == 0 and sneaker_size_quantity_new > 0:
-        subscribed_users = await get_active_permanent_subscriptions_for_sneaker_service(
+    return sneaker_size_quantity_old == 0 and sneaker_size_quantity_new > 0
+
+
+# TODO: переместить
+async def get_subscribed_emails(
+    session: AsyncSession,
+    sneaker_id: int,
+    sneaker_size_update: SneakerSizeUpdate,
+):
+    subscribed_users = await get_active_permanent_subscriptions_for_sneaker_service(
+        session=session,
+        sneaker_id=sneaker_id,
+        size_id=sneaker_size_update.size.size_id,
+    )
+
+    subscribed_users_one_time = (
+        await get_active_one_time_subscriptions_for_sneaker_service(
             session=session,
             sneaker_id=sneaker_id,
             size_id=sneaker_size_update.size.size_id,
         )
+    )
+    return list(set(list(subscribed_users) + list(subscribed_users_one_time)))
 
-        subscribed_users_one_time = await get_active_one_time_subscriptions_for_sneaker_service(
-            session=session,
-            sneaker_id=sneaker_id,
-            size_id=sneaker_size_update.size.size_id,
+
+async def send_notification_for_subscribed_emails(emails):
+    for email in emails:
+        process_sneaker_size_quantity_update.delay(
+            hostname=settings.smtp_config.smtp_hostname,
+            port=settings.smtp_config.smtp_port,
+            start_tls=settings.smtp_config.smtp_start_tls,
+            username=settings.smtp_config.smtp_username,
+            password=settings.smtp_config.smtp_password,
+            sender_gmail="Sneaker Shop <bondarenkokirill150208@gmail.com>",
+            recipient_gmail=email,
+            email_title="Уведомление о поступлении товара",
+            body_title="Данная модель кроссовок поступила в наличие",
         )
-
-        for user_email in subscribed_users:
-            handle_update_quantity.delay(
-                hostname=settings.smtp_config.smtp_hostname,
-                port=settings.smtp_config.smtp_port,
-                start_tls=settings.smtp_config.smtp_start_tls,
-                username=settings.smtp_config.smtp_username,
-                password=settings.smtp_config.smtp_password,
-                sender_gmail="Sneaker Shop <bondarenkokirill150208@gmail.com>",
-                recipient_gmail=user_email,
-                email_title="Уведомление о поступлении товара",
-                body_title="Данная модель кроссовок поступила в наличие",
-            )
-
-        for user_email_one_time in subscribed_users_one_time:
-            handle_update_quantity.delay(
-                hostname=settings.smtp_config.smtp_hostname,
-                port=settings.smtp_config.smtp_port,
-                start_tls=settings.smtp_config.smtp_start_tls,
-                username=settings.smtp_config.smtp_username,
-                password=settings.smtp_config.smtp_password,
-                sender_gmail="Sneaker Shop <bondarenkokirill150208@gmail.com>",
-                recipient_gmail=user_email_one_time,
-                email_title="Уведомление о поступлении товара",
-                body_title="Данная модель кроссовок поступила в наличие",
-            )
-
-
-# TODO: доработать логику
+        # TODO: доработать логику
