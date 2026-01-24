73ec580ae6f274efe9147f60cde2096ee1301289
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-24 12:52:39 +0300

    refactor(stock_notification_service): Transfer endpoint service logic to the orchestrator
    The modified endpoint:
    POST /api/v1/permanent_subscriptions/
    
    The service logic has been moved to the following location:
    stock_notification_service/stock_notification/services/subscription/orchestrators.py - create_user_permanent_subscription_orchestrator

--- backend/stock_notification_service/stock_notification_service/stock_notification/routers/permanent_subscription.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/routers/permanent_subscription.py
@@ -10,20 +10,9 @@ from stock_notification_service.stock_notification.models import db_helper
 from stock_notification_service.stock_notification.schemas.subscription import (
     SubscriptionCreate,
 )
-from stock_notification_service.stock_notification.services.sneaker.checkers import (
-    check_sneaker_active_service,
-)
-from stock_notification_service.stock_notification.services.sneaker_size.checkers import (
-    check_inactive_sneaker_size_service,
-)
-from stock_notification_service.stock_notification.services.subscription.one_time.checkers import (
-    check_active_one_time_subscription_service,
-)
-from stock_notification_service.stock_notification.services.subscription.permanent.checkers import (
-    check_active_permanent_subscription_service,
-)
-from stock_notification_service.stock_notification.services.subscription.permanent.create import (
-    create_user_permanent_subscription_service,
+from stock_notification_service.stock_notification.services.subscription.orchestrators import (
+    create_user_permanent_subscription_orchestrator,
+    reactivate_all_permanent_subscriptions_for_user_orchestrator,
 )
 from stock_notification_service.stock_notification.services.subscription.permanent.deactivate import (
     deactivate_user_permanent_subscription_service,
@@ -33,11 +22,6 @@ from stock_notification_service.stock_notification.services.subscription.permane
 )
 from stock_notification_service.stock_notification.services.subscription.permanent.fetch import (
     get_active_permanent_subscriptions_for_user_service,
-    get_inactive_permanent_subscription_for_user_service,
-)
-from stock_notification_service.stock_notification.services.subscription.permanent.reactivate import (
-    reactivate_permanent_subscription_by_id_service,
-    reactivate_permanent_subscription_by_sneaker_size_service,
 )
 
 router = APIRouter(
@@ -55,62 +39,17 @@ async def create_user_permanent_subscription(
     user_id: int = Depends(get_current_user_id),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-    async with session.begin():
-        try:
-            # проверка активности sneaker
-            await check_sneaker_active_service(
-                session=session,
-                sneaker_id=subscription_create.sneaker_id,
-            )
-
-            # проверка доступности размера для sneaker и проверка что размер на данный момент неактивен
-            await check_inactive_sneaker_size_service(
-                session=session,
-                sneaker_id=subscription_create.sneaker_id,
-                size_id=subscription_create.size_id,
-            )
-
-            # проверка активной перманентной подписки
-            await check_active_permanent_subscription_service(
-                session=session,
-                user_id=user_id,
-                sneaker_id=subscription_create.sneaker_id,
-                size_id=subscription_create.size_id,
-            )
-
-            # проверка активности одноразовой подписки
-            await check_active_one_time_subscription_service(
-                session=session,
-                user_id=user_id,
-                sneaker_id=subscription_create.sneaker_id,
-                size_id=subscription_create.size_id,
-            )
-
-            # реактивация деактивной перманентной подписки
-            update_subscription = (
-                await reactivate_permanent_subscription_by_sneaker_size_service(
-                    session=session,
-                    user_id=user_id,
-                    sneaker_id=subscription_create.sneaker_id,
-                    size_id=subscription_create.size_id,
-                )
-            )
-            if update_subscription:
-                return update_subscription
-
-            # создание перманентной подписки
-            return await create_user_permanent_subscription_service(
-                sneaker_id=subscription_create.sneaker_id,
-                size_id=subscription_create.size_id,
-                user_id=user_id,
-                session=session,
-            )
-
-        except IntegrityError as e:
-            raise HTTPException(
-                status_code=404,
-                detail="Не удалось найти требуемую модель кроссовок",
-            )
+    try:
+        return await create_user_permanent_subscription_orchestrator(
+            session=session,
+            user_id=user_id,
+            subscription_create=subscription_create,
+        )
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=404,
+            detail="Не удалось найти требуемую модель кроссовок",
+        )
 
 
 @router.patch("/{subscription_id}/deactivate")
--- backend/stock_notification_service/stock_notification_service/stock_notification/services/subscription/orchestrators.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/subscription/orchestrators.py
@@ -32,8 +32,18 @@ from stock_notification_service.stock_notification.services.subscription.one_tim
 from stock_notification_service.stock_notification.services.subscription.permanent.checkers import (
     check_active_permanent_subscription_service,
 )
+from stock_notification_service.stock_notification.services.subscription.permanent.create import (
+    create_user_permanent_subscription_service,
+)
 from stock_notification_service.stock_notification.services.subscription.permanent.deactivate_bulk import (
     deactivate_all_permanent_subscriptions_for_sneaker_service,
+)
+from stock_notification_service.stock_notification.services.subscription.permanent.fetch import (
+    get_inactive_permanent_subscription_for_user_service,
+)
+from stock_notification_service.stock_notification.services.subscription.permanent.reactivate import (
+    reactivate_permanent_subscription_by_sneaker_size_service,
+    reactivate_permanent_subscription_by_id_service,
 )
 from stock_notification_service.stock_notification.services.subscription.permanent.reactivate_bulk import (
     reactivate_all_permanent_subscriptions_for_sneaker_service,
@@ -152,6 +162,100 @@ async def reactivate_all_one_time_subscriptions_for_user_orchestrator(
         )
 
         result = await reactivate_one_time_subscription_by_id_service(
+            subscription_id=subscription_id,
+            user_id=user_id,
+            session=session,
+        )
+
+    return result
+
+
+async def create_user_permanent_subscription_orchestrator(
+    session: AsyncSession,
+    user_id: int,
+    subscription_create: SubscriptionCreate,
+):
+    async with session.begin():
+        # проверка активности sneaker
+        await check_sneaker_active_service(
+            session=session,
+            sneaker_id=subscription_create.sneaker_id,
+        )
+
+        # проверка доступности размера для sneaker и проверка что размер на данный момент неактивен
+        await check_inactive_sneaker_size_service(
+            session=session,
+            sneaker_id=subscription_create.sneaker_id,
+            size_id=subscription_create.size_id,
+        )
+
+        # проверка активной перманентной подписки
+        await check_active_permanent_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=subscription_create.sneaker_id,
+            size_id=subscription_create.size_id,
+        )
+
+        # проверка активности одноразовой подписки
+        await check_active_one_time_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=subscription_create.sneaker_id,
+            size_id=subscription_create.size_id,
+        )
+
+        # реактивация деактивной перманентной подписки
+        update_subscription = (
+            await reactivate_permanent_subscription_by_sneaker_size_service(
+                session=session,
+                user_id=user_id,
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+            )
+        )
+        if update_subscription:
+            result = update_subscription
+        else:
+            # создание перманентной подписки
+            result = await create_user_permanent_subscription_service(
+                sneaker_id=subscription_create.sneaker_id,
+                size_id=subscription_create.size_id,
+                user_id=user_id,
+                session=session,
+            )
+    return result
+
+
+async def reactivate_all_permanent_subscriptions_for_user_orchestrator(
+    session: AsyncSession,
+    user_id: int,
+    subscription_id: int,
+):
+    async with session.begin():
+        inactive_subscription = (
+            await get_inactive_permanent_subscription_for_user_service(
+                session=session,
+                user_id=user_id,
+                subscription_id=subscription_id,
+            )
+        )
+
+        await check_active_permanent_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=inactive_subscription.sneaker_id,
+            size_id=inactive_subscription.size_id,
+        )
+
+        await check_active_one_time_subscription_service(
+            session=session,
+            user_id=user_id,
+            sneaker_id=inactive_subscription.sneaker_id,
+            size_id=inactive_subscription.size_id,
+        )
+
+        result = await reactivate_permanent_subscription_by_id_service(
             subscription_id=subscription_id,
             user_id=user_id,
             session=session,
