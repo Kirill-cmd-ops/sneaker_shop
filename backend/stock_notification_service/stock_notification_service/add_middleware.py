7fc7b92fb128a4af551d96f2a0c52c4d42181625
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-25 19:31:06 +0300

    feat(stock_notification_service): Add an additional port to middleware from which requests will be accepted
    - A port has been added to allow_origins http://127.0.0.1:3000
    - Added the expose_headers parameter with the value ["*"]

--- backend/stock_notification_service/stock_notification_service/add_middleware.py
+++ backend/stock_notification_service/stock_notification_service/add_middleware.py
@@ -4,8 +4,12 @@ from starlette.middleware.cors import CORSMiddleware
 def add_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=[
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["*"],
     )
