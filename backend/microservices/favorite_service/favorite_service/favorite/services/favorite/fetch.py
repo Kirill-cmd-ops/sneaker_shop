f788eb0c6d39cdeb4690b8591034c05e72c55d53
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-25 22:13:43 +0300

    chore(root): Move the favorite_service directory
    - The favorite_service directory has been moved from backend/ to backend/microservices/

--- backend/microservices/favorite_service/favorite_service/favorite/services/favorite/fetch.py
+++ backend/microservices/favorite_service/favorite_service/favorite/services/favorite/fetch.py
@@ -7,17 +7,16 @@ from favorite_service.favorite.models import Favorite
 
 
 async def get_favorite_service(session: AsyncSession, user_id: int):
-    async with session.begin():
-        favorite = await session.scalar(
-            select(Favorite)
-            .where(Favorite.user_id == user_id)
-            .options(selectinload(Favorite.sneaker_associations))
-        )
+    favorite = await session.scalar(
+        select(Favorite)
+        .where(Favorite.user_id == user_id)
+        .options(selectinload(Favorite.sneaker_associations))
+    )
 
-        if favorite is None:
-            raise HTTPException(
-                status_code=404, detail="У данного пользователя нету избранного"
-            )
+    if favorite is None:
+        raise HTTPException(
+            status_code=404, detail="У данного пользователя нету избранного"
+        )
 
     return favorite.sneaker_associations
 
