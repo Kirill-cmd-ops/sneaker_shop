666d7bea1039e2666f69865a239394fa7b5e92ed
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-26 00:57:23 +0300

    refactor(stock_notification_service): To add user-related service layer coroutines, create your own session and explicitly use transactions
    - Creating your own session has been added to the following coroutines:
    create_user_service, delete_user_service, update_user_service
    - Explicit use of transactions added to coroutines: create_user_service, delete_user_service, update_user_service

--- backend/stock_notification_service/stock_notification_service/stock_notification/services/user/create.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/user/create.py
@@ -1,15 +1,11 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from stock_notification_service.stock_notification.models import User
+from stock_notification_service.stock_notification.models import User, db_helper
 from stock_notification_service.stock_notification.schemas import UserCreate
 
 
 async def create_user_service(
-    session: AsyncSession,
     user_create: UserCreate,
 ):
-    user = User(**user_create.dict())
-    session.add(user)
-    await session.flush()
-
-    await session.commit()
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            user = User(**user_create.dict())
+            session.add(user)
--- backend/stock_notification_service/stock_notification_service/stock_notification/services/user/delete.py
+++ backend/stock_notification_service/stock_notification_service/stock_notification/services/user/delete.py
@@ -1,11 +1,10 @@
 from sqlalchemy import delete
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from stock_notification_service.stock_notification.models import User
+from stock_notification_service.stock_notification.models import User, db_helper
 
 
-async def delete_user_service(session: AsyncSession, user_id: int):
-    stmt = delete(User).where(User.id == user_id)
-    await session.execute(stmt)
-
-    await session.commit()
+async def delete_user_service(user_id: int):
+    async with db_helper.session_context() as session:
+        async with session.begin():
+            stmt = delete(User).where(User.id == user_id)
+            await session.execute(stmt)
