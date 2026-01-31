914742fa1333889773861fdfeb6d01923cc1ddb1
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-01-31 23:43:54 +0300

    refactor(frontend): Change the size of the registration component display

--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -3,9 +3,10 @@ import RegisterForm from "../components/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <main className="flex flex-col items-center min-h-screen bg-white text-black p-10">
-      <h1 className="text-3xl font-bold mb-6">Регистрация</h1>
-      <RegisterForm />
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white text-black p-4">
+      <div className="py-8">
+        <RegisterForm />
+      </div>
     </main>
   );
 }
