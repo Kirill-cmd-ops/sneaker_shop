3441654dbf6ec3331f6a44e82383467c11c4d480
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-02-07 11:49:53 +0000

    EEW38VN2-71 Remove redundant try/except blocks in cart service

--- backend/microservices/cart_service/cart_service/cart/kafka/handlers/sneakers.py
+++ backend/microservices/cart_service/cart_service/cart/kafka/handlers/sneakers.py
@@ -5,33 +5,30 @@ from microservices.cart_service.cart_service.cart.services.sneaker.update import
 
 
 async def handle_sneaker_event(key: str | None, value: dict):
-    try:
-        event_type = value.get("event_type")
-        if event_type == "sneaker_created":
-            data = value.get("data")
-            sneaker_create = SneakerCreate(**data)
-            sneaker_create_data = sneaker_create.model_dump(exclude={"size_ids"})
-            size_ids = [
-                size.model_dump(include={"size_id", "quantity"})
-                for size in sneaker_create.size_ids
-            ]
+    event_type = value.get("event_type")
+    if event_type == "sneaker_created":
+        data = value.get("data")
+        sneaker_create = SneakerCreate(**data)
+        sneaker_create_data = sneaker_create.model_dump(exclude={"size_ids"})
+        size_ids = [
+            size.model_dump(include={"size_id", "quantity"})
+            for size in sneaker_create.size_ids
+        ]
 
-            await create_sneaker_service(
-                sneaker_data=sneaker_create_data,
-                size_ids=size_ids,
-            )
-        elif event_type == "sneaker_updated":
-            data = value.get("data")
-            sneaker_id = value.get("sneaker_id")
-            sneaker_update = SneakerUpdate(**data)
-            sneaker_update_data = sneaker_update.model_dump(exclude_unset=True)
-            await update_sneaker_service(
-                sneaker_id=sneaker_id,
-                sneaker_data=sneaker_update_data,
-            )
+        await create_sneaker_service(
+            sneaker_data=sneaker_create_data,
+            size_ids=size_ids,
+        )
+    elif event_type == "sneaker_updated":
+        data = value.get("data")
+        sneaker_id = value.get("sneaker_id")
+        sneaker_update = SneakerUpdate(**data)
+        sneaker_update_data = sneaker_update.model_dump(exclude_unset=True)
+        await update_sneaker_service(
+            sneaker_id=sneaker_id,
+            sneaker_data=sneaker_update_data,
+        )
 
-        elif event_type == "sneaker_deleted":
-            sneaker_id = value.get("sneaker_id")
-            await delete_sneaker_service(sneaker_id=sneaker_id)
-    except Exception as e:
-        print("Ошибка:", e)
+    elif event_type == "sneaker_deleted":
+        sneaker_id = value.get("sneaker_id")
+        await delete_sneaker_service(sneaker_id=sneaker_id)
