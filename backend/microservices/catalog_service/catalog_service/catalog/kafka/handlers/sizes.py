9810f6555f5a1d49da3ef80a10bfe4b75c38e636
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-02-07 17:43:32 +0300

    EEW38VN2-73 refactor(catalog_service): remove redundant try/except blocks

--- backend/microservices/catalog_service/catalog_service/catalog/kafka/handlers/sizes.py
+++ backend/microservices/catalog_service/catalog_service/catalog/kafka/handlers/sizes.py
@@ -5,22 +5,19 @@ from microservices.catalog_service.catalog_service.catalog.services.record.delet
 
 
 async def handle_size_event(key: str | None, value: dict):
-    try:
-        event_type = value.get("event_type")
-        if event_type == "size_created":
-            data = value.get("data")
-            size_create = SizeCreate(**data)
-            size_create_data = size_create.model_dump()
-            await create_record_service(
-                table_name=Size,
-                data=size_create_data,
-            )
+    event_type = value.get("event_type")
+    if event_type == "size_created":
+        data = value.get("data")
+        size_create = SizeCreate(**data)
+        size_create_data = size_create.model_dump()
+        await create_record_service(
+            table_name=Size,
+            data=size_create_data,
+        )
 
-        elif event_type == "size_deleted":
-            size_id = value.get("size_id")
-            await delete_record_service(
-                table_name=Size,
-                record_id=size_id,
-            )
-    except Exception as e:
-        print("Ошибка:", e)
+    elif event_type == "size_deleted":
+        size_id = value.get("size_id")
+        await delete_record_service(
+            table_name=Size,
+            record_id=size_id,
+        )
