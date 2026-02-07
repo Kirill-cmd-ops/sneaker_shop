9c06855cd22e7d9093e7a796ec2a25530b55e0b7
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-02-07 18:01:00 +0300

    EEW38VN2-74 refactor(sneaker_details_service): remove redundant try/except blocks

--- backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/user_id.py
+++ backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/user_id.py
@@ -4,9 +4,6 @@ from fastapi import Header, HTTPException
 async def get_current_user_id(
         user_id: str = Header(None, alias="X-User-Id")
 ) -> int | None:
-    try:
-        if user_id:
-            return int(user_id)
-        return None
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid X-User-Id Header")
+    if user_id:
+        return int(user_id)
+    return None
--- backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
+++ backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/seeds/__init__.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from microservices.sneaker_details_service.sneaker_details_service.sneaker_details.models import db_helper
 from microservices.sneaker_details_service.sneaker_details_service.sneaker_details.seeds.brands import seed_brands
 from microservices.sneaker_details_service.sneaker_details_service.sneaker_details.seeds.sizes import seed_sizes
@@ -18,24 +17,16 @@ from microservices.sneaker_details_service.sneaker_details_service.sneaker_detai
 
 
 async def run_seeds():
-    session_gen = db_helper.session_getter()
-    session: AsyncSession = await anext(session_gen)
+    async with db_helper.session_getter() as session:
+        async with session.begin():
+            await seed_brands(session=session)
+            await seed_sizes(session=session)
+            await seed_countries(session=session)
+            await seed_sneakers(session=session)
+            await seed_sneaker_sizes(session=session)
+            await seed_colors(session=session)
+            await seed_sneaker_colors(session=session)
+            await seed_materials(session=session)
+            await seed_sneaker_materials(session=session)
 
-    try:
-        await seed_brands(session=session)
-        await seed_sizes(session=session)
-        await seed_countries(session=session)
-        await seed_sneakers(session=session)
-        await seed_sneaker_sizes(session=session)
-        await seed_colors(session=session)
-        await seed_sneaker_colors(session=session)
-        await seed_materials(session=session)
-        await seed_sneaker_materials(session=session)
-        await session.commit()
-        print("All seeds completed successfully!")
-    except Exception as e:
-        await session.rollback()
-        print(f"Error during seeding: {e}")
-        raise
-    finally:
-        await session.close()
+    print("All seeds completed successfully!")
