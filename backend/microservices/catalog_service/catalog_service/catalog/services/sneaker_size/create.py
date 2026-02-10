fa0d8149a327a3a1d86dd80a7082a85e160fd61c
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-02-10 21:48:47 +0300

    EEW38VN2-46 feat(catalog_service): add stubs to prevent exceptions in Kafka handler coroutines

--- backend/microservices/catalog_service/catalog_service/catalog/services/sneaker_size/create.py
+++ backend/microservices/catalog_service/catalog_service/catalog/services/sneaker_size/create.py
@@ -1,6 +1,7 @@
 from typing import Dict, Any
 
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.exc import IntegrityError
 
 from microservices.catalog_service.catalog_service.catalog.models import SneakerSizeAssociation, db_helper
 
@@ -9,15 +10,18 @@ async def add_sizes_to_sneaker_service(
         sneaker_id: int,
         size_list: list[Dict[str, Any]],
 ):
-    async with db_helper.session_context() as session:
-        async with session.begin():
-            sneaker_sizes = [
-                {
-                    "sneaker_id": sneaker_id,
-                    "size_id": size_data["size_id"],
-                    "quantity": size_data["quantity"],
-                }
-                for size_data in size_list
-            ]
+    try:
+        async with db_helper.session_context() as session:
+            async with session.begin():
+                sneaker_sizes = [
+                    {
+                        "sneaker_id": sneaker_id,
+                        "size_id": size_data["size_id"],
+                        "quantity": size_data["quantity"],
+                    }
+                    for size_data in size_list
+                ]
 
-            await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
+                await session.execute(insert(SneakerSizeAssociation).values(sneaker_sizes))
+    except IntegrityError:
+        return
