28e68922ec1aadacc080d8de924d46ef137929f3
Bondarenko Kirill <bondarenkokirill150208@gmail.com>
2026-02-10 21:53:17 +0300

    EEW38VN2-46 feat(catalog_service): add stubs to prevent exceptions in Kafka handler coroutines (#33)

--- backend/microservices/catalog_service/catalog_service/catalog/services/sneaker/create.py
+++ backend/microservices/catalog_service/catalog_service/catalog/services/sneaker/create.py
@@ -1,5 +1,7 @@
 from typing import Dict, Any
 
+from sqlalchemy.exc import IntegrityError
+
 from microservices.catalog_service.catalog_service.catalog.models import Sneaker, SneakerSizeAssociation, db_helper
 
 
@@ -7,17 +9,20 @@ async def create_sneaker_service(
         sneaker_data: Dict[str, Any],
         size_ids: list[Dict[str, Any]],
 ):
-    async with db_helper.session_context() as session:
-        async with session.begin():
-            sneaker = Sneaker(**sneaker_data)
-            session.add(sneaker)
-            await session.flush()
+    try:
+        async with db_helper.session_context() as session:
+            async with session.begin():
+                sneaker = Sneaker(**sneaker_data)
+                session.add(sneaker)
+                await session.flush()
 
-            if size_ids:
-                for size in size_ids:
-                    sneaker_sizes = SneakerSizeAssociation(
-                        sneaker_id=sneaker.id,
-                        size_id=size["size_id"],
-                        quantity=size["quantity"],
-                    )
-                    session.add(sneaker_sizes)
+                if size_ids:
+                    for size in size_ids:
+                        sneaker_sizes = SneakerSizeAssociation(
+                            sneaker_id=sneaker.id,
+                            size_id=size["size_id"],
+                            quantity=size["quantity"],
+                        )
+                        session.add(sneaker_sizes)
+    except IntegrityError:
+        return
