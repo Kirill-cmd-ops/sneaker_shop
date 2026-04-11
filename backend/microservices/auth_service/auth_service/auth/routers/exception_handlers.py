37ca755203efa9ca1061abafc013f436ebba95c4
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-04-11 09:01:52 +0300

    EEW38VN2-99 refactor(auth_service): replace async def with def where there is no i/o-bound in auth_service

--- backend/microservices/auth_service/auth_service/auth/routers/exception_handlers.py
+++ backend/microservices/auth_service/auth_service/auth/routers/exception_handlers.py
@@ -6,17 +6,17 @@ from microservices.auth_service.auth_service.auth.domain.exceptions import RoleN
     BlacklistAlreadyExists
 
 
-async def role_not_found_handler(request: Request, exc:RoleNotFound) -> JSONResponse:
+def role_not_found_handler(request: Request, exc: RoleNotFound) -> JSONResponse:
     return JSONResponse(status_code=404, content="details: Role not found")
 
 
-async def user_not_found_handler(request: Request, exc:UserNotFound) -> JSONResponse:
+def user_not_found_handler(request: Request, exc: UserNotFound) -> JSONResponse:
     return JSONResponse(status_code=404, content="details: User not found")
 
 
-async def user_role_association_already_exists_handler(request: Request, exc:UserRoleAssociationAlreadyExists) -> JSONResponse:
+def user_role_association_already_exists_handler(request: Request, exc: UserRoleAssociationAlreadyExists) -> JSONResponse:
     return JSONResponse(status_code=409, content="details: User role association already exists")
 
 
-async def blacklist_already_exists_handler(request: Request, exc:BlacklistAlreadyExists) -> JSONResponse:
+def blacklist_already_exists_handler(request: Request, exc: BlacklistAlreadyExists) -> JSONResponse:
     return JSONResponse(status_code=409, content="details: Blacklist already exists")
