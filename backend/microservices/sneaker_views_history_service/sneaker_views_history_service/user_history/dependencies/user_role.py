747a7e502254b7ace2192a102026b437049e0cb2
Kirill-cmd-ops <bondarenkokirill150208@gmail.com>
2026-04-11 10:01:42 +0300

    EEW38VN2-104 refactor(sneaker_views_history_service): replace async def with def where there is no i/o-bound

--- backend/microservices/sneaker_views_history_service/sneaker_views_history_service/user_history/dependencies/user_role.py
+++ backend/microservices/sneaker_views_history_service/sneaker_views_history_service/user_history/dependencies/user_role.py
@@ -1,5 +1,5 @@
 from fastapi import Header
 
 
-async def get_current_user_role(user_role: str = Header(..., alias="X-User-Role")) -> str:
+def get_current_user_role(user_role: str = Header(..., alias="X-User-Role")) -> str:
     return user_role
