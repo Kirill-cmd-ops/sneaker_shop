3a623c0ef9f7e0815b964d752c94ebed34d944ab
Bondarenko Kirill <bondarenkokirill150208@gmail.com>
2026-04-11 09:39:04 +0300

    EEW38VN2-103 refactor(sneaker_details_service): replace async def with def where there is no i/o-bound (#49)

--- backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/user_id.py
+++ backend/microservices/sneaker_details_service/sneaker_details_service/sneaker_details/dependencies/user_id.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException
 
 
-async def get_current_user_id(
+def get_current_user_id(
         user_id: str = Header(None, alias="X-User-Id")
 ) -> int | None:
     if user_id:
